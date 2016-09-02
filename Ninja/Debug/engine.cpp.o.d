80397df5f5fb414f27f81555d7306995c456df68
Adriqun <adrmic98@gmail.com>
2016-09-02 23:26:15 +0200

    up

--- Ninja/Debug/engine.cpp.o.d
+++ Ninja/Debug/engine.cpp.o.d
@@ -1,5 +1,5 @@
 Debug/engine.cpp.o: engine.cpp engine.h core.h link_button.h sprite.h \
- menu_play_button.h text.h
+ menu_play_button.h text.h menu_title.h menu_music_button.h
 
 engine.h:
 
@@ -12,3 +12,7 @@ sprite.h:
 menu_play_button.h:
 
 text.h:
+
+menu_title.h:
+
+menu_music_button.h:
