1c3f29c03a7fa45e050b22cf07a071f05e773fda
Adriqun <adrmic98@gmail.com>
2016-09-03 18:20:54 +0200

    core update, new state = -2

--- Ninja/core.cpp
+++ Ninja/core.cpp
@@ -1,9 +1,10 @@
 #include "core.h"
 #include <stdio.h>
+#include <SDL2/SDL_mixer.h>
 
-Core::Core( unsigned width, unsigned height, unsigned FPS )
+Core::Core( int state, unsigned width, unsigned height, unsigned FPS )
 {
-    state = 0;
+    this->state = state;
     open = true;
 
     this->FPS = FPS;
@@ -38,6 +39,8 @@ void Core::free()
         color.g = 0;
         color.b = 0;
         color.a = 0;
+		
+		Mix_Quit();
     }
 }
 
@@ -47,7 +50,7 @@ bool Core::load( string title )
 
     free();
 
-    window = new RenderWindow( VideoMode( width, height ), title, Style::Titlebar | Style::Close );
+    window = new sf::RenderWindow( sf::VideoMode( width, height ), title, sf::Style::Titlebar | sf::Style::Close );
     if( window == NULL )
     {
         printf( "Not created window!\n" );
@@ -57,7 +60,14 @@ bool Core::load( string title )
     {
         window->setFramerateLimit( FPS );
         printf( "Created window!\n" );
-    }
+
+		if( Mix_OpenAudio( 44100, MIX_DEFAULT_FORMAT, 2, 2048 ) < 0 )
+		{
+			printf( "Mix could not initialize! Error: %s\n", Mix_GetError() );
+			success = false;
+		}
+		
+	}
 
 
     return success;
@@ -66,12 +76,12 @@ bool Core::load( string title )
 
 
 
-Event& Core::getEvent()
+sf::Event& Core::getEvent()
 {
     return event;
 }
 
-RenderWindow*& Core::getWindow()
+sf::RenderWindow*& Core::getWindow()
 {
     return window;
 }
@@ -82,7 +92,7 @@ void Core::clear()
 {
     window->clear( color );
 
-    if( window->getSize() != Vector2u( width, height ) )
+    if( window->getSize() != sf::Vector2u( width, height ) )
     {
         width = window->getSize().x;
         height = window->getSize().y;
@@ -99,7 +109,7 @@ void Core::setVisible( bool visible )
     window->setVisible( visible );
 }
 
-void Core::setColor( Uint8 r, Uint8 g, Uint8 b, Uint8 a )
+void Core::setColor( sf::Uint8 r, sf::Uint8 g, sf::Uint8 b, sf::Uint8 a )
 {
     color.r = r;
     color.g = g;
--- Ninja/core.h
+++ Ninja/core.h
@@ -4,7 +4,6 @@
 #include <SFML/Window/Event.hpp>
 #include <SFML/Graphics/RenderWindow.hpp>
 
-using namespace sf;
 using namespace std;
 
 class Core
@@ -16,27 +15,27 @@ class Core
     unsigned width;
     unsigned height;
 
-    Event event;
-    Color color;
-    RenderWindow* window;
+    sf::Event event;
+    sf::Color color;
+    sf::RenderWindow* window;
 
 public:
 
-    Core( unsigned width = 0, unsigned height = 0, unsigned FPS = 60 );
+    Core( int state, unsigned width = 0, unsigned height = 0, unsigned FPS = 60 );
     ~Core();
 
     void free();
     bool load( string title = "" );
 
-    Event& getEvent();
-    RenderWindow*& getWindow();
+    sf::Event& getEvent();
+    sf::RenderWindow*& getWindow();
 
 
     void clear();
     void display();
 
     void setVisible( bool visible );
-    void setColor( Uint8 r, Uint8 g, Uint8 b, Uint8 a );
+    void setColor( sf::Uint8 r, sf::Uint8 g, sf::Uint8 b, sf::Uint8 a = 0xFF );
 
 
     int& getState();    // get or set state, e.g. from intro to menu
