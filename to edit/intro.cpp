4f20db78695d25094e27dc6100367950bf84e264
Adriqun <adrmic98@gmail.com>
2016-09-07 21:26:33 +0200

    intro doesn't has bool

--- to edit/intro.cpp
+++ to edit/intro.cpp
@@ -29,33 +29,31 @@ Intro::~Intro()
 	
 bool Intro::load( int screen_w, int screen_h )
 {
-	if( !shuriken.load( "intro/shuriken.png" ) )
-		return false;
+	shuriken.load( "intro/shuriken.png" );
 	
 	bg.setAlpha( 255 );
-	if( !bg.create( screen_w, screen_h, 0x15, 0x15, 0x1D ) )
-		return false;
+	bg.create( screen_w, screen_h, 0x15, 0x15, 0x1D );
 		
 	
 	nr = 4;
 	text = new MyText[ nr ];
 	
 	// WHEN NOTHING...
-	if( !text[ 0 ].setFont( "intro/Jaapokki-Regular.otf", 37, 0xFF, 0xFF, 0xFF ) )
-		return false;
-	else if( text[ 0 ].setText( "When nothing makes sense..." ) )
-	{
-		text[ 0 ].setPosition( screen_w/2 - text[ 0 ].getWidth()/2, screen_h/2 - text[ 0 ].getHeight()/2 );
-	}
+	text[ 0 ].setFont( "intro/Jaapokki-Regular.otf", 37, 0xFF, 0xFF, 0xFF );
+
+	text[ 0 ].setText( "When nothing makes sense..." );
+
+	text[ 0 ].setPosition( screen_w/2 - text[ 0 ].getWidth()/2, screen_h/2 - text[ 0 ].getHeight()/2 );
+
 	
 	
 	// NINJA
-	if( !text[ 1 ].setFont( "intro/Jaapokki-Regular.otf", 37, 0x70, 0xB7, 0x59 ) )
-		return false;
-	else if( text[ 1 ].setText( "Ninja" ) )
-	{
-		text[ 1 ].setPosition( screen_w/2 - text[ 1 ].getWidth()/2 - shuriken.getWidth() -10, screen_h/2 - text[ 1 ].getHeight()/2 );
-	}
+	text[ 1 ].setFont( "intro/Jaapokki-Regular.otf", 37, 0x70, 0xB7, 0x59 );
+
+	text[ 1 ].setText( "Ninja" );
+
+	text[ 1 ].setPosition( screen_w/2 - text[ 1 ].getWidth()/2 - shuriken.getWidth() -10, screen_h/2 - text[ 1 ].getHeight()/2 );
+
 	
 	shuriken.setPosition( text[ 1 ].getRight() + 10, screen_h/2 - shuriken.getHeight()/2 +10 );
 	
@@ -63,21 +61,20 @@ bool Intro::load( int screen_w, int screen_h )
 	
 	
 	// ADRIAN MICHALEK
-	if( !text[ 2 ].setFont( "intro/Jaapokki-Regular.otf", 37, 0xFF, 0xFF, 0xFF ) )
-		return false;
-	else if( text[ 2 ].setText( "Adrian Michalek" ) )
-	{
-		text[ 2 ].setPosition( screen_w/2 - text[ 2 ].getWidth()/2, screen_h/2 - text[ 2 ].getHeight()/2 );
-	}
+	text[ 2 ].setFont( "intro/Jaapokki-Regular.otf", 37, 0xFF, 0xFF, 0xFF );
+
+	text[ 2 ].setText( "Adrian Michalek" );
+
+	text[ 2 ].setPosition( screen_w/2 - text[ 2 ].getWidth()/2, screen_h/2 - text[ 2 ].getHeight()/2 );
+
 	
 	
 	// PRODUCED ...
-	if( !text[ 3 ].setFont( "intro/Jaapokki-Regular.otf", 37, 0x70, 0xB7, 0x59 ) )
-		return false;
-	else if( text[ 3 ].setText( "produced by" ) )
-	{
+	text[ 3 ].setFont( "intro/Jaapokki-Regular.otf", 37, 0x70, 0xB7, 0x59 );
+
+	text[ 3 ].setText( "produced by" );
+	
 		text[ 3 ].setPosition( screen_w/2 - text[ 3 ].getWidth()/2, screen_h/2 - text[ 3 ].getHeight()/2 - text[ 2 ].getHeight() -10 );
-	}
 	
 	
 	return true;
