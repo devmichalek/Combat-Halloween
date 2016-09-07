5e928a0da2f7e8da49c55c6d8f955d51211ea2b2
Adriqun <adrmic98@gmail.com>
2016-09-07 20:01:35 +0200

    ready documentation

--- to edit/chunk.h
+++ to edit/chunk.h
@@ -1,22 +1,34 @@
-// Adrian Michalek, 2016.07.10 - class Chunk
+/**
+    chunk.h
+    Purpose: class Chunk to load and play wav files based on SDL_mixer
+
+    @author Adrian Michalek
+    @version 2016.09.07
+	@email adrmic98@gmail.com
+*/
+
 
 #pragma once
-
-#include <SDL2/SDL_mixer.h>
+#include <SDL2/SDL_mixer.h>		// Mix_Chunk, Mix_FreeChunk, Mix_GetError, Mix_VolumeChunk
+#include <iostream>				// string, cerr, <<
 
 class Chunk
 {
+	Uint8 volume;
+	std::string path;
     Mix_Chunk* chunk;
 
 public:
 
     Chunk();
     ~Chunk();
-    void free();
+	void free();	// Delete objects
 
-    bool load( const char* path, int volume = 0 );
+    void load( std::string path, int volume = 0 );
     void play();
 
-    Mix_Chunk* get();
+    inline Mix_Chunk* get();
     void setVolume( int volume );
+	
+	std::ostream& operator <<( std::ostream& s );	// Print path and volume
 };
