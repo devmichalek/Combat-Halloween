f689e7e4f65b38860e8c5ea583c3ae407d9f58e5
devmichalek <devmichalek@gmail.com>
2016-09-07 21:29:35 +0200

    try, throw, catch, cerr

--- to edit/text.h
+++ to edit/text.h
@@ -1,18 +1,27 @@
-#pragma once
+/**
+    text.h
+    Purpose: class MyText to load and draw text
 
+    @author Adrian Michalek
+    @version 2016.09.07
+	@email adrmic98@gmail.com
+*/
+
+
+#pragma once
 #include "SFML/Graphics.hpp"
-#include <string>
+#include <iostream>
 
 using namespace std;
 
 class MyText
 {
-	int state;
 	int w, h;
     int x, y;
 	
 	sf::Font* font;
 	sf::Text* text;
+	
 	int size;
 	
 	int alpha;
@@ -24,18 +33,17 @@ public:
 	~MyText();
 	void free();
 	
-	bool setFont( string path, int size = 1, int r = 0x00, int g = 0x00, int b = 0x00 );
-	bool setText( string line );
+	void setFont( string path, int size = 1, sf::Uint8 r = 0x00, sf::Uint8 g = 0x00, sf::Uint8 b = 0x00 );
+	void setText( string line );
 	
 	void setPosition( float x, float y );
-    void setScale( float s );
+    void setScale( float w, float h );
 
     sf::Text& get();
-	
 	bool checkCollision( int x, int y, int w = 0, int h = 0 );
 	
-	void fadein( int i, int max );
-	void fadeout( int i, int min );
+	void fadein( int i = 1, int max = 255 );
+	void fadeout( int i = 1, int min = 255 );
 	
 	void setColor( sf::Uint8 r = 0x00, sf::Uint8 g = 0x00, sf::Uint8 b = 0x00 );
 	void setSize( int size = 1 );
@@ -43,13 +51,15 @@ public:
 	int getAlpha();
 	void setAlpha( int alpha = 0 );
 	
+	int getX();
+	int getY();
 	
 	int getWidth();
     int getHeight();
+	
     int getLeft();
     int getRight();
+	
     int getTop();
     int getBot();
-	int getX();
-	int getY();
 };
