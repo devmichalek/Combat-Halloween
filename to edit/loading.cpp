3b290a8f127be9319040b5b01c3422ac3c16e92f
Adrian Michalek <adrmic98@gmail.com>
2016-09-11 17:45:10 +0200

    ended loading

--- to edit/loading.cpp
+++ to edit/loading.cpp
@@ -13,35 +13,30 @@ Loading::~Loading()
 	text.free();
 }
 	
-bool Loading::load( int screen_w, int screen_h )
+void Loading::load( int screen_w, int screen_h )
 {
-	bool success = true;
-	
+	text.setID( "loading-text" );
 	text.setFont( "intro/Jaapokki-Regular.otf", 40, 0xFF, 0xFF, 0xFF );
-
 	text.setText( "Loading" );
-
-
 	text.setAlpha( 255 );
 	text.setPosition( screen_w/2 - text.getWidth()/2, screen_h/2 - text.getHeight()/2 );
 
 	
-	
+	background.setID( "loading-background" );
 	background.setAlpha( 255 );
 	background.create( screen_w, screen_h, 0x15, 0x15, 0x1D );
 	
 	
+	white_bar.setID( "loading-white_bar" );
 	white_bar.setAlpha( 255 );
 	white_bar.create( 300, 50, 0xFF, 0xFF, 0xFF );
 	white_bar.setPosition( screen_w/2 - white_bar.getWidth()/2, text.getBot() +15 );
 
 	
+	green_bar.setID( "loading-green_bar" );
 	green_bar.setAlpha( 255 );
 	green_bar.create( 1, 50, 0x70, 0xB7, 0x59 );
 	green_bar.setPosition( screen_w/2 - white_bar.getWidth()/2, text.getBot() +15 );
-	
-		
-	return success;
 }
 
 void Loading::draw( sf::RenderWindow* &window, int range )
