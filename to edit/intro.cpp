62547c9f055d5338b967216066a76642ef1a87b0
Adrian Michalek <adrmic98@gmail.com>
2016-09-11 17:44:57 +0200

    ready to show

--- to edit/intro.cpp
+++ to edit/intro.cpp
@@ -27,10 +27,12 @@ Intro::~Intro()
 }
 
 	
-bool Intro::load( int screen_w, int screen_h )
+void Intro::load( int screen_w, int screen_h )
 {
+	shuriken.setID( "intro-shuriken" ); // name of class and name of object
 	shuriken.load( "intro/shuriken.png" );
 	
+	bg.setID( "intro-background" );
 	bg.setAlpha( 255 );
 	bg.create( screen_w, screen_h, 0x15, 0x15, 0x1D );
 		
@@ -39,45 +41,36 @@ bool Intro::load( int screen_w, int screen_h )
 	text = new MyText[ nr ];
 	
 	// WHEN NOTHING...
+	text[ 0 ].setID( "intro-text[0]" );
 	text[ 0 ].setFont( "intro/Jaapokki-Regular.otf", 37, 0xFF, 0xFF, 0xFF );
-
 	text[ 0 ].setText( "When nothing makes sense..." );
-
 	text[ 0 ].setPosition( screen_w/2 - text[ 0 ].getWidth()/2, screen_h/2 - text[ 0 ].getHeight()/2 );
 
 	
 	
 	// NINJA
+	text[ 1 ].setID( "intro-text[1]" );
 	text[ 1 ].setFont( "intro/Jaapokki-Regular.otf", 37, 0x70, 0xB7, 0x59 );
-
 	text[ 1 ].setText( "Ninja" );
-
 	text[ 1 ].setPosition( screen_w/2 - text[ 1 ].getWidth()/2 - shuriken.getWidth() -10, screen_h/2 - text[ 1 ].getHeight()/2 );
-
-	
 	shuriken.setPosition( text[ 1 ].getRight() + 10, screen_h/2 - shuriken.getHeight()/2 +10 );
 	
 	
 	
 	
 	// ADRIAN MICHALEK
+	text[ 2 ].setID( "intro-text[2]" );
 	text[ 2 ].setFont( "intro/Jaapokki-Regular.otf", 37, 0xFF, 0xFF, 0xFF );
-
 	text[ 2 ].setText( "Adrian Michalek" );
-
 	text[ 2 ].setPosition( screen_w/2 - text[ 2 ].getWidth()/2, screen_h/2 - text[ 2 ].getHeight()/2 );
 
 	
 	
 	// PRODUCED ...
+	text[ 3 ].setID( "intro-text[3]" );
 	text[ 3 ].setFont( "intro/Jaapokki-Regular.otf", 37, 0x70, 0xB7, 0x59 );
-
 	text[ 3 ].setText( "produced by" );
-	
-		text[ 3 ].setPosition( screen_w/2 - text[ 3 ].getWidth()/2, screen_h/2 - text[ 3 ].getHeight()/2 - text[ 2 ].getHeight() -10 );
-	
-	
-	return true;
+	text[ 3 ].setPosition( screen_w/2 - text[ 3 ].getWidth()/2, screen_h/2 - text[ 3 ].getHeight()/2 - text[ 2 ].getHeight() -10 );
 }
 
 void Intro::draw( sf::RenderWindow* &window )
