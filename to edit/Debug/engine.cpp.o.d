b40153ae40eab400fe1830b7409e9cc73f78cf90
devmichalek <devmichalek@gmail.com>
2016-09-13 22:58:29 +0200

    .o.cpp .o update

--- to edit/Debug/engine.cpp.o.d
+++ to edit/Debug/engine.cpp.o.d
@@ -1,33 +1,37 @@
-Debug/engine.cpp.o: engine.cpp engine.h core.h loading.h sprite.h text.h \
- intro.h link_button.h chunk.h menu_play_button.h menu_title.h \
- menu_music_button.h menu_log.h menu_exit_log.h music.h game_timer.h
+Debug/engine.cpp.o: engine.cpp engine.h core.h start/loading.h \
+ templates/sprite.h templates/text.h start/intro.h menu/link_button.h \
+ templates/chunk.h menu/menu_play_button.h menu/menu_title.h \
+ menu/menu_music_button.h menu/menu_log.h menu/menu_exit_log.h \
+ menu/music_volume.h templates/music.h play/game_timer.h
 
 engine.h:
 
 core.h:
 
-loading.h:
+start/loading.h:
 
-sprite.h:
+templates/sprite.h:
 
-text.h:
+templates/text.h:
 
-intro.h:
+start/intro.h:
 
-link_button.h:
+menu/link_button.h:
 
-chunk.h:
+templates/chunk.h:
 
-menu_play_button.h:
+menu/menu_play_button.h:
 
-menu_title.h:
+menu/menu_title.h:
 
-menu_music_button.h:
+menu/menu_music_button.h:
 
-menu_log.h:
+menu/menu_log.h:
 
-menu_exit_log.h:
+menu/menu_exit_log.h:
 
-music.h:
+menu/music_volume.h:
 
-game_timer.h:
+templates/music.h:
+
+play/game_timer.h:
