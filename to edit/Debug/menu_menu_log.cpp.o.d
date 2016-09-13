db939a2c6bd7cec84b0e950b797b1bccfe4165a3
devmichalek <devmichalek@gmail.com>
2016-09-13 22:58:29 +0200

    .o.cpp .o update

--- to edit/Debug/menu_menu_log.cpp.o.d
+++ to edit/Debug/menu_menu_log.cpp.o.d
@@ -1,10 +1,10 @@
-Debug/menu_menu_log.cpp.o: menu/menu_log.cpp menu/menu_log.h sprite.h \
- text.h chunk.h
+Debug/menu_menu_log.cpp.o: menu/menu_log.cpp menu/menu_log.h \
+ templates/sprite.h templates/text.h templates/chunk.h
 
 menu/menu_log.h:
 
-sprite.h:
+templates/sprite.h:
 
-text.h:
+templates/text.h:
 
-chunk.h:
+templates/chunk.h:
