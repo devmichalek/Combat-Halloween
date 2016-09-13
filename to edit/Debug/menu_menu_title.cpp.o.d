969987bdb95beb42383f728edececce47ab21fd8
Adrian Michalek <adrmic98@gmail.com>
2016-09-13 22:58:29 +0200

    .o.cpp .o update

--- to edit/Debug/menu_menu_title.cpp.o.d
+++ to edit/Debug/menu_menu_title.cpp.o.d
@@ -1,8 +1,8 @@
 Debug/menu_menu_title.cpp.o: menu/menu_title.cpp menu/menu_title.h \
- sprite.h text.h
+ templates/sprite.h templates/text.h
 
 menu/menu_title.h:
 
-sprite.h:
+templates/sprite.h:
 
-text.h:
+templates/text.h:
