1684ca87f8a7762ccd17a5aa55d9170f709de7f6
Adrian Michalek <adrmic98@gmail.com>
2016-09-14 15:32:36 +0200

    sprite

--- to edit/templates/sprite.h
+++ to edit/templates/sprite.h
@@ -17,7 +17,6 @@ class MySprite
 {
 	string ID;				// Identity
 	
-	
 	sf::IntRect rect;		// Rectangle
 	sf::Color color;		// Color
 	
@@ -34,39 +33,42 @@ public:
     ~MySprite();
     void free();
 	
-	#ifdef _WIN32
-	void setColor( int i );
-	#endif
+	
 	
     void load( string path, int nr = 0 );
-	void create( int w, int h, sf::Uint8 r = 0x00, sf::Uint8 g = 0x00, sf::Uint8 b = 0x00 );	// Create blank texture
+	void create( int w, int h, sf::Color color );	// Create blank texture
 	
-    void setOffset( int n );
+	
+	
     void setPosition( float x, float y );
 	
-	int getAlpha();
-	void setAlpha( int alpha = 0 );
 	
-	void setColor( sf::Uint8 r = 0x00, sf::Uint8 g = 0x00, sf::Uint8 b = 0x00 );
+	void setColor( sf::Color color );
     void setScale( float s = 1, float z = 1 );
 	
-	sf::Sprite& get();
+	
 	bool checkCollision( int x, int y, int w = 0, int h = 0 );
 	
 	void fadein( int i, int max );
 	void fadeout( int i, int min );
 	
+	
+	sf::Uint8 getAlpha();
+	void setAlpha( sf::Uint8 a = 0 );
+	
 	int getX();
 	int getY();
-	
     int getWidth();
     int getHeight();
 	
     int getLeft();
     int getRight();
-	
     int getTop();
     int getBot();
 	
+	void setOffset( int n );
+	int getOffset();
+	
+	sf::Sprite& get();
 	std::ostream& operator <<( std::ostream& s );	// Print ID, rect, alpha
 };
