228ba9a9c5b43f50e732dcb398bd13507feb2c65
Adrian Michalek <adrmic98@gmail.com>
2016-09-15 16:38:32 +0200

    title in menu state

--- to edit/menu/title.cpp
+++ to edit/menu/title.cpp
@@ -1,49 +1,54 @@
-#include "menu_title.h"
+#include "title.h"
 #include <stdlib.h>
 
-Menu_title::Menu_title()
+Title::Title()
 {
-    // empty
+    belt = NULL;
 }
 
-Menu_title::~Menu_title()
+Title::~Title()
 {
-    belt.free();
+	if( belt != NULL )
+	{
+		delete belt;
+		belt = NULL;
+	}
+
 	title.free();
 }
 
-void Menu_title::load( int screen_w )
+void Title::load( int screen_w )
 {
-	belt.setID( "menu_title-belt" );
-    belt.load( "menu/belt.png" );
-	belt.setPosition( screen_w/2 - belt.getWidth()/2, 10 );
+	belt = new MySprite( "Title-belt" );
+    belt->load( "menu/belt.png" );
+	belt->setPosition( screen_w/2 - belt->getWidth()/2, 10 );
 	
-	title.setID( "menu_title-title" );
+	title.setID( "Title-title" );
 	title.setFont( "menu/KGHAPPY.ttf", 80, 0x7F, 0x99, 0x95 );
 	title.setText( "Ninja" );
 	title.setPosition( screen_w/2 - title.getWidth()/2, 15 );
 }
 
-void Menu_title::draw( sf::RenderWindow &window )
+void Title::draw( sf::RenderWindow &window )
 {
-    window.draw( belt.get() );
+    window.draw( belt->get() );
 	window.draw( title.get() );
 }
 
 
-int Menu_title::getBot()
+int Title::getBot()
 {
-	return belt.getBot();
+	return belt->getBot();
 }
 
-void Menu_title::fadein( int i, int max )
+void Title::fadein( int i, int max )
 {
-	belt.fadein( i, max );
+	belt->fadein( i, max );
 	title.fadein( i, max );
 }
 
-void Menu_title::fadeout( int i, int min )
+void Title::fadeout( int i, int min )
 {
-	belt.fadeout( i, min );
+	belt->fadeout( i, min );
 	title.fadeout( i, min );
 }
