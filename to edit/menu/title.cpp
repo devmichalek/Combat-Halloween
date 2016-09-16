7b22fc6fbcd040a98793d5d48f15ae2fa1f3dd62
devmichalek <devmichalek@gmail.com>
2016-09-16 21:38:33 +0200

    title class

--- to edit/menu/title.cpp
+++ to edit/menu/title.cpp
@@ -3,27 +3,22 @@
 
 Title::Title()
 {
-    belt = NULL;
+    // empty
 }
 
 Title::~Title()
 {
-	if( belt != NULL )
-	{
-		delete belt;
-		belt = NULL;
-	}
-
+	belt.free();
 	title.free();
 }
 
 void Title::load( int screen_w )
 {
-	belt = new MySprite( "Title-belt" );
-    belt->load( "menu/belt.png" );
-	belt->setPosition( screen_w/2 - belt->getWidth()/2, 10 );
+	belt.setName( "title-belt" );
+    belt.load( "menu/belt.png" );
+	belt.setPosition( screen_w/2 - belt.getWidth()/2, 10 );
 	
-	title.setID( "Title-title" );
+	title.setID( "title-title" );
 	title.setFont( "menu/KGHAPPY.ttf", 80, 0x7F, 0x99, 0x95 );
 	title.setText( "Ninja" );
 	title.setPosition( screen_w/2 - title.getWidth()/2, 15 );
@@ -31,24 +26,24 @@ void Title::load( int screen_w )
 
 void Title::draw( sf::RenderWindow &window )
 {
-    window.draw( belt->get() );
+    window.draw( belt.get() );
 	window.draw( title.get() );
 }
 
 
 int Title::getBot()
 {
-	return belt->getBot();
+	return belt.getBot();
 }
 
 void Title::fadein( int i, int max )
 {
-	belt->fadein( i, max );
+	belt.fadein( i, max );
 	title.fadein( i, max );
 }
 
 void Title::fadeout( int i, int min )
 {
-	belt->fadeout( i, min );
+	belt.fadeout( i, min );
 	title.fadeout( i, min );
 }
