179be70742bd696bd1e89428ab4d987964534f68
devmichalek <devmichalek@gmail.com>
2016-09-24 16:54:27 +0200

    new progress bar system

--- to edit/loading/loading.cpp
+++ to edit/loading/loading.cpp
@@ -3,51 +3,57 @@
 Loading::Loading()
 {
 	state = 0;
+	counter = 0;
 }
 
 Loading::~Loading()
 {
 	text.free();
-	
 	background.free();
-	white_bar.free();
-	green_bar.free();
+	
+	progress_bar.free();
+	counter = 0;
 }
 	
 void Loading::load( int screen_w, int screen_h )
 {
 	text.setID( "loading-text" );
 	text.setFont( "intro/Jaapokki-Regular.otf", 40, 0xFF, 0xFF, 0xFF );
-	text.setText( "Loading" );
+	text.setText(  "Loading " + to_string( state ) + "%"  );
 	text.setAlpha( 255 );
 	text.center( screen_w, screen_h );
 
 	
 	background.setName( "loading-background" );
-	background.create( screen_w, screen_h, sf::Color( 0x15, 0x15, 0x1D ) );
+	background.create( screen_w, screen_h );
+	background.setColor( sf::Color( 0x15, 0x15, 0x1D ) );
 	background.setAlpha( 255 );
 	
 
-	white_bar.setName( "loading-white_bar" );
-	white_bar.create( 300, 50, sf::Color( 0xFF, 0xFF, 0xFF ) );
-	white_bar.setAlpha( 255 );
-	white_bar.setPosition( screen_w/2 - white_bar.getWidth()/2, text.getBot() +15 );
-
-
-	green_bar.setName( "loading-green_bar" );
-	green_bar.create( 1, 50, sf::Color( 0x70, 0xB7, 0x59 ) );
-	green_bar.setAlpha( 255 );
-	green_bar.setPosition( screen_w/2 - white_bar.getWidth()/2, text.getBot() +15 );
+	progress_bar.setName( "loading-progress_bar" );
+	progress_bar.load( "loading/progress bar.png", 8 );
+	progress_bar.setAlpha( 255 );
+	progress_bar.setPosition( screen_w/2 - progress_bar.getWidth()/2, text.getBot() +15 );
 }
 
 void Loading::draw( sf::RenderWindow* &window, int range )
 {
-	green_bar.setScale( white_bar.getWidth() *state/range );
 	window->draw( background.get() );
-	window->draw( white_bar.get() );
-	window->draw( green_bar.get() );
+	
 	window->draw( text.get() );
+	text.setText( "Loading " + to_string( state ) + "%" );
+	text.reloadPosition();
+	
 	state ++;
+	
+	progress_bar.setOffset( counter /5 );
+	window->draw( progress_bar.get() );
+	
+	counter++;
+	if( counter == 8 *5 )
+	{
+		counter = 0;
+	}
 }
 
 int Loading::getState()
