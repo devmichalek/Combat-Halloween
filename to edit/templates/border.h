daba01c0d905c9af2a837b98272be7246813b4e4
devmichalek <devmichalek@gmail.com>
2016-09-25 10:42:43 +0200

    rect class - own

---
+++ to edit/templates/border.h
@@ -0,0 +1,26 @@
+#pragma once
+
+class Border
+{
+	
+protected:
+	int width;
+	int height;
+	int left;
+	int top;
+	
+	
+public:
+
+	void set( int x, int y, int w, int h );
+	
+	int getX();
+	int getY();
+    int getWidth();
+    int getHeight();
+	
+    int getLeft();
+    int getRight();
+    int getTop();
+    int getBot();
+};
