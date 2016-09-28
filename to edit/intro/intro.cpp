0488cc984d252daa674829aaf47db63b87b59808
devmichalek <devmichalek@gmail.com>
2016-09-28 11:42:18 +0200

    set colors, font etc. from txt file

--- to edit/intro/intro.cpp
+++ to edit/intro/intro.cpp
@@ -1,4 +1,5 @@
 #include "intro.h"
+#include "templates/file.h"
 
 Intro::Intro()
 {
@@ -29,13 +30,16 @@ Intro::~Intro()
 	
 void Intro::load( const int &screen_w, const int &screen_h )
 {
+	File file;
+	file.load( "data/intro_settings.txt" );
+	
 	shuriken.setName( "intro-shuriken" );
-	shuriken.load( "intro/shuriken.png" );
+	shuriken.load( file.s( 0 ) );
 	
 	bg.setName( "intro-background" );
 	bg.create( screen_w, screen_h );
-	bg.setColor( sf::Color( 0x15, 0x15, 0x1D ) );
-	bg.setAlpha( 0xFF );
+	bg.setColor( sf::Color( file.i(1), file.i(2), file.i(3) ) );
+	bg.setAlpha( file.i(4) );
 		
 	
 	nr = 4;
@@ -43,15 +47,16 @@ void Intro::load( const int &screen_w, const int &screen_h )
 	
 	// WHEN NOTHING...
 	text[ 0 ].setID( "intro-text[0]" );
-	text[ 0 ].setFont( "intro/Jaapokki-Regular.otf", 37, 0xFF, 0xFF, 0xFF );
+	text[ 0 ].setFont( file.s(5), file.i(6), file.i(7), file.i(8), file.i(9) );
 	text[ 0 ].setText( "When nothing makes sense..." );
 	text[ 0 ].center( screen_w, screen_h );
 
-	
+
+
 	
 	// NINJA
 	text[ 1 ].setID( "intro-text[1]" );
-	text[ 1 ].setFont( "intro/Jaapokki-Regular.otf", 37, 0x70, 0xB7, 0x59 );
+	text[ 1 ].setFont( file.s(5), file.i(6), file.i(10), file.i(11), file.i(12) );
 	text[ 1 ].setText( "Ninja" );
 	text[ 1 ].setPosition( screen_w/2 - text[ 1 ].getWidth()/2 - shuriken.getWidth() -10, screen_h/2 - text[ 1 ].getHeight()/2 );
 	shuriken.setPosition( text[ 1 ].getRight() + 10, screen_h/2 - shuriken.getHeight()/2 +10 );
@@ -61,7 +66,7 @@ void Intro::load( const int &screen_w, const int &screen_h )
 	
 	// ADRIAN MICHALEK
 	text[ 2 ].setID( "intro-text[2]" );
-	text[ 2 ].setFont( "intro/Jaapokki-Regular.otf", 37, 0xFF, 0xFF, 0xFF );
+	text[ 2 ].setFont( file.s(5), file.i(6), file.i(7), file.i(8), file.i(9) );
 	text[ 2 ].setText( "Adrian Michalek" );
 	text[ 2 ].center( screen_w, screen_h );
 
@@ -69,9 +74,11 @@ void Intro::load( const int &screen_w, const int &screen_h )
 	
 	// PRODUCED ...
 	text[ 3 ].setID( "intro-text[3]" );
-	text[ 3 ].setFont( "intro/Jaapokki-Regular.otf", 37, 0x70, 0xB7, 0x59 );
+	text[ 3 ].setFont( file.s(5), file.i(6), file.i(10), file.i(11), file.i(12) );
 	text[ 3 ].setText( "produced by" );
 	text[ 3 ].center( screen_w, screen_h, 0, -text[ 2 ].getHeight()-10 );
+	
+	file.close();
 }
 
 void Intro::draw( sf::RenderWindow* &window )
@@ -79,22 +86,22 @@ void Intro::draw( sf::RenderWindow* &window )
 	window->draw( bg.get() );
 	if( text[ 0 ].getAlpha() < 255 )
 	{
-		text[ 0 ].fadein( 3, 255 );
+		text[ 0 ].fadein( 3 );
 		
 		window->draw( text[ 0 ].get() );
 	}
 	else if( text[ 1 ].getAlpha() < 255 )
 	{
-		text[ 1 ].fadein( 3, 255 );
-		shuriken.fadein( 3, 255 );
+		text[ 1 ].fadein( 3 );
+		shuriken.fadein( 3 );
 		
 		window->draw( text[ 1 ].get() );
 		window->draw( shuriken.get() );
 	}
 	else if( text[ 2 ].getAlpha() < 255 )
 	{
-		text[ 2 ].fadein( 3, 255 );
-		text[ 3 ].fadein( 3, 255 );
+		text[ 2 ].fadein( 3 );
+		text[ 3 ].fadein( 3 );
 		
 		window->draw( text[ 2 ].get() );
 		window->draw( text[ 3 ].get() );
