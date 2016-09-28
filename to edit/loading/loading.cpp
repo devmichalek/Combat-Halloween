e4d383ff5fadd301d6a2f7c206640aa486efc5de
devmichalek <devmichalek@gmail.com>
2016-09-28 11:41:39 +0200

    loading path, sizes from txt file

--- to edit/loading/loading.cpp
+++ to edit/loading/loading.cpp
@@ -1,39 +1,54 @@
 #include "loading.h"
+#include "templates/file.h"
 
 Loading::Loading()
 {
 	state = 0;
 	counter = 0;
+	max = 0;
 }
 
 Loading::~Loading()
+{
+	free();
+}
+
+void Loading::free()
 {
 	text.free();
 	background.free();
 	
 	progress_bar.free();
 	counter = 0;
+	max = 0;
 }
 	
 void Loading::load( const int &screen_w, const int &screen_h )
 {
+	File file;
+	file.load( "data/loading_settings.txt" );
+	
 	text.setID( "loading-text" );
-	text.setFont( "intro/Jaapokki-Regular.otf", 40, 0xFF, 0xFF, 0xFF );
-	text.setText(  "Loading " + to_string( state ) + "%"  );
-	text.setAlpha( 255 );
+	
+	text.setFont( file.s(0), file.i(1), file.i(2), file.i(3), file.i(4) );
+	text.setText( "Loading " + to_string( state ) + "%"  );
+	text.setAlpha( file.i(5) );
 	text.center( screen_w, screen_h );
 
 	
+	progress_bar.setName( "loading-progress_bar" );
+	progress_bar.load( file.s(6), file.i(7) );
+	progress_bar.setAlpha( file.i(8) );
+	progress_bar.setPosition( screen_w/2 - progress_bar.getWidth()/2, text.getBot() +15 );
+	
+	
 	background.setName( "loading-background" );
 	background.create( screen_w, screen_h );
-	background.setColor( sf::Color( 0x15, 0x15, 0x1D ) );
-	background.setAlpha( 255 );
+	background.setColor( sf::Color( file.i(9), file.i(10), file.i(11) ) );
+	background.setAlpha( file.i(12) );
 	
-
-	progress_bar.setName( "loading-progress_bar" );
-	progress_bar.load( "loading/progress bar.png", 8 );
-	progress_bar.setAlpha( 255 );
-	progress_bar.setPosition( screen_w/2 - progress_bar.getWidth()/2, text.getBot() +15 );
+	max = file.i(7);
+	file.free();
 }
 
 void Loading::draw( sf::RenderWindow* &window )
@@ -50,7 +65,7 @@ void Loading::draw( sf::RenderWindow* &window )
 	window->draw( progress_bar.get() );
 	
 	counter++;
-	if( counter == 8 *5 )
+	if( counter == max *5 )
 	{
 		counter = 0;
 	}
