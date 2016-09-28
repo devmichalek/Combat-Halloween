fe9e128461335c8e50b9abb7e1c071b1e38ed5db
Adrian Michalek <adrmic98@gmail.com>
2016-09-28 11:46:46 +0200

    new button in select level state

---
+++ to edit/level/backtomenu.cpp
@@ -0,0 +1,67 @@
+#include "backtomenu.h"
+
+
+void Backtomenu::load( int screen_w )
+{
+	button.setName( "link_button-backtomenu" );
+    button.load( "menu/backtomenu.png", 4 );
+
+	if( locked )
+		button.setOffset( 3 );    // locked offset = 3
+		
+	button.setScale( 0.5, 0.5 );
+	button.setPosition( screen_w - button.getWidth() -20, 20 );
+	
+	click.setID( "link_button-backtomenu" );
+	click.load( "menu/click.wav", 50 );
+	
+	state = 0;
+}
+
+void Backtomenu::handle( sf::Event &event )
+{
+	if( !locked && button.getAlpha() == 255 )
+	{
+		int x, y;
+		button.setOffset( 0 );
+
+		if( event.type == sf::Event::MouseMoved )
+		{
+			x = event.mouseMove.x;
+			y = event.mouseMove.y;
+				
+			if( button.checkCollision( x, y ) )
+				button.setOffset( 1 );
+			else
+				focus = false;
+		}
+
+		if( event.type == sf::Event::MouseButtonPressed )
+		{
+			x = event.mouseButton.x;
+			y = event.mouseButton.y;
+				
+			if( button.checkCollision( x, y ) )
+			{
+				button.setOffset( 2 );
+					
+				if( play )
+					click.play();
+						
+				focus = true;
+				state = 1;
+			}
+		}
+			
+		if( event.type == sf::Event::MouseButtonReleased )
+			focus = false;
+			
+		if( focus )
+			button.setOffset( 2 );
+	}
+}
+
+int Backtomenu::getState()
+{
+	return state;
+}
---
+++ to edit/level/backtomenu.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "menu/link_button.h"
+
+class Backtomenu :public Link_button
+{
+	int state;
+	
+public:
+
+	using Link_button::Link_button;
+	
+	void load( int screen_w );
+	void handle( sf::Event &event );
+	int getState();
+};
