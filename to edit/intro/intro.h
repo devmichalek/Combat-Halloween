25a247e48a3b74645a72e4c8af6b92586904910b
Adrian Michalek <adrmic98@gmail.com>
2016-10-11 17:04:31 +0200

    new function: free()

--- to edit/intro/intro.h
+++ to edit/intro/intro.h
@@ -7,7 +7,7 @@ class Intro
 {
 	MySprite bg;		// background
 	MySprite shuriken;	// shuriken image
-	MyText* text;
+	MyText* text;	// some sentences
 	
 	int nr;		// how many text objects
 	bool quit;
@@ -16,8 +16,9 @@ public:
 	
 	Intro();
 	~Intro();
+	void free();
 	
-	void load( const int &screen_w, const int &screen_h );
-	void draw( sf::RenderWindow* &window );
-	bool isQuit();
+	void load( const int &screen_w, const int &screen_h );	// load
+	void draw( sf::RenderWindow* &window );	// render
+	bool isQuit();	// tell us when we can stop rendering intro
 };
