9bbc0ff8b017864cbde785782503a49f1473fb22
Adrian Michalek <adrmic98@gmail.com>
2016-10-12 23:40:36 +0200

    return by const reference

--- to edit/templates/border.h
+++ to edit/templates/border.h
@@ -14,15 +14,16 @@ public:
 
 	void set( int x, int y, int w, int h );
 	
-	int getX();
-	int getY();
-    int getWidth();
-    int getHeight();
+	// accessors
+	const int& getX() const;
+	const int& getY() const;
+    const int& getWidth() const;
+    const int& getHeight() const;
 	
-    int getLeft();
-    int getRight();
-    int getTop();
-    int getBot();
+    const int& getLeft() const;
+    const int getRight() const;
+    const int& getTop() const;
+    const int getBot() const;
 	
 	bool checkCollision( int x, int y, int w = 0, int h = 0 );
 };
