20db5a5fee9f70998b90535bc4dd73b53b4d8c13
Adrian Michalek <adrmic98@gmail.com>
2016-10-14 23:20:57 +0200

    one of four world state

---
+++ to edit/wood/play_wood.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "state.h"
+#include "templates/sprite.h"
+#include "hero/hero.h"
+
+class Play_wood
+{
+	State* state;
+	MySprite* bg;
+	Hero* hero;
+	
+public:
+
+	Play_wood();
+    ~Play_wood();
+    void free();
+	
+    void load( int screen_w, int screen_h );
+
+    void handle( sf::Event &event );
+    void draw( sf::RenderWindow* &window );
+	
+	State* getState();
+	void set( State* state );
+	
+	bool isQuit();
+	bool nextState();
+	bool backToLevel();
+	
+	void reloadMusic();
+};
