42e65b658b973436197c42a0c8afa24c23e3ba25
Adrian Michalek <adrmic98@gmail.com>
2016-10-25 19:43:52 +0200

    new 3 huge objects

--- to edit/engine.h
+++ to edit/engine.h
@@ -7,6 +7,9 @@
 #include "level/levelmenu.h"
 
 #include "wood/play_wood.h"
+#include "halloween/play_halloween.h"
+#include "desert/play_desert.h"
+#include "winter/play_winter.h"
 
 class Engine
 {
@@ -15,7 +18,11 @@ class Engine
 	Intro* intro;
 	Menu* menu;
 	Level_menu* level_menu;
+	
 	Play_wood* play_wood;
+	Play_halloween* play_halloween;
+	Play_desert* play_desert;
+	Play_winter* play_winter;
 
 public:
 
