f1dfe8225059d418fa039ac242079c4a49420dd9
devmichalek <devmichalek@gmail.com>
2016-10-25 19:44:49 +0200

    halloween state

---
+++ to edit/halloween/play_halloween.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "templates/state.h"
+#include "hero/hero.h"
+
+class Play_halloween
+{
+	State* state;
+	Hero* hero;
+	
+public:
+
+	Play_halloween();
+    ~Play_halloween();
+    void free();
+	
+    void load( int screen_w, int screen_h );
+
+    void handle( sf::Event &event );
+    void draw( sf::RenderWindow* &window );
+	
+	State* getState();
+	void set( State* state );
+	
+	bool isQuit();
+	bool nextState();
+	bool backToLevel();
+	
+	void reloadMusic();
+};
