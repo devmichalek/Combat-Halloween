d75afba0134416d06968d71fcaf1c32b03e06946
devmichalek <devmichalek@gmail.com>
2016-11-13 16:34:18 +0100

    the same changes as in winter state

--- to edit/wood/play_wood.cpp
+++ to edit/wood/play_wood.cpp
@@ -51,9 +51,11 @@ void Play_wood::draw( sf::RenderWindow* &window )
 	bg->fadein( 2 );
 	hero->fadein( 2 );
 	
-	if( hero->moveLeft() ) {}
-	else { hero->moveRight(); }
-	hero->jump();
+	if( hero->attack() ) {}
+	else if( hero->jump() ) {}
+	else if( hero->moveLeft() ) {}
+	else if( hero->moveRight() ) {}
+	else hero->idle();
 }
 
 	
