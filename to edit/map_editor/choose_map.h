35676af50ac53c685cbcb1755351204db0607ae9
devmichalek <devmichalek@gmail.com>
2016-11-17 19:28:43 +0100

    which map you want to edit

---
+++ to edit/map_editor/choose_map.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "templates/sprite.h"
+#include "templates/text.h"
+
+class Choose_map
+{
+	int state;
+	
+	int nr;
+	MySprite* sprite;
+	
+	MyText text;
+	
+public:
+
+	Choose_map();
+    ~Choose_map();
+	void free();
+
+    void load( int screen_w );
+    void draw( sf::RenderWindow* &window );
+    void handle( sf::Event &event );
+};
