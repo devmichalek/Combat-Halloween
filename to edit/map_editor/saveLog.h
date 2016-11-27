ee343fdb1089007f2dc3c8aeb6757faa64a934e3
devmichalek <devmichalek@gmail.com>
2016-11-27 20:32:15 +0100

    different colors, new mechanics

--- to edit/map_editor/saveLog.h
+++ to edit/map_editor/saveLog.h
@@ -2,15 +2,26 @@
 
 #include "templates/sprite.h"
 #include "templates/text.h"
+#include <fstream>
 
 class SaveLog
 {
 	MySprite bar;
-	MyText basic;
-	MyText edit;
 	
-	string edit_s;
+	MyText limb;
+	MyText editable;
+	
+	MyText save;
+	MySprite savebar;
+	
+	string name;
+	string path;
 	bool action;
+	
+	fstream file;
+	
+	string data;
+	int platform;
 	
 public:
 	
@@ -21,4 +32,8 @@ public:
     void load( int screen_w, int screen_h, string folder );
     void draw( sf::RenderWindow* &window );
     void handle( sf::Event &event );
+	
+	void saveAsFile();
+	void supplyData( string data );
+	void supplyPlatform( int p );
 };
