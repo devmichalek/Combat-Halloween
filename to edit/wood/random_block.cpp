b981e991373b599e0e79927a95949434de27b8df
devmichalek <devmichalek@gmail.com>
2016-12-03 20:55:42 +0100

    new role-playing game system

--- to edit/wood/random_block.cpp
+++ to edit/wood/random_block.cpp
@@ -1,24 +1,37 @@
 #include "wood/random_block.h"
 
+
+
 bool Random_block::checkCollision( int x, int y, int w, int h )
 {
 	for( unsigned i = 0; i < envelope.size(); i++ )
 	{
-		if( block[ envelope[ i ]->nr ].checkCollision( x, y, w, h ) )
-			return true;
+		if( envelope[ i ]->line )
+		{
+			if( block[ envelope[ i ]->nr ].checkCollision( x, y, w, h ) )
+				return true;
+		}
 	}
 	
 	return false;
 }
-
-
-
 
 Block::Block()
 {
 	nr = x = y = 0;
 	line = false;
 }
+
+Law::Law( int nr, bool law )
+{
+	this->nr = nr;
+	this->law = law;
+}
+
+
+
+
+
 
 Random_block::Random_block()
 {
@@ -48,6 +61,13 @@ void Random_block::free()
 		block = NULL;
 		nr = 0;
 	}
+	
+	for( unsigned i = 0; i < rules_right.size(); i++ )
+	{
+		rules_right[ i ].clear();
+	}
+	
+	rules_right.clear();
 	
 	width = 0;
 	screen_w = 0;
@@ -80,7 +100,9 @@ void Random_block::load( int screen_w, int screen_h, int number )
 		block[ i ].setAlpha( 0xFF );
 	}
 	
+	ruleRightSide();
 	
+	// start
 	positioning();
 }
 
@@ -88,23 +110,152 @@ void Random_block::draw( sf::RenderWindow* &window )
 {
 	for( unsigned i = 0; i < envelope.size(); i++ )
 	{
-		block[ envelope[ i ]->nr ].setPosition( envelope[ i ]->x, envelope[ i ]->y );
-		window->draw( block[ envelope[ i ]->nr ].get() );
+		if( envelope[ i ]->line )
+		{
+			block[ envelope[ i ]->nr ].setPosition( envelope[ i ]->x, envelope[ i ]->y );
+			window->draw( block[ envelope[ i ]->nr ].get() );
+		}
+	}
+}
+
+void Random_block::drawBG( sf::RenderWindow* &window )
+{
+	for( unsigned i = 0; i < envelope.size(); i++ )
+	{
+		if( !envelope[ i ]->line )
+		{
+			block[ envelope[ i ]->nr ].setPosition( envelope[ i ]->x, envelope[ i ]->y );
+			window->draw( block[ envelope[ i ]->nr ].get() );
+		}
 	}
 }
 
 void Random_block::positioning()
 {
 	envelope.push_back( new Block() );
-	
-	// first block
-	envelope[ 0 ]->nr = 0;
+	envelope[ 0 ]->nr = 1;
 	envelope[ 0 ]->x = 0;
-	envelope[ 0 ]->y = screen_h -block[ envelope[ 0 ]->nr ].getHeight();
+	envelope[ 0 ]->y = screen_h -width;
 	envelope[ 0 ]->line = true;
+	
+	envelope.push_back( new Block() );
+	envelope[ 1 ]->nr = 15;
+	envelope[ 1 ]->x = 128;
+	envelope[ 1 ]->y = screen_h -width;
+	envelope[ 1 ]->line = true;
+	
+	envelope.push_back( new Block() );
+	envelope[ 2 ]->nr = 11;
+	envelope[ 2 ]->x = 256;
+	envelope[ 2 ]->y = screen_h -width;
+	envelope[ 2 ]->line = true;
+	
+	envelope.push_back( new Block() );
+	envelope[ 3 ]->nr = 13;
+	envelope[ 3 ]->x = 256;
+	envelope[ 3 ]->y = screen_h -width*2;
+	envelope[ 3 ]->line = true;
+	
+	
 }
 
 int Random_block::getScreenWidth()
 {
 	return screen_w;
+}
+
+void Random_block::ruleRightSide()
+{
+	vector <Law*> temporary;
+	
+	// 0
+	temporary.clear();
+	temporary.push_back( new Law( 1 ) );
+	temporary.push_back( new Law( 2 ) );
+	temporary.push_back( new Law( 3 ) );
+	rules_right.push_back( temporary );
+	
+	// 1
+	temporary.clear();
+	temporary.push_back( new Law( 2 ) );
+	temporary.push_back( new Law( 3 ) );
+	rules_right.push_back( temporary );
+	
+	// 2
+	temporary.clear();
+	temporary.push_back( new Law( -1 ) );	// means that the block can not be put on the right
+	rules_right.push_back( temporary );
+	
+	// 3
+	temporary.clear();
+	temporary.push_back( new Law( 14 ) );
+	rules_right.push_back( temporary );
+	
+	// 4
+	temporary.clear();
+	temporary.push_back( new Law( 1 ) );
+	temporary.push_back( new Law( 2 ) );
+	temporary.push_back( new Law( 3 ) );
+	rules_right.push_back( temporary );
+	
+	// 5
+	temporary.clear();
+	temporary.push_back( new Law( 6 ) );
+	rules_right.push_back( temporary );
+	
+	// 6
+	temporary.clear();
+	temporary.push_back( new Law( 7 ) );
+	rules_right.push_back( temporary );
+	
+	// 7
+	temporary.clear();
+	temporary.push_back( new Law( -1 ) );
+	rules_right.push_back( temporary );
+	
+	// 8
+	temporary.clear();
+	temporary.push_back( new Law( 9 ) );
+	temporary.push_back( new Law( 15 ) );
+	rules_right.push_back( temporary );
+	
+	// 9
+	temporary.clear();
+	temporary.push_back( new Law( -1 ) );
+	rules_right.push_back( temporary );
+	
+	// 10
+	temporary.clear();
+	temporary.push_back( new Law( 11 ) );
+	temporary.push_back( new Law( 12 ) );
+	temporary.push_back( new Law( 13 ) );
+	rules_right.push_back( temporary );
+	
+	// 11
+	temporary.clear();
+	temporary.push_back( new Law( 12 ) );
+	temporary.push_back( new Law( 13 ) );
+	rules_right.push_back( temporary );
+	
+	// 12
+	temporary.clear();
+	temporary.push_back( new Law( -1 ) );
+	rules_right.push_back( temporary );
+	
+	// 13
+	temporary.clear();
+	temporary.push_back( new Law( 14 ) );
+	rules_right.push_back( temporary );
+	
+	// 14
+	temporary.clear();
+	temporary.push_back( new Law( 11 ) );
+	temporary.push_back( new Law( 12 ) );
+	temporary.push_back( new Law( 13 ) );
+	rules_right.push_back( temporary );
+	
+	// 15
+	temporary.clear();
+	temporary.push_back( new Law( 9 ) );
+	rules_right.push_back( temporary );
 }
--- to edit/wood/random_block.h
+++ to edit/wood/random_block.h
@@ -13,10 +13,21 @@ public:
 	Block();
 };
 
+class Law
+{
+public:
+	int nr;
+	bool law;
+	
+	Law( int nr, bool law = true );
+};
+
 class Random_block
 {
 	int nr;
 	MySprite* block;
+	
+	vector < vector <Law*> > rules_right;
 	
 	int width;
 	int screen_w;
@@ -32,8 +43,14 @@ public:
 	
 	void load( int screen_w, int screen_h, int number );
 	void draw( sf::RenderWindow* &window );
+	void drawBG( sf::RenderWindow* &window );
 	void positioning();
 	
 	bool checkCollision( int x, int y, int w, int h );
 	int getScreenWidth();
+	
+	void fadein( int v = 1, int max = 255 );
+	void fadeout( int v = 1, int min = 0 );
+	
+	void ruleRightSide();
 };
