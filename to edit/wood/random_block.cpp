064c498b95e8e4a411e2f2f1b02d7a458bbbcb89
devmichalek <devmichalek@gmail.com>
2016-12-04 01:20:05 +0100

    collision system repaired - set XY before checking XY

--- to edit/wood/random_block.cpp
+++ to edit/wood/random_block.cpp
@@ -8,29 +8,16 @@ bool Random_block::checkCollision( int x, int y, int w, int h )
 	{
 		if( envelope[ i ]->line )
 		{
+			block[ envelope[ i ]->nr ].setPosition( envelope[ i ]->x, envelope[ i ]->y );
 			if( block[ envelope[ i ]->nr ].checkCollision( x, y, w, h ) )
+			{
 				return true;
+			}
 		}
 	}
 	
 	return false;
 }
-
-Block::Block()
-{
-	nr = x = y = 0;
-	line = false;
-}
-
-Law::Law( int nr, bool law )
-{
-	this->nr = nr;
-	this->law = law;
-}
-
-
-
-
 
 
 Random_block::Random_block()
@@ -66,8 +53,29 @@ void Random_block::free()
 	{
 		rules_right[ i ].clear();
 	}
-	
 	rules_right.clear();
+	
+	
+	for( unsigned i = 0; i < rules_left.size(); i++ )
+	{
+		rules_left[ i ].clear();
+	}
+	rules_left.clear();
+	
+	
+	for( unsigned i = 0; i < rules_bot.size(); i++ )
+	{
+		rules_bot[ i ].clear();
+	}
+	rules_bot.clear();
+	
+	
+	for( unsigned i = 0; i < rules_top.size(); i++ )
+	{
+		rules_top[ i ].clear();
+	}
+	rules_top.clear();
+	
 	
 	width = 0;
 	screen_w = 0;
@@ -133,30 +141,39 @@ void Random_block::drawBG( sf::RenderWindow* &window )
 void Random_block::positioning()
 {
 	envelope.push_back( new Block() );
-	envelope[ 0 ]->nr = 1;
+	envelope[ 0 ]->nr = 2;
 	envelope[ 0 ]->x = 0;
 	envelope[ 0 ]->y = screen_h -width;
 	envelope[ 0 ]->line = true;
 	
+	
+	
 	envelope.push_back( new Block() );
-	envelope[ 1 ]->nr = 15;
+	envelope[ 1 ]->nr = 10;
 	envelope[ 1 ]->x = 128;
-	envelope[ 1 ]->y = screen_h -width;
+	envelope[ 1 ]->y = screen_h -width*2;
 	envelope[ 1 ]->line = true;
 	
 	envelope.push_back( new Block() );
-	envelope[ 2 ]->nr = 11;
-	envelope[ 2 ]->x = 256;
+	envelope[ 2 ]->nr = 14;
+	envelope[ 2 ]->x = 128;
 	envelope[ 2 ]->y = screen_h -width;
 	envelope[ 2 ]->line = true;
 	
+	
+	
 	envelope.push_back( new Block() );
-	envelope[ 3 ]->nr = 13;
+	envelope[ 3 ]->nr = 10;
 	envelope[ 3 ]->x = 256;
 	envelope[ 3 ]->y = screen_h -width*2;
 	envelope[ 3 ]->line = true;
 	
 	
+	envelope.push_back( new Block() );
+	envelope[ 4 ]->nr = 10;
+	envelope[ 4 ]->x = 256;
+	envelope[ 4 ]->y = screen_h -width;
+	envelope[ 4 ]->line = true;
 }
 
 int Random_block::getScreenWidth()
@@ -177,6 +194,7 @@ void Random_block::ruleRightSide()
 	
 	// 1
 	temporary.clear();
+	temporary.push_back( new Law( 1 ) );
 	temporary.push_back( new Law( 2 ) );
 	temporary.push_back( new Law( 3 ) );
 	rules_right.push_back( temporary );
@@ -205,6 +223,7 @@ void Random_block::ruleRightSide()
 	
 	// 6
 	temporary.clear();
+	temporary.push_back( new Law( 6 ) );
 	temporary.push_back( new Law( 7 ) );
 	rules_right.push_back( temporary );
 	
@@ -233,6 +252,7 @@ void Random_block::ruleRightSide()
 	
 	// 11
 	temporary.clear();
+	temporary.push_back( new Law( 11 ) );
 	temporary.push_back( new Law( 12 ) );
 	temporary.push_back( new Law( 13 ) );
 	rules_right.push_back( temporary );
@@ -256,6 +276,200 @@ void Random_block::ruleRightSide()
 	
 	// 15
 	temporary.clear();
+	temporary.push_back( new Law( 15 ) );
 	temporary.push_back( new Law( 9 ) );
 	rules_right.push_back( temporary );
+}
+
+void Random_block::ruleLeftSide()
+{
+	vector <Law*> temporary;
+	
+	// 0
+	temporary.clear();
+	temporary.push_back( new Law( -1 ) );
+	rules_left.push_back( temporary );
+	
+	// 1
+	temporary.clear();
+	temporary.push_back( new Law( 0 ) );
+	temporary.push_back( new Law( 1 ) );
+	temporary.push_back( new Law( 4 ) );
+	rules_left.push_back( temporary );
+	
+	// 2
+	temporary.clear();
+	temporary.push_back( new Law( 0 ) );
+	temporary.push_back( new Law( 1 ) );
+	temporary.push_back( new Law( 4 ) );
+	rules_left.push_back( temporary );
+	
+	// 3
+	temporary.clear();
+	temporary.push_back( new Law( 0 ) );
+	temporary.push_back( new Law( 1 ) );
+	temporary.push_back( new Law( 4 ) );
+	rules_left.push_back( temporary );
+	
+	// 4
+	temporary.clear();
+	temporary.push_back( new Law( 13 ) );
+	rules_left.push_back( temporary );
+	
+	// 5
+	temporary.clear();
+	temporary.push_back( new Law( -1 ) );
+	rules_left.push_back( temporary );
+	
+	// 6
+	temporary.clear();
+	temporary.push_back( new Law( 6 ) );
+	temporary.push_back( new Law( 5 ) );
+	rules_left.push_back( temporary );
+	
+	// 7
+	temporary.clear();
+	temporary.push_back( new Law( 5 ) );
+	temporary.push_back( new Law( 7 ) );
+	rules_left.push_back( temporary );
+	
+	// 8
+	temporary.clear();
+	temporary.push_back( new Law( -1 ) );
+	rules_left.push_back( temporary );
+	
+	// 9
+	temporary.clear();
+	temporary.push_back( new Law( 8 ) );
+	temporary.push_back( new Law( 15 ) );
+	rules_left.push_back( temporary );
+	
+	// 10
+	temporary.clear();
+	temporary.push_back( new Law( -1 ) );
+	rules_left.push_back( temporary );
+	
+	// 11
+	temporary.clear();
+	temporary.push_back( new Law( 10 ) );
+	temporary.push_back( new Law( 11 ) );
+	temporary.push_back( new Law( 14 ) );
+	rules_left.push_back( temporary );
+	
+	// 12
+	temporary.clear();
+	temporary.push_back( new Law( 10 ) );
+	temporary.push_back( new Law( 11 ) );
+	temporary.push_back( new Law( 14 ) );
+	rules_left.push_back( temporary );
+	
+	// 13
+	temporary.clear();
+	temporary.push_back( new Law( 10 ) );
+	temporary.push_back( new Law( 11 ) );
+	temporary.push_back( new Law( 14 ) );
+	rules_left.push_back( temporary );
+	
+	// 14
+	temporary.clear();
+	temporary.push_back( new Law( 13 ) );
+	rules_left.push_back( temporary );
+	
+	// 15
+	temporary.clear();
+	temporary.push_back( new Law( 8 ) );
+	temporary.push_back( new Law( 15 ) );
+	rules_left.push_back( temporary );
+}
+
+void Random_block::ruleBotSide()
+{
+	vector <Law*> temporary;
+	
+	// 0
+	temporary.clear();
+	temporary.push_back( new Law( 8 ) );
+	temporary.push_back( new Law( 10 ) );
+	temporary.push_back( new Law( 14 ) );
+	rules_bot.push_back( temporary );
+	
+	// 1
+	temporary.clear();
+	temporary.push_back( new Law( 11 ) );
+	temporary.push_back( new Law( 15 ) );
+	rules_bot.push_back( temporary );
+	
+	// 2
+	temporary.clear();
+	temporary.push_back( new Law( 9 ) );
+	temporary.push_back( new Law( 12 ) );
+	temporary.push_back( new Law( 13 ) );
+	rules_bot.push_back( temporary );
+	
+	// 3
+	temporary.clear();
+	temporary.push_back( new Law( 11 ) );
+	temporary.push_back( new Law( 15 ) );
+	rules_bot.push_back( temporary );
+	
+	// 4
+	temporary.clear();
+	temporary.push_back( new Law( 11 ) );
+	temporary.push_back( new Law( 15 ) );
+	rules_bot.push_back( temporary );
+	
+	// 5-9
+	for( int i = 0; i < 5; i++ )
+	{
+		temporary.clear();
+		temporary.push_back( new Law( -1 ) );
+		rules_bot.push_back( temporary );
+	}
+	
+	
+	// 10
+	temporary.clear();
+	temporary.push_back( new Law( 10 ) );
+	temporary.push_back( new Law( 14 ) );
+	rules_bot.push_back( temporary );
+	
+	// 11
+	temporary.clear();
+	temporary.push_back( new Law( 11 ) );
+	temporary.push_back( new Law( 15 ) );
+	rules_bot.push_back( temporary );
+	
+	// 12
+	temporary.clear();
+	temporary.push_back( new Law( 12 ) );
+	temporary.push_back( new Law( 13 ) );
+	rules_bot.push_back( temporary );
+	
+	// 13
+	temporary.clear();
+	temporary.push_back( new Law( 11 ) );
+	temporary.push_back( new Law( 15 ) );
+	rules_bot.push_back( temporary );
+	
+	// 14
+	temporary.clear();
+	temporary.push_back( new Law( 11 ) );
+	temporary.push_back( new Law( 15 ) );
+	rules_bot.push_back( temporary );
+	
+	// 15
+	temporary.clear();
+	temporary.push_back( new Law( -1 ) );
+	rules_bot.push_back( temporary );
+}
+
+void Random_block::ruleTopSide()
+{
+	vector <Law*> temporary;
+	
+	// 0
+	temporary.clear();
+	temporary.push_back( new Law( -1 ) );
+	rules_top.push_back( temporary );
+	
 }
