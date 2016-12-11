29390a1c969d5fb624529c3a41c8e2f492803a87
devmichalek <devmichalek@gmail.com>
2016-12-11 02:41:16 +0100

    climg function

--- to edit/wood/play_wood.cpp
+++ to edit/wood/play_wood.cpp
@@ -6,6 +6,7 @@ Play_wood::Play_wood()
 	hero = new Hero;
 	bg = new Moving_bg;
 	random_block = new Random_block;
+	kunai = new Kunai;
 }
 
 Play_wood::~Play_wood()
@@ -19,6 +20,7 @@ void Play_wood::free()
 	delete hero;
 	delete bg;
 	delete random_block;
+	delete kunai;
 }
 
 	
@@ -26,6 +28,7 @@ void Play_wood::load( int screen_w, int screen_h )
 {
 	bg->load( "data/sprites/play/1.png" );
 	random_block->load( screen_w, screen_h, 1 );
+	kunai->load();
 }
 
 void Play_wood::setHero( int screen_w, int screen_h, int type )
@@ -48,15 +51,6 @@ void Play_wood::handle( sf::Event &event )
 
 void Play_wood::draw( sf::RenderWindow* &window )
 {
-	bg->fadein( 2 );
-	hero->fadein( 2 );
-	
-	bg->draw( window );
-	
-	random_block->drawBG( window );
-	hero->draw( window );
-	random_block->draw( window );
-	
 	// Y
 	hero->gravitation();
 	if( random_block->checkCollision( hero->getX(), hero->getY(), hero->getW(), hero->getH() ) )
@@ -68,16 +62,72 @@ void Play_wood::draw( sf::RenderWindow* &window )
 		hero->gliding();
 	}
 	
+	
+	// destroy kunai
+	for( unsigned i = 0; i < kunai->getNr(); i++ )
+	{
+		if( random_block->checkCollision( kunai->getX( i ), kunai->getY( i ), kunai->getW(), kunai->getH() ) ||
+		kunai->getX( i ) + kunai->getW()> random_block->getScreenWidth() ||
+		kunai->getX( i ) < 0 )
+		{
+			kunai->destroy( i );
+		}
+	}
+	
 
 	// X
 	hero->sliding();
-	if( hero->jumpAttack() )
+	
+	if( hero->climbing() )
+	{
+		if( random_block->checkLadder( hero->getX(), hero->getY(), hero->getW(), hero->getH() ) )
+		{
+			if( !random_block->checkCollision( hero->getX(), hero->getY(), hero->getW(), hero->getH() ) )
+			{
+				hero->newPosition();
+			}
+			
+			hero->allowClimbing();
+		}
+	}
+	
+	if( random_block->checkLadder( hero->getX(), hero->getY(), hero->getW(), hero->getH() ) )
+	{
+		hero->goUpAndDown();
+	}
+	else
+	{
+		hero->banClimbing();
+	}
+	
+	if( hero->jumpThrow() )
+	{
+		if( hero->throwed() )
+		{
+			kunai->throwing( hero->getThrowX(), hero->getThrowY(), hero->getThrowVel() );
+		}
+		
+		if( random_block->checkCollision( hero->getX(), hero->getY(), hero->getW(), hero->getH() ) ||
+			hero->getX() + hero->getW()> random_block->getScreenWidth() ||
+			hero->getX() < 0 )
+		{
+			hero->undoJump();
+		}
+	}
+	else if( hero->jumpAttack() )
 	{
 		if( random_block->checkCollision( hero->getX(), hero->getY(), hero->getW(), hero->getH() ) ||
 			hero->getX() + hero->getW()> random_block->getScreenWidth() ||
 			hero->getX() < 0 )
 		{
 			hero->undoJump();
+		}
+	}
+	else if( hero->throwing() )
+	{
+		if( hero->throwed() )
+		{
+			kunai->throwing( hero->getThrowX(), hero->getThrowY(), hero->getThrowVel() );
 		}
 	}
 	else if( hero->attack() ) {}
@@ -103,6 +153,19 @@ void Play_wood::draw( sf::RenderWindow* &window )
 	{
 		hero->idle();
 	}
+	
+	bg->move( hero->getDirection() );
+	
+	bg->fadein( 2 );
+	random_block->fadein( 2 );
+	hero->fadein( 2 );
+	kunai->fadein( 2 );
+	
+	bg->draw( window );
+	random_block->drawBG( window );
+	hero->draw( window );
+	kunai->draw( window );
+	random_block->draw( window );
 }
 
 	
