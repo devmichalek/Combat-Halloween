bee975f5dd8a32bcee573fee321edd384fd4962d
devmichalek <devmichalek@gmail.com>
2016-12-17 15:19:48 +0100

    law for rules class

---
+++ to edit/world/law.cpp
@@ -0,0 +1,8 @@
+#include "world/law.h"
+
+Law::Law( int nr, bool line, bool bg )
+{
+	this->nr = nr;
+	this->line = line;
+	this->bg = bg;
+}
---
+++ to edit/world/law.h
@@ -0,0 +1,11 @@
+#pragma once
+
+class Law
+{
+public:
+	int nr;
+	bool line;
+	bool bg;
+	
+	Law( int nr, bool line = true, bool bg = false );
+};
