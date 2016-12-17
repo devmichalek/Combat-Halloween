d679c9b52af65828a22378183cf6a69cbcefe027
devmichalek <devmichalek@gmail.com>
2016-12-17 15:21:09 +0100

    rules

---
+++ to edit/world/rules.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "world/law.h"
+#include <vector>
+
+using namespace std;
+
+class Rules
+{
+	vector < vector <Law*> > rules_right;
+	vector < vector <Law*> > rules_left;
+	vector < vector <Law*> > rules_bot;
+	vector < vector <Law*> > rules_top;
+	
+public:
+
+	Rules();
+	~Rules();
+	void free();
+	
+	void ruleRightSide();
+	void ruleLeftSide();
+	void ruleBotSide();
+	void ruleTopSide( int number );
+};
