339a168c8f76d3a33e9e014ef076f41bc105b931
devmichalek <devmichalek@gmail.com>
2016-12-25 23:11:30 +0100

    set top rules as unvisible

--- to edit/world/rules.h
+++ to edit/world/rules.h
@@ -10,7 +10,7 @@ class Rules
 	vector < vector <Law*> > rules_right;
 	vector < vector <Law*> > rules_left;
 	vector < vector <Law*> > rules_bot;
-	vector < vector <Law*> > rules_top;
+	// vector < vector <Law*> > rules_top;
 	
 public:
 	Rules();
@@ -20,7 +20,7 @@ public:
 	void ruleRightSide();
 	void ruleLeftSide();
 	void ruleBotSide();
-	void ruleTopSide( int number );
+	// void ruleTopSide( int number );
 	
 	vector <Law*> getRightRules( int which );
 };
