e1031657fabf7d67fbeaf45ad9ac192851480b16
devmichalek <devmichalek@gmail.com>
2016-12-26 23:44:37 +0100

    new kind of setting XY

--- to edit/world/moving_bg.cpp
+++ to edit/world/moving_bg.cpp
@@ -2,9 +2,11 @@
 
 Moving_bg::Moving_bg()
 {
+	screen_w = 0;
+	screen_h = 0;
+	
 	vel = 0;
-	direction = 0;
-	x1 = x2 = 0;
+	x = y = 0;
 }
 
 Moving_bg::~Moving_bg()
@@ -14,93 +16,68 @@ Moving_bg::~Moving_bg()
 
 void Moving_bg::free()
 {
-	one.free();
-	two.free();
+	sprite.free();
+	
+	screen_w = 0;
+	screen_h = 0;
 	
 	vel = 0;
-	direction = 0; // none
-	x1 = x2 = 0;
+	x = y = 0;
 }
 	
-void Moving_bg::load( string path )
+	
+	
+void Moving_bg::load( string path, int w, int h )
 {
-	one.setName( "moving_bg-one" );
-	one.load( path );
-	one.setPosition( -one.getWidth()/2, 0 );
-	x1 = -one.getWidth()/2;
+	screen_w = w;
+	screen_h = h;
 	
-	two.setName( "moving_bg-two" );
-	two.load( path );
-	two.setPosition( two.getWidth()/2, 0 );
-	x2 = two.getWidth()/2;
+	printf( "%d %d\n", screen_w, screen_h );
 	
-	vel = 0.10;
+	sprite.setName( "moving_bg-sprite" );
+	sprite.load( path );
+	
+	x = -(sprite.getWidth() -screen_w)*(0 *100 /screen_w) /100;
+	y = -(sprite.getHeight() -screen_h)*((screen_h-238) *100 /screen_h) /100;
+	
+	sprite.setPosition( x, y );
+	
+	vel = 0.1;
 }
 
 void Moving_bg::draw( sf::RenderWindow* &window )
 {
-	window->draw( one.get() );
-	window->draw( two.get() );
-	
-	if( direction == 1 ) // left
-	{
-		if( one.getRight() >= one.getWidth()*2 )
-		{
-			one.setPosition( two.getLeft() -one.getWidth(), 0 );
-			x1 = two.getLeft();
-		}
-		
-		if( two.getRight() >= two.getWidth()*2 )
-		{
-			two.setPosition( one.getLeft() -two.getWidth(), 0 );
-			x2 = one.getLeft();
-		}
-		
-		x1 += vel;
-		x2 += vel;
-		
-		one.setPosition( x1, 0 );
-		two.setPosition( x2, 0 );
-	}
-	
-	else if( direction == 2 ) // right
-	{
-		if( one.getX() <= -one.getWidth() )
-		{
-			one.setPosition( two.getRight(), 0 );
-			x1 = two.getRight();
-		}
-		
-		if( two.getX() <= -two.getWidth() )
-		{
-			two.setPosition( one.getRight(), 0 );
-			x2 = one.getRight();
-		}
-		
-		x1 -= vel;
-		x2 -= vel;
-		
-		one.setPosition( x1, 0 );
-		two.setPosition( x2, 0 );
-	}
-	
-	// printf( "1: %d, 2: %d\n", one.getX(), two.getX() );
-	direction = 0;
+	window->draw( sprite.get() );
 }
 
-void Moving_bg::move( sf::Uint8 direction )
+void Moving_bg::setXY( int posX, int posY )
 {
-	this->direction = direction;
+	int new_x = -(sprite.getWidth() -screen_w)*(posX *100 /screen_w) /100;
+	int new_y = -(sprite.getHeight() -screen_h)*(posY *100 /screen_h) /100;
+	
+	if( x != new_x )
+	{
+		if( x < new_x )	x += vel;
+		else			x -= vel;
+	}
+	
+	if( y != new_y )
+	{
+		if( y < new_y )	y += vel;
+		else			y -= vel;
+	}
+	
+	sprite.setPosition( x, y );
 }
+	
+	
 	
 void Moving_bg::fadein( int v, int max )
 {
-	one.fadein( v, max );
-	two.fadein( v, max );
+	sprite.fadein( v, max );
 }
 
 void Moving_bg::fadeout( int v, int min )
 {
-	one.fadeout( v, min );
-	two.fadeout( v, min );
+	sprite.fadeout( v, min );
 }
