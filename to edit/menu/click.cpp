f7b29cf71cad38bbfc6d1df846a7677247f2d935
devmichalek <devmichalek@gmail.com>
2017-01-07 21:31:57 +0100

    new constructor and destructor

--- to edit/menu/click.cpp
+++ to edit/menu/click.cpp
@@ -1,9 +1,27 @@
 #include "click.h"
 
+Click::Click()
+{
+	focus = false;
+	play = true;
+}
+
+Click::~Click()
+{
+	focus = false;
+	play = true;
+	click.free();
+}
+
 
 void Click::turn()
 {
 	play = !play;
+}
+
+bool Click::isPlayable()
+{
+	return play;
 }
 
 void Click::setVolume( sf::Uint8 volume )
