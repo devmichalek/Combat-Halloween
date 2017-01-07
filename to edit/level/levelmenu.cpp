746597d95f2ee48eaab3cb7548258a9b6e624d4d
devmichalek <devmichalek@gmail.com>
2017-01-07 20:32:39 +0100

    new getResult - getWorldSize()

--- to edit/level/levelmenu.cpp
+++ to edit/level/levelmenu.cpp
@@ -8,6 +8,7 @@ Level_menu::Level_menu()
 	backtomenu = new Backtomenu( "", false );
 	choice = new Choice;
 	character = new Character;
+	worldsize = new Worldsize;
 }
 
 Level_menu::~Level_menu()
@@ -22,6 +23,7 @@ void Level_menu::free()
 	delete backtomenu;
 	delete choice;
 	delete character;
+	delete worldsize;
 }
 
 State* Level_menu::getState()
@@ -39,6 +41,7 @@ void Level_menu::set( State* state )
 	{
 		backtomenu->turn();
 		choice->turn();
+		worldsize->turn();
 	}
 	
 	// Set music volume
@@ -47,6 +50,7 @@ void Level_menu::set( State* state )
 	// Set chunk volume
 	backtomenu->setVolume( state->cVolume );
 	choice->setVolume( state->cVolume );
+	worldsize->setVolume( state->cVolume );
 }
 	
 void Level_menu::load( int screen_w, int screen_h )
@@ -56,6 +60,7 @@ void Level_menu::load( int screen_w, int screen_h )
 	backtomenu->load( screen_w );
 	choice->load( screen_w, screen_h );
 	character->load( screen_w, screen_h );
+	worldsize->load( screen_w, screen_h );
 }
 
 
@@ -64,14 +69,17 @@ void Level_menu::handle( sf::Event &event )
 	if( !choice->isChosen() && backtomenu->getState() == 0 )
 	{
 		choice->handle( event );
+		worldsize->handle( event );
+		backtomenu->handle( event );
 	}
 	else if( !character->nextState() )
 	{
 		if( backtomenu->getState() == 1 )
+		{
 			character->handle( event );
+			backtomenu->handle( event );
+		}
 	}
-	
-	backtomenu->handle( event );
 }
 
 void Level_menu::draw( sf::RenderWindow* &window )
@@ -83,6 +91,7 @@ void Level_menu::draw( sf::RenderWindow* &window )
 	if( choice->isChosen() )
 	{
 		character->move( -20, 0 );
+		worldsize->move( -20, -1000 );
 		if( choice->move( -20, -1000 ) )
 			backtomenu->setState( 1 );
 	}
@@ -90,6 +99,7 @@ void Level_menu::draw( sf::RenderWindow* &window )
 	window->draw( background->get() );
 	backtomenu->draw( *window );
 	choice->draw( *window );
+	worldsize->draw( *window );
 	character->draw( window );
 	
 	if( backtomenu->getState() == 0 )
@@ -98,6 +108,7 @@ void Level_menu::draw( sf::RenderWindow* &window )
 		music->fadein( 1, state->mVolume );
 		backtomenu->fadein( 3, 255 );
 		choice->fadein( 3, 255 );
+		worldsize->fadein( 3, 255 );
 		character->fadein( 3, 255 );
 	}
 	
@@ -107,6 +118,7 @@ void Level_menu::draw( sf::RenderWindow* &window )
 		music->fadeout( 3 );
 		backtomenu->fadeout( 3, 0 );
 		choice->fadeout( 3, 0 );
+		worldsize->fadeout( 3, 0 );
 		character->fadeout( 3, 0 );
 		
 		if( choice->getAlpha() == 0 )
@@ -125,6 +137,7 @@ void Level_menu::draw( sf::RenderWindow* &window )
 			music->fadeout( 3 );
 			backtomenu->fadeout( 3, 0 );
 			choice->fadeout( 3, 0 );
+			worldsize->fadeout( 3, 0 );
 			character->fadeout( 3, 0 );
 			
 			if( choice->getAlpha() == 0 )
@@ -139,6 +152,7 @@ void Level_menu::draw( sf::RenderWindow* &window )
 	else if( backtomenu->getState() == 2 )
 	{
 		character->move( 20, 1000 );
+		worldsize->move( 20, 0 );
 		if( choice->move( 20, 0 ) )
 		{
 			choice->reset();
@@ -174,6 +188,7 @@ bool Level_menu::backToMenu()
 	{
 		state->state = 0;
 		backtomenu->setState( 0 );
+		worldsize->reset();
 		return true;
 	}
 		
@@ -194,4 +209,9 @@ int Level_menu::getMap()
 int Level_menu::getCharacter()
 {
 	return character->getResult();
+}
+
+int Level_menu::getWorldsize()
+{
+	return worldsize->getResult();
 }
