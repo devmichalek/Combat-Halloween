882cd934b61a536c7154444300eb50dc7673ffec
devmichalek <devmichalek@gmail.com>
2017-01-15 00:32:46 +0100

    class Backtomenu - button with function 'back to menu'

--- to edit/03_level/backtomenu.cpp
+++ to edit/03_level/backtomenu.cpp
@@ -1,13 +1,23 @@
-#include "backtomenu.h"
+/**
+    backtomenu.h
+    Purpose: class Backtomenu - button with function "back to menu".
 
+    @author Adrian Michalek
+    @version 2016.12.05
+	@email adrmic98@gmail.com
+*/
+
+#include "backtomenu.h"
 
 void Backtomenu::load( int screen_w )
 {
-	button.setName( "link_button-backtomenu" );
+	button.setName( "backtomenu-backtomenu" );
     button.load( "data/sprites/menu/backtomenu.png", 4 );
 
 	if( locked )
+	{
 		button.setOffset( 3 );    // locked offset = 3
+	}
 		
 	button.setScale( 0.5, 0.5 );
 	button.setPosition( screen_w - button.getWidth() -20, 20 );
@@ -20,54 +30,75 @@ void Backtomenu::load( int screen_w )
 
 void Backtomenu::handle( sf::Event &event )
 {
-	if( !locked && button.getAlpha() == 255 )
+	if( !locked )
 	{
-		int x, y;
-		button.setOffset( 0 );
-
-		if( event.type == sf::Event::MouseMoved )
+		if( button.getAlpha() == 0xFF )
 		{
-			x = event.mouseMove.x;
-			y = event.mouseMove.y;
+			int x, y;
+			button.setOffset( 0 );
+
+			if( event.type == sf::Event::MouseMoved )
+			{
+				x = event.mouseMove.x;
+				y = event.mouseMove.y;
+					
+				if( button.checkCollision( x, y ) )
+				{
+					button.setOffset( 1 );
+				}
+				else
+				{
+					focus = false;
+				}
+			}
+
+			if( event.type == sf::Event::MouseButtonPressed )
+			{
+				x = event.mouseButton.x;
+				y = event.mouseButton.y;
+					
+				if( button.checkCollision( x, y ) )
+				{
+					button.setOffset( 2 );
+						
+					if( play )
+					{
+						click.play();
+					}
+					
+					focus = true;
+					
+					if( state == 1 )
+					{
+						state = 2;
+					}
+					else if( state == 0 )
+					{
+						state = -1;
+					}
+
+				}
+			}
 				
-			if( button.checkCollision( x, y ) )
-				button.setOffset( 1 );
-			else
+			if( event.type == sf::Event::MouseButtonReleased )
+			{
 				focus = false;
-		}
-
-		if( event.type == sf::Event::MouseButtonPressed )
-		{
-			x = event.mouseButton.x;
-			y = event.mouseButton.y;
+			}
 				
-			if( button.checkCollision( x, y ) )
+			if( focus )
 			{
 				button.setOffset( 2 );
-					
-				if( play )
-					click.play();
-						
-				focus = true;
-				
-				if( state == 1 )
-					state = 2;
-				else if( state == 0 )
-					state = -1;
 			}
 		}
-			
-		if( event.type == sf::Event::MouseButtonReleased )
-			focus = false;
-			
-		if( focus )
-			button.setOffset( 2 );
 	}
 }
 
-void Backtomenu::setState( int here )
+
+
+void Backtomenu::setState( int state )
 {
-	state = here;
+	button.setOffset( 0 );
+	this->state = state;
 }
 
 int Backtomenu::getState()
--- to edit/03_level/backtomenu.h
+++ to edit/03_level/backtomenu.h
@@ -1,6 +1,15 @@
+/**
+    backtomenu.h
+    Purpose: class Backtomenu - button with function "back to menu".
+
+    @author Adrian Michalek
+    @version 2016.12.05
+	@email adrmic98@gmail.com
+*/
+
 #pragma once
 
-#include "menu/link_button.h"
+#include "02_menu/link_button.h"
 
 class Backtomenu :public Link_button
 {
@@ -13,6 +22,6 @@ public:
 	void load( int screen_w );
 	void handle( sf::Event &event );
 	
-	void setState( int here );
+	void setState( int state );
 	int getState();
 };
