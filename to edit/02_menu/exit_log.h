a54a2b39601c9a3749513f973708ef9730e040b8
devmichalek <devmichalek@gmail.com>
2017-01-15 00:28:04 +0100

    class Exit_log to show exit state

--- to edit/02_menu/exit_log.h
+++ to edit/02_menu/exit_log.h
@@ -1,3 +1,12 @@
+/**
+    exit_log.h
+    Purpose: class Exit_log to show exit state.
+
+    @author Adrian Michalek
+    @version 2016.10.27
+	@email adrmic98@gmail.com
+*/
+
 #pragma once
 
 #include "play_button.h"
@@ -6,10 +15,12 @@ class Exit_log :public Play_button
 {
 
 public:
+
 	using::Play_button::Play_button;
 	
 	void load( int screen_w, int screen_h );
 	void draw( sf::RenderWindow* &window );
 	void handle( sf::Event &event );
+	
 	int getState();
 };
