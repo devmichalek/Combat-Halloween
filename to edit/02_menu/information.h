2247a6190663add4a9babe1508984d71860babfa
devmichalek <devmichalek@gmail.com>
2017-01-15 00:28:21 +0100

    class Information - to show text during keyboard state

--- to edit/02_menu/information.h
+++ to edit/02_menu/information.h
@@ -1,21 +1,33 @@
+/**
+    information.h
+    Purpose: class Information - to show text during keyboard state.
+
+    @author Adrian Michalek
+    @version 2016.11.02
+	@email adrmic98@gmail.com
+*/
+
 #pragma once
 
-#include "templates/text.h"
+#include "drawable/text.h"
+#include "drawable/sprite.h"
 
 class Information
 {
 	int nr;
 	MyText* text;
 	
+	int info_nr;
+	MyText* info_text;
+	
 public:
 
 	Information();
-	~Information();
-	void free();
-	
-	void load( int screen_h );
+    ~Information();
+
+    void load( int right, int top, int screen_h );
     void draw( sf::RenderWindow &window );
 
-	void fadein( int v = 1, int max = 255 );
-	void fadeout( int v = 1, int min = 0 );
+	void fadein( int j = 1, int max = 255 );
+	void fadeout( int j = 1, int min = 0 );
 };
