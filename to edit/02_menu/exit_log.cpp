360c4e65a10e4c73f1aad6b480f1deec2a54e6e7
devmichalek <devmichalek@gmail.com>
2017-01-15 00:28:04 +0100

    class Exit_log to show exit state

--- to edit/02_menu/exit_log.cpp
+++ to edit/02_menu/exit_log.cpp
@@ -1,46 +1,68 @@
-#include "exit_log.h"
+/**
+    exit_log.h
+    Purpose: class Exit_log to show exit state.
 
+    @author Adrian Michalek
+    @version 2016.10.27
+	@email adrmic98@gmail.com
+*/
+
+#include "exit_log.h"
 
 void Exit_log::load( int screen_w, int screen_h )
 {
-	mySprite.setName( "menu_exit_log-mySprite" );
+	mySprite.setName( "exit_log-mySprite" );
     mySprite.load( "data/sprites/menu/exit.png" );
 	mySprite.setAlpha( 255 );
 	mySprite.center( 0, 0, screen_w, screen_h );
 
-	myText.setName( "menu_exit_log-myText" );
+	myText.setName( "exit_log-myText" );
 	myText.setFont( "data/fonts/BADABB__.TTF", 33, 0xFF, 0xFF, 0xFF );
 	myText.setText( "q-quit  b-back" );
 	myText.setAlpha( 255 );
 	myText.center( screen_w, screen_h, 0, -6 );
 
-	click.setID( "menu_exit_log-click" );
+	click.setID( "exit_log-click" );
 	click.load( "data/sounds/click.wav", 50 );
 }
 
 void Exit_log::handle( sf::Event &event )
 {
 	static bool rel = false;
-	if( event.type == sf::Event::KeyPressed && !rel && state < 2 )
+	
+	if( event.type == sf::Event::KeyPressed && state < 2 )
 	{
-		rel = true;
-		if( event.key.code == sf::Keyboard::Q )
+		if( !rel )
 		{
-			if( play )
-				click.play();
-				
-			if( state == 0 )
-				state = 1;
-			else if( state == 1 )
-				state = 2;
-		}
-		else if( event.key.code == sf::Keyboard::B )
-		{
-			if( state == 1 )
+			rel = true;
+			if( event.key.code == sf::Keyboard::Q )
 			{
 				if( play )
+				{
 					click.play();
-				state = 0;
+				}
+				
+				if( state == 0 )
+				{
+					state = 1;
+				}
+				else if( state == 1 )
+				{
+					state = 2;
+				}
+					
+			}
+			else if( event.key.code == sf::Keyboard::B )
+			{
+				if( state == 1 )
+				{
+					if( play )
+					{
+						click.play();
+					}
+						
+					state = 0;
+				}
 			}
 		}
 	}
@@ -58,6 +80,7 @@ void Exit_log::draw( sf::RenderWindow* &window )
 		window->draw( myText.get() );
 	}
 }
+
 
 
 int Exit_log::getState()
