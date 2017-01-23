1dfd4929806d1949db9b87ae5ea69e6bfcd0536a
devmichalek <devmichalek@gmail.com>
2017-01-23 12:35:43 +0100

    deleted all ladder stuff, deleted vectors, new vector of planks

--- to edit/04_platform/world/brick.h
+++ to edit/04_platform/world/brick.h
@@ -4,54 +4,43 @@
 #include "block.h"
 #include "ladder.h"
 
-
-class Special_ladder
-{
-public:
-	
-	int x, y;
-	bool used;
-	
-	Special_ladder();
-	~Special_ladder();
-};
-
 class Brick
 {
-	int nr;
-	MySprite* block;
-	
-	int ladder_nr;
-	MySprite* ladder;
+	vector <MySprite*> block;
+	vector <Block*> blocks;
+	vector <Block*> bg_blocks;
 	
 	int width;
-	int screen_w, screen_h;
+	int screen_w;
+	int screen_h;
 	
 	int left, right;
-	int world_type;
-	
-	vector < Block* > blocks;
-	vector < Ladder* > ladders;
-	
-	Special_ladder red, white, black;
 	
 	int lastGrass;
 	int fallenX;
 	
 public:
 	
+	Brick();
+	~Brick();
+	void free();
+	
+	void load( int screen_w, int screen_h, int nr, int type );
+	void draw( sf::RenderWindow* &window );
+	
+	void fadein( int v = 1, int max = 0xFF );
+	void fadeout( int v = 1, int min = 0 );
+
+	
+	
 	void addBlock( int chosen, int x_width, int floor );
-	void addLadder( int x, int y );
-	
 	bool randFloor( bool &top, sf::Uint8 floor, sf::Uint8 &new_floor );
-	void fill( int a, int n );	//  Fill from left to right 
-	void islands();
-	void positioning( int size );
+	void fill( int a, int n );	//  Fill from left to right
+	vector <Plank*> top_islands( int w2, int h2 );
+	vector <Plank*> positioning( int size, int w1, int h1, int w2, int h2 );
 	
-	
-	bool checkCollision( Rect* rect );
+	// bool checkCollision( Rect* rect );
 	bool checkBlockByPixel( Rect* rect );
-	bool checkLadder( Rect* rect );
 	
 	sf::Uint8 moveX( sf::Uint8 direction, float vel );
 	sf::Uint8 getWidth();
@@ -62,19 +51,5 @@ public:
 	int getLastGrassX();
 	void setNewX( int heroX );
 	int backToGrass();
-	
-	
-	Brick();
-	~Brick();
-	void free();
-
-	void load( int screen_w, int screen_h, int nr, int type );
-	void draw( sf::RenderWindow* &window );
-	void drawLadders( sf::RenderWindow* &window );
-	
-	int getScreenWidth();
-	int getScreenHeight();
-	void fadein( int v = 1, int max = 0xFF );
-	void fadeout( int v = 1, int min = 0 );
 };
 
