523d0b341268108ddb2d7e1dfaf5730c90547954
devmichalek <devmichalek@gmail.com>
2017-01-23 12:36:15 +0100

    new meaning, another huge object with textures and positions - ladder class

--- to edit/04_platform/world/ladder.cpp
+++ to edit/04_platform/world/ladder.cpp
@@ -1,12 +1,211 @@
 #include "ladder.h"
 #include <cstdlib>
 
+Plank::Plank()
+{
+	x = y = nr = 0;
+	red = green = blue = 0;
+}
+
+
+
 Ladder::Ladder()
 {
-	x = y = 0;
-	nr = 0;
+	red = green = blue = add = 0;
+}
+
+Ladder::~Ladder()
+{
+	free();
+}
+
+void Ladder::free()
+{
+	for( unsigned i = 0; i < ladder.size(); i++ )
+	{
+		ladder[ i ]->free();
+	}
 	
-	red = 0;
-	green = 0;
-	blue = 0;
+	ladder.clear();
+	ladders.clear();
+	
+	red = green = blue = add = 0;
+}
+
+
+void Ladder::load( int type )
+{
+	free();
+	
+	for( int i = 0; i < 2; i++ )
+	{
+		ladder.push_back( new MySprite() );
+		ladder[ i ]->setName( "brick-ladder[" +to_string( i ) +"]" );
+		ladder[ i ]->loadByImage( "data/sprites/play/ladder/" +to_string( i ) +".png" );
+	}
+	
+	// setColor
+	if( type == 0 )
+	{
+		red = 0;
+		green = 0xFF;
+		blue = 0xAA;
+	}
+	else if( type == 1 )
+	{
+		red = 0xFF;
+		green = 0x9C;
+		blue = 0x50;
+	}
+	else if( type == 2 )
+	{
+		red = 0;
+		green = 0xFF;
+		blue = 0xFF -0xAA;
+		add = 0xAA;
+	}
+	else if( type == 3 )
+	{
+		red = 0xFF;
+		green = 0xFF;
+		blue = 0xAA;
+	}
+}
+
+void Ladder::draw( sf::RenderWindow* &window, int screen_w )
+{
+	for( unsigned i = 0; i < ladders.size(); i++ )
+	{
+		if( ladders[ i ]->x > -screen_w/2 && ladders[ i ]->x < screen_w )
+		{
+			ladder[ ladders[ i ]->nr ]->setPosition( ladders[ i ]->x, ladders[ i ]->y );
+			ladder[ ladders[ i ]->nr ]->setColor( sf::Color( ladders[ i ]->red, ladders[ i ]->green, ladders[ i ]->blue ) );
+			window->draw( ladder[ ladders[ i ]->nr ]->get() );
+		}
+	}
+}
+
+
+void Ladder::fadein( int v, int max )
+{
+	for( unsigned i = 0; i < ladder.size(); i++ )
+	{
+		ladder[ i ]->fadein( v, max );
+	}
+}
+
+void Ladder::fadeout( int v, int min )
+{
+	for( unsigned i = 0; i < ladder.size(); i++ )
+	{
+		ladder[ i ]->fadeout( v, min );
+	}
+}
+
+
+int Ladder::strToInt( string s )
+{
+	bool m=false;
+    int tmp=0;
+    unsigned i=0;
+    if(s[0]=='-')
+    {
+          i++;
+          m = true;
+    }
+	
+    while(i<s.size())
+    {
+      tmp = 10*tmp+s[i]-48;
+      i++;
+    }
+	
+    return m ? -tmp : tmp; 
+}
+
+void Ladder::positioning(  vector <Plank*> planks )
+{
+	ladders = planks;
+	
+	for( unsigned i = 0; i < ladders.size(); i++ )
+	{
+		ladders[ i ]->red = red;
+		ladders[ i ]->green = green;
+		ladders[ i ]->blue = rand()%blue +add;
+	}
+}
+
+
+void Ladder::moveX( sf::Uint8 direction, float vel )
+{
+	if( direction == 1 )
+	{
+		for( unsigned i = 0; i < ladders.size(); i++ )
+		{
+			ladders[ i ]->x += vel;
+		}
+	}
+	else if( direction == 2 )
+	{
+		for( unsigned i = 0; i < ladders.size(); i++ )
+		{
+			ladders[ i ]->x -= vel;
+		}
+	}
+}
+
+int Ladder::backToGrass( int add )
+{
+	for( unsigned i = 0; i < ladders.size(); i++ )
+	{
+		ladders[ i ]->x += add;
+	}
+	
+	return add;
+}
+
+void Ladder::addLadder( int x, int y )
+{
+	// add ladder.
+	ladders.push_back( new Plank() );
+	
+	// set x.
+	ladders[ ladders.size()-1 ]->x = x;
+	
+	// set y.
+	ladders[ ladders.size()-1 ]->y = y;
+	
+	ladders[ ladders.size()-1 ]->red;
+	ladders[ ladders.size()-1 ]->green;
+	ladders[ ladders.size()-1 ]->blue = rand()%blue +add;
+}
+
+bool Ladder::checkLadder( Rect* rect, int screen_w )
+{
+	if( rect != NULL )
+	{
+		for( unsigned i = 0; i < ladders.size(); i++ )
+		{
+			if( ladders[ i ]->x > -screen_w/2 && ladders[ i ]->x < screen_w )
+			{
+				ladder[ ladders[ i ]->nr ]->setPosition( ladders[ i ]->x, ladders[ i ]->y );
+				if( ladder[ ladders[ i ]->nr ]->checkCollision( rect->getX() +rect->getWidth() /4, rect->getY() -50, rect->getWidth() /2, rect->getHeight() ) )
+				{
+					return true;
+				}
+			}
+		}
+	}
+	
+	return false;
+}
+
+int Ladder::getW( sf::Uint8 nr )
+{
+	return ladder[ nr ]->getWidth();
+}
+
+int Ladder::getH( sf::Uint8 nr )
+{
+	return ladder[ nr ]->getHeight();
 }
--- to edit/04_platform/world/ladder.h
+++ to edit/04_platform/world/ladder.h
@@ -1,9 +1,12 @@
 #pragma once
 
-#include <SFML/Graphics/Color.hpp>
+#include "drawable/sprite.h"
+#include <vector>
 
-class Ladder
+
+class Plank
 {
+	
 public:
 	
 	int x, y;
@@ -13,5 +16,37 @@ public:
 	sf::Uint8 green;
 	sf::Uint8 blue;
 	
+	Plank();
+};
+
+class Ladder
+{
+	vector <MySprite*> ladder;
+	vector < Plank* > ladders;
+	
+	// Color
+	sf::Uint8 red, green, blue, add;
+	
+public:
+	
 	Ladder();
+	~Ladder();
+	void free();
+	
+	void load( int type );
+	void draw( sf::RenderWindow* &window, int screen_w );
+	
+	void fadein( int v = 1, int max = 0xFF );
+	void fadeout( int v = 1, int min = 0 );
+	
+	int strToInt( string s );
+	void positioning( vector <Plank*> planks );
+	
+	void moveX( sf::Uint8 direction, float vel );
+	int backToGrass( int add );
+	void addLadder( int x, int y );
+	bool checkLadder( Rect* rect, int screen_w );
+	
+	int getW( sf::Uint8 nr );
+	int getH( sf::Uint8 nr );
 };
