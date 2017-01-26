6c1881edc3dcf009bd37b0928a5a3b01bc438138
devmichalek <devmichalek@gmail.com>
2017-01-26 01:00:06 +0100

    plank as new class which contains pos x y, nr of sprite, and color for ladders (textures)

---
+++ to edit/04_platform/world/plank.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "block.h"
+#include <SFML/Graphics/Color.hpp>
+
+class Plank :public Block
+{
+	
+public:
+	
+	sf::Uint8 red;
+	sf::Uint8 green;
+	sf::Uint8 blue;
+	
+	Plank();
+	~Plank();
+	void free();
+};
