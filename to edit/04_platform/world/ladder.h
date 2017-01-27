d92a57f9d92f5738a23b582efac0c11c426fe8d7
devmichalek <devmichalek@gmail.com>
2017-01-27 17:19:30 +0100

    added some comments, fasten functions

--- to edit/04_platform/world/ladder.h
+++ to edit/04_platform/world/ladder.h
@@ -6,8 +6,8 @@
 
 class Ladder
 {
-	vector <MySprite*> ladder;
-	vector <Plank*> ladders;
+	vector <Plank*> planks;
+	vector <MySprite*> sprites;
 	
 	// Color
 	sf::Uint8 red, green, blue, add;
@@ -22,13 +22,12 @@ public:
 	void fadein( int v = 1, int max = 0xFF );
 	void fadeout( int v = 1, int min = 0 );
 	
-	int strToInt( string s );
+	int to_int( string s );
 	void positioning( vector <Plank*> planks );
 	
 	void moveX( sf::Uint8 direction, float vel );
-	int backToGrass( int add );
-	void addLadder( int x, int y );
-	bool checkLadder( Rect* rect, int screen_w );
+	void backToGrass( int add );
+	bool checkCollision( Rect* rect, int screen_w );
 	
 	int getW( sf::Uint8 nr );
 	int getH( sf::Uint8 nr );
