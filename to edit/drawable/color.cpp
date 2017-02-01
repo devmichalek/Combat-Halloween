36dcac821e2e39b765e0cf8340c3b6f5fe9c1a7c
devmichalek <devmichalek@gmail.com>
2017-02-01 23:18:23 +0100

    new purpose

--- to edit/drawable/color.cpp
+++ to edit/drawable/color.cpp
@@ -1,3 +1,12 @@
+/**
+    color.h
+    Purpose: class Color represents 4 sf::Uint8 values.
+
+    @author Adrian Michalek
+    @version 2016.09.09
+	@email adrmic98@gmail.com
+*/
+
 #include "drawable/color.h"
 
 sf::Color Color::getColor()
--- to edit/drawable/color.h
+++ to edit/drawable/color.h
@@ -1,3 +1,12 @@
+/**
+    color.h
+    Purpose: class Color represents 4 sf::Uint8 values.
+
+    @author Adrian Michalek
+    @version 2016.09.09
+	@email adrmic98@gmail.com
+*/
+
 #pragma once
 
 #include <SFML/Graphics/Color.hpp>
@@ -13,7 +22,7 @@ public:
 	sf::Color getColor();
 	void setColor( sf::Color color );
 	
-	void fadein( int v = 1, int max = 255 );
+	void fadein( int v = 1, int max = 0xFF );
 	void fadeout( int v = 1, int min = 0 );
 	
 	sf::Uint8 getAlpha();
