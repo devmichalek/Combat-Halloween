8747171767c6e7570ffb2f1e2f996f89a67aec08
devmichalek <devmichalek@gmail.com>
2017-02-01 23:18:50 +0100

    update, deleted old not used functions

--- to edit/drawable/sprite.cpp
+++ to edit/drawable/sprite.cpp
@@ -9,33 +9,15 @@
 
 
 #include "sprite.h"
-// #include <fstream>
-#include <typeinfo>
 
 #ifdef _WIN32
 #include <windows.h>
 #endif
 
-void MySprite::operator =( MySprite& mysprite )
-{
-	name = mysprite.getName();
-	sprite = new sf::Sprite;
-	*sprite = mysprite.get();
-	offset = mysprite.getOffset();
-	nr = mysprite.getNr();
-	safe_width = mysprite.getSafe_width();
-	safe_height = mysprite.getSafe_height();
-	color = mysprite.getColor();
-	color.a = mysprite.getAlpha();
-	
-	width = mysprite.getWidth();
-	height = mysprite.getHeight();
-	left = mysprite.getLeft();
-	top = mysprite.getTop();
-}
 
 
-MySprite::MySprite( int x, int y, sf::Uint8 alpha )
+
+MySprite::MySprite()
 {
 	name = "";
 	
@@ -46,15 +28,13 @@ MySprite::MySprite( int x, int y, sf::Uint8 alpha )
 	color.r = 0xFF;
 	color.g = 0xFF;
 	color.b = 0xFF;
-	color.a = alpha;
+	color.a = 0;
 	
-    safe_width = width = 0;
-	safe_height = height = 0;
-	left = x;
-	top = y;
-	
-    nr = 0;
+	nr = 0;
     offset = 0;
+	
+    safe_width = width = left = 0;
+	safe_height = height = top = 0;
 }
 
 MySprite::~MySprite()
@@ -94,28 +74,14 @@ void MySprite::free()
 	color.b = 0xFF;
 	color.a = 0x00;
 	
-    safe_width = width = 0;
-	safe_height = height = 0;
-	left = 0;
-	top = 0;
-	
 	nr = 0;
     offset = 0;
-}
-
-
-
-
-
-const sf::Sprite& MySprite::get() const
-{
-    if( nr == 0 || nr == 1 )
-    {
-        return *sprite;
-    }
 	
-    return sprite[ offset ];
+    safe_width = width = left = 0;
+	safe_height = height = top = 0;
 }
+
+
 
 #ifdef __linux__
 void MySprite::load( string path, int nr )
@@ -124,6 +90,16 @@ void MySprite::load( string path, int nr )
 	
 	try
 	{
+		image = new sf::Image;
+		if( image == NULL )
+		{
+			throw name + " \x1B[91mnot created\x1B[0m image";
+		}
+		else if( !image->loadFromFile( path ) )
+		{
+			throw name + " \x1B[91mnot loaded\x1B[0m " + path;
+		}
+		
 		texture = new sf::Texture;
 		if( texture == NULL )
 		{
@@ -225,14 +201,20 @@ void MySprite::create( int w, int h )
 		}
 		else
 		{
+			sf::Uint8* pixels;
+			pixels = new sf::Uint8 [ w * h * 4 ];
 			
-			sf::Uint8 pixels[ w * h * 4 ];
 			for( int i = 0; i < w*h*4; i++ )
 			{
 				pixels[ i ] = 0xFF;
 			}
+			
 			nr = 0;
+			
 			texture->update( pixels );
+			
+			delete [] pixels;
+			pixels = NULL;
 		}
 	}
 	catch( string msg )
@@ -240,8 +222,6 @@ void MySprite::create( int w, int h )
 		cerr << msg << endl;
 	}
 	
-	
-
 	try
 	{
 		sprite = new sf::Sprite;
@@ -264,6 +244,93 @@ void MySprite::create( int w, int h )
 #endif
 
 
+
+void MySprite::fadein( int v, int max )
+{
+	if( color.a < max )
+	{
+		int alpha = color.a + v;
+		
+		if( alpha > max )
+		{
+			alpha = max;
+		}
+		
+		color.a = alpha;
+		
+		if( nr == 0 || nr == 1 )
+		{
+			sprite->setColor( color );
+		}
+		else if( nr > 1 )
+		{
+			for( int i = 0; i < nr; i++ )
+			{
+				sprite[ i ].setColor( color );
+			}
+		}
+	}
+}
+
+void MySprite::fadeout( int v, int min )
+{
+	if( color.a > min )
+	{
+		int alpha = color.a - v;
+		
+		if( alpha < min )
+		{
+			alpha = min;
+		}
+		
+		color.a = alpha;
+
+		if( nr == 0 || nr == 1 )
+		{
+			sprite->setColor( color );
+		}
+		else if( nr > 1 )
+		{
+			for( int i = 0; i < nr; i++ )
+			{
+				sprite[ i ].setColor( color );
+			}
+		}
+	}
+}
+
+
+
+void MySprite::setScale( float x, float y )
+{
+    if( nr == 0 || nr == 1 )
+    {
+        sprite->setScale( x, y );
+    }
+    else if( nr > 1 )
+    {
+        for( int i = 0; i < nr; i++ )
+        {
+            sprite[ i ].setScale( x, y );
+        }
+    }
+	
+	if( x < 0 ) x = -x;
+	if( y < 0 ) y = -y;
+	
+	width = safe_width * x;
+	height = safe_height * y;
+}
+
+const sf::Sprite& MySprite::get() const
+{
+    if( nr == 0 || nr == 1 )
+    {
+        return *sprite;
+    }
+	
+    return sprite[ offset ];
+}
 
 
 
@@ -303,154 +370,6 @@ void MySprite::center( int x, int y, int w, int h )
     }
 }
 
-
-
-
-
-std::ostream& MySprite::operator <<( std::ostream& str )
-{
-	std::string result = "";
-	result += name;
-	result += " x: " + left;
-	result += " y: " + top;
-	result += " w: " + width;
-	result += " h: " + height;
-	result += " alpha: " + color.a;
-	
-	return str << result;
-}
-
-bool MySprite::operator ==( MySprite& mySprite )
-{
-	if( width == mySprite.getWidth() && height == mySprite.getHeight() )
-	{
-		if( color == mySprite.getColor() )
-		{
-			if( name == mySprite.getName() )
-			{
-				return true;
-			}
-		}
-	}
-	
-	return false;
-}
-
-bool MySprite::operator >( MySprite& mySprite )
-{
-	if( width > mySprite.getWidth() && height > mySprite.getHeight() )
-	{
-		return true;
-	}
-	
-	return false;
-}
-
-bool MySprite::operator >=( MySprite& mySprite )
-{
-	if( width >= mySprite.getWidth() && height >= mySprite.getHeight() )
-	{
-		return true;
-	}
-	
-	return false;
-}
-
-bool MySprite::operator <( MySprite& mySprite )
-{
-	if( width < mySprite.getWidth() && height < mySprite.getHeight() )
-	{
-		return true;
-	}
-	
-	return false;
-}
-
-bool MySprite::operator <=( MySprite& mySprite )
-{
-	if( width <= mySprite.getWidth() && height <= mySprite.getHeight() )
-	{
-		return true;
-	}
-	
-	return false;
-}
-
-int MySprite::getNr()
-{
-	return nr;
-}
-
-int MySprite::getSafe_width()
-{
-	return safe_width;
-}
-
-int MySprite::getSafe_height()
-{
-	return safe_height;
-}
-
-
-
-
-/*
-const string& MySprite::getTypeId()
-{
-	string str = typeid( nr ).name;
-	
-	return str;
-}
-*/
-
-
-
-
-void MySprite::fadein( int v, int max )
-{
-	if( color.a < max )
-	{
-		int alpha = color.a + v;
-		if( alpha > max )
-			alpha = max;
-		color.a = alpha;
-		
-		if( nr == 0 || nr == 1 )
-		{
-			sprite->setColor( color );
-		}
-		else if( nr > 1 )
-		{
-			for( int i = 0; i < nr; i++ )
-			{
-				sprite[ i ].setColor( color );
-			}
-		}
-	}
-}
-
-void MySprite::fadeout( int v, int min )
-{
-	if( color.a > min )
-	{
-		int alpha = color.a - v;
-		if( alpha < min )
-			alpha = min;
-		color.a = alpha;
-
-		if( nr == 0 || nr == 1 )
-		{
-			sprite->setColor( color );
-		}
-		else if( nr > 1 )
-		{
-			for( int i = 0; i < nr; i++ )
-			{
-				sprite[ i ].setColor( color );
-			}
-		}
-	}
-}
 
 
 void MySprite::setColor( sf::Color color )
@@ -510,45 +429,14 @@ void MySprite::setAlpha( sf::Uint8 a )
 	}
 }
 
-
-const int& MySprite::getOffset() const
-{
-	return offset;
-}
-
 void MySprite::setOffset( int n )
 {
     offset = n;
 }
 
-
-void MySprite::setScale( float x, float y )
+const int& MySprite::getOffset() const
 {
-    if( nr == 0 || nr == 1 )
-    {
-        sprite->setScale( x, y );
-    }
-    else if( nr > 1 )
-    {
-        for( int i = 0; i < nr; i++ )
-        {
-            sprite[ i ].setScale( x, y );
-        }
-    }
-	
-	if( x < 0 ) x = -x;
-	if( y < 0 ) y = -y;
-	
-	width = safe_width * x;
-	height = safe_height * y;
-}
-
-
-
-
-const std::string& MySprite::getName() const
-{
-	return name;
+	return offset;
 }
 
 void MySprite::setName( std::string name )
@@ -556,235 +444,12 @@ void MySprite::setName( std::string name )
 	this->name = name;
 }
 
-
-/*
-void MySprite::createTxt( string name, string path )
+const std::string& MySprite::getName() const
 {
-	fstream file;
-	
-	file.open( name.c_str(), std::ios::out | std::ios::app );
-	if( file.bad() )
-	{
-		printf( "Can not create file %s\n", name.c_str() );
-	}
-	else
-	{
-		try
-		{
-			sf::Texture tex;
-			if( !tex.loadFromFile( path ) )
-			{
-				throw name + " \x1B[91mnot loaded\x1B[0m " + path;
-			}
-			else
-			{
-				tex.setSmooth( true );
-				
-				sf::Image image = tex.copyToImage();
-				int w = tex.getSize().x;
-				int h = tex.getSize().y;
-				
-				file << std::to_string( w ) << " " << std::to_string( h ) << "\n";
-				// printf( "%d %d\n", w, h );
-				
-				// int counter = 0;
-				// Following red, green, blue, alpha of texture
-				for( int i = 0; i < h; i++ )
-				{
-					for( int j = 0; j < w; j++ )
-					{
-						file << std::to_string( static_cast <int> ( image.getPixel( i, j ).r ) ) << " ";
-						file << std::to_string( static_cast <int> ( image.getPixel( i, j ).g ) ) << " ";
-						file << std::to_string( static_cast <int> ( image.getPixel( i, j ).b ) ) << " ";
-						file << std::to_string( static_cast <int> ( image.getPixel( i, j ).a ) ) << " ";
-						// counter += 4;
-					}
-				}
-				
-				// printf( "Counter %d\n", counter );
-			}
-		}
-		catch( string msg )
-		{
-			cerr << msg << endl;
-		}
-		
-	}
+	return name;
 }
 
-void MySprite::loadTxt( string path )
-{
-	free();
-	
-	fstream file;
-	
-	file.open( path.c_str(), std::ios::in );
-	if( file.bad() )
-	{
-		printf( "Can not load file %s\n", path.c_str() );
-	}
-	else
-	{
-		file >> rect.width;
-		file >> rect.height;
-		
-		// printf( "%d %d", rect.width, rect.height );
-		
-		try
-		{
-			texture = new sf::Texture;
-			if( texture == NULL )
-			{
-				throw name + " \x1B[91mnot created\x1B[0m texture";
-			}
-			else if( !texture->create( rect.width, rect.height ) )
-			{
-				throw name + " \x1B[91mnot created blank texture\x1B[0m";
-			}
-			else
-			{
-				pixels = new sf::Uint8[ rect.width * rect.height * 4 ];
-				if( pixels == NULL )
-				{
-					throw name + " \x1B[91mnot created\x1B[0m array of pixels";
-				}
-				else
-				{
-					this->color = color;
-					this->color.a = 0x00;
-					
-					int led;
-					// int counter = 0;
-					for( int i = 0; i < rect.width*rect.height*4; i ++ )
-					{
-						file >> led;
-						pixels[ i ] = static_cast <sf::Uint8> ( led );
-						// counter ++;
-					}
-					
-					// printf( "Counter %d\n", counter );
-				}
-				
-				nr = 0;
-				texture->update( pixels );
-			}
-		}
-		catch( string msg )
-		{
-			cerr << msg << endl;
-		}
-		
-		
-		try
-		{
-			sprite = new sf::Sprite;
-			if( sprite == NULL )
-			{
-				throw name + " \x1B[91mnot created\x1B[0m sprite";
-			}
-			else
-			{
-				sprite->setTexture( *texture );
-			}
-		}
-		catch( string msg )
-		{
-			cerr << msg << endl;
-		}
-	}
-}
-*/
 
-    
-void MySprite::loadByImage( string path, int nr )
-{
-	free();
-	
-	try
-	{
-		image = new sf::Image;
-		if( image == NULL )
-		{
-			throw name + " \x1B[91mnot created\x1B[0m image";
-		}
-		else if( !image->loadFromFile( path ) )
-		{
-			throw name + " \x1B[91mnot loaded\x1B[0m " + path;
-		}
-		
-		texture = new sf::Texture;
-		if( texture == NULL )
-		{
-			throw name + " \x1B[91mnot created\x1B[0m texture";
-		}
-		else if( !texture->loadFromFile( path ) )
-		{
-			throw name + " \x1B[91mnot loaded\x1B[0m " + path;
-		}
-		else
-		{
-			texture->setSmooth( true );
-			
-			if( nr < 1 )	throw name + " \x1B[91mnumber is less than 1\x1B[0m";
-			else			this->nr = nr;
-		}
-	}
-	catch( string msg )
-	{
-		cerr << msg << endl;
-	}
-	
-	if( nr == 1 )
-	{
-		try
-		{
-			sprite = new sf::Sprite;
-			if( sprite == NULL )
-			{
-				throw name + " \x1B[91mnot created\x1B[0m sprite";
-			}
-			else
-			{
-				sprite->setTexture( *texture );
-				sprite->setColor( color );
-				safe_width = width = texture->getSize().x;
-				safe_height = height = texture->getSize().y;
-			}
-		}
-		catch( string msg )
-		{
-			cerr << msg << endl;
-		}
-	}
-	else if( nr > 1 )
-	{
-		try
-		{
-			sprite = new sf::Sprite[ nr ];
-			if( sprite == NULL )
-			{
-				throw name + " \x1B[91mnot created\x1B[0m array of sprite";
-			}
-			else
-			{
-				safe_width = width = texture->getSize().x / nr;
-				safe_height = height = texture->getSize().y;
-			}
-			
-			sf::IntRect r;	// temporary rectangle
-			for( int i = 0; i < nr; i++ )
-			{
-				sprite[ i ].setTexture( *texture );
-				sprite[ i ].setTextureRect( sf::IntRect( i * width, 0, width, height ) );
-				sprite[ i ].setColor( color );
-			}
-		}
-		catch( string msg )
-		{
-			cerr << msg << endl;
-		}
-	}
-}
 
 bool MySprite::checkPixelCollision( int x, int y )
 {
@@ -799,4 +464,75 @@ bool MySprite::checkPixelCollision( int x, int y )
 	}
 	
 	return false;
+}
+
+
+
+bool MySprite::operator ==( MySprite& mySprite )
+{
+	if( width == mySprite.getWidth() && height == mySprite.getHeight() )
+	{
+		if( color == mySprite.getColor() )
+		{
+			if( name == mySprite.getName() )
+			{
+				return true;
+			}
+		}
+	}
+	
+	return false;
+}
+
+bool MySprite::operator >( MySprite& mySprite )
+{
+	if( width > mySprite.getWidth() && height > mySprite.getHeight() )
+	{
+		return true;
+	}
+	
+	return false;
+}
+
+bool MySprite::operator >=( MySprite& mySprite )
+{
+	if( width >= mySprite.getWidth() && height >= mySprite.getHeight() )
+	{
+		return true;
+	}
+	
+	return false;
+}
+
+bool MySprite::operator <( MySprite& mySprite )
+{
+	if( width < mySprite.getWidth() && height < mySprite.getHeight() )
+	{
+		return true;
+	}
+	
+	return false;
+}
+
+bool MySprite::operator <=( MySprite& mySprite )
+{
+	if( width <= mySprite.getWidth() && height <= mySprite.getHeight() )
+	{
+		return true;
+	}
+	
+	return false;
+}
+
+std::ostream& MySprite::operator <<( std::ostream& str )
+{
+	std::string result = "";
+	result += name;
+	result += " x: " + left;
+	result += " y: " + top;
+	result += " w: " + width;
+	result += " h: " + height;
+	result += " alpha: " + color.a;
+	
+	return str << result;
 }
--- to edit/drawable/sprite.h
+++ to edit/drawable/sprite.h
@@ -17,14 +17,14 @@ using namespace std;
 
 class MySprite :public Rect, public Color
 {
-	string name;		// Identity
+	string name;	// Identity.
+	
     sf::Sprite* sprite;
     sf::Texture* texture;
 	sf::Image* image;
 	
-	
-    int nr;		// How many offset's, 0 = blank texture, 1 = loaded image, 1 < image with offsets
-    int offset;	// Topical offset
+    int nr;		// How many offset's, 0 = blank texture, 1 = loaded image, 1 < sprite.
+    int offset;	// Topical offset.
 	
 	int safe_width;
 	int safe_height;
@@ -32,51 +32,43 @@ class MySprite :public Rect, public Color
 	
 public:
 
-	void operator =( MySprite& mysprite );
-
-    MySprite( int x = 0, int y = 0, sf::Uint8 alpha = 0x00 );
+    MySprite();
     ~MySprite();
-    void free();	// Delete everything
+    void free();
+	
+	void load( string path, int nr = 1 );	// Load.
+	void create( int w, int h );	// Create blank texture.
+	
+	void fadein( int v = 1, int max = 0xFF );
+	void fadeout( int v = 1, int min = 0 );
 	
 	
-	const sf::Sprite& get() const;			// Return sprite
-    void load( string path, int nr = 1 );	// Load texture
-	void create( int w, int h );			// Create blank texture
 	
-	
+	void setScale( float x = 1, float y = 1 );
+	const sf::Sprite& get() const;	// Return sprite
+   
     void setPosition( float x, float y );
 	void center( int x, int y, int w, int h );
 	
 	
-	std::ostream& operator <<( std::ostream& str );	// Print name, rect, alpha
-	bool operator ==( MySprite& mySprite );			// Return true if we have the same size, color, name
-	bool operator >( MySprite& mySprite );
-	bool operator >=( MySprite& mySprite );
-	bool operator <( MySprite& mySprite );
-	bool operator <=( MySprite& mySprite );
-	
-	
-	// const string& getTypeId();
-	
-	
-	void fadein( int v = 1, int max = 255 );
-	void fadeout( int v = 1, int min = 0 );
 	
 	void setColor( sf::Color color );
 	void setAlpha( sf::Uint8 a = 0 );
 	
-	const int& getOffset() const;
 	void setOffset( int n );
+	const int& getOffset() const;
 	
-	int getNr();
-	int getSafe_width();
-	int getSafe_height();
-	
-	void setScale( float x = 1, float y = 1 );
-	
-	const string& getName() const;
 	void setName( string name );
+	const string& getName() const;
 	
-	void loadByImage( string path, int nr = 1 );
+	
 	bool checkPixelCollision( int x, int y );
+	
+	
+	bool operator ==( MySprite& mySprite );
+	bool operator >( MySprite& mySprite );
+	bool operator >=( MySprite& mySprite );
+	bool operator <( MySprite& mySprite );
+	bool operator <=( MySprite& mySprite );
+	std::ostream& operator <<( std::ostream& str );
 };
