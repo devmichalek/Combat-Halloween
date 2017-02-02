18d11cee6d15321238953c77cd7248eeddc425c5
devmichalek <devmichalek@gmail.com>
2017-02-02 21:45:16 +0100

    delay, alpha_line stuff

--- to edit/03_level/character.h
+++ to edit/03_level/character.h
@@ -1,6 +1,6 @@
 /**
     character.h
-    Purpose: class Character - shows 2 animations in level state.
+    Purpose: class Character - shows 2 animations in level state and allow to choose what of them, then game starts.
 
     @author Adrian Michalek
     @version 2016.12.06
@@ -32,13 +32,17 @@ class Character :public Click
 	int range;
 	bool keep;
 	
+	// Other stuff
+	sf::Uint8 delay;
+	sf::Uint8 alpha_line;
+	
 public:
 	
 	Character();
     ~Character();
-	
 	void free();
 	void reset( int screen_w, int screen_h );
+
 
     void load( int screen_w, int screen_h );
     void draw( sf::RenderWindow* &window );
