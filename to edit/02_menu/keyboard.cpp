e6e08e9eddc0c78d07d59c8bf1aef21e1b65ff6e
devmichalek <devmichalek@gmail.com>
2017-02-02 21:42:38 +0100

    new functions: add(), substract(), isPossibleKey(), handleButton(), more vectors, more iterators (of topical vector), keys repaired, key instead of double key, saving repaired

--- to edit/02_menu/keyboard.cpp
+++ to edit/02_menu/keyboard.cpp
@@ -1,6 +1,6 @@
 /**
     keyboard.h
-    Purpose: class Keyboard and DoubleKey - to change hero keys.
+    Purpose: class Keyboard and Key - to manage keyboard.
 
     @author Adrian Michalek
     @version 2016.11.11
@@ -8,55 +8,96 @@
 */
 
 #include "keyboard.h"
-#include <string>
 #include <fstream>
+
+
+Key::Key(int one, int two )
+{
+	this->one = one;
+	this->two = two;
+}
+
+Key::~Key()
+{
+	free();
+}
+
+void Key::free()
+{
+	one = two = -1;
+}
+
+bool Key::isEmpty()
+{
+	if( one == -1 && two == -1 )
+	{
+		return true;
+	}
+	
+	return false;
+}
+
+bool Key::oneIsFree()
+{
+	if( one == -1 || two == -1 )
+	{
+		return true;
+	}
+	
+	return false;
+}
 
 
 
 Keyboard::Keyboard()
 {
-    nr = 0;
-	text = NULL;
-	
 	focus = false;
 	which = -1;
 	
-	add = 0;
-	substract = 0;
-	addMode = 0;
-	
+	addMode = false;
+	release = false;
 	lastChosen = -1;
 }
 
 Keyboard::~Keyboard()
 {
+	free();
+}
+
+void Keyboard::free()
+{
 	save_button.free();
 	
-	if( text != NULL )
+	freeObject( text );
+	freeObject( keys );
+	freeObject( actual_keys );
+	if( !banned_keys.empty() )
 	{
-		for( int i = 0; i < nr; i++ )
-		{
-			text[ i ].free();
-		}
-		
-		delete [] text;
-		text = NULL;
-		nr = 0;
+		banned_keys.clear();
 	}
-	
-	keys.clear();
-	actual_keys.clear();
-	banned_keys.clear();
 	
 	focus = false;
 	which = -1;
 	
-	add = 0;
-	substract = 0;
-	addMode = 0;
-	
+	addMode = false;
+	release = false;
 	lastChosen = -1;
 }
+
+template <typename object>
+void Keyboard::freeObject( vector <object> o )
+{
+	if( !o.empty() )
+	{
+		for( auto &it :o )
+		{
+			it->free();
+		}
+
+		o.clear();
+	}
+}
+
 
 
 
@@ -68,12 +109,12 @@ void Keyboard::load( int left, int right, int bot, int screen_w, int screen_h )
 	save_button.setScale( 0.5, 0.5 );
 	save_button.setPosition( screen_w - save_button.getWidth() -30, screen_h/2 -80 );
 	
-	nr = 18;
-	text = new MyText[ nr ];
-	for( int i = 0; i < nr; i++ )
+	// text
+	for( int i = 0; i < 18; i++ )
 	{
-		text[ i ].setName( "keyboard-text nr " + std::to_string( i ) );
-		text[ i ].setFont( "data/fonts/Jaapokki-Regular.otf", 25, 0xFF, 0xFF, 0xFF );
+		text.push_back( new MyText() );
+		text[ i ]->setName( "keyboard-text nr " + std::to_string( i ) );
+		text[ i ]->setFont( "data/fonts/Jaapokki-Regular.otf", 25, 0xFF, 0xFF, 0xFF );
 	}
 
 	// load default keys
@@ -92,7 +133,7 @@ void Keyboard::load( int left, int right, int bot, int screen_w, int screen_h )
 			if( a != -2 )
 			{
 				b = strToInt( buf );
-				keys.push_back( addKey( a, b ) );
+				keys.push_back( new Key( a, b ) );
 				a = -2;
 			}
 			else
@@ -116,57 +157,58 @@ void Keyboard::load( int left, int right, int bot, int screen_w, int screen_h )
 			if( a != -2 )
 			{
 				b = strToInt( buf );
-				actual_keys.push_back( addKey( a, b ) );
+				actual_keys.push_back( new Key( a, b ) );
 				a = -2;
 			}
 			else
 				a = strToInt( buf );
 		}
 		
-		for( int i = 1; i < nr; i += 2 )
+		for( unsigned i = 1; i < text.size(); i += 2 )
 		{
 			string newName = "";
-			newName += getName( actual_keys[ (i-1) / 2 ].a );
-			if( actual_keys[ (i-1) / 2 ].b != -1 )
-				newName += " + " + getName( actual_keys[ (i-1) / 2 ].b );
-				
-			text[ i ].setText( newName );
+			
+			newName += getName( actual_keys[ (i-1) / 2 ]->one );
+			
+			if( actual_keys[ (i-1) / 2 ]->two != -1 )
+			{
+				newName += " + " + getName( actual_keys[ (i-1) / 2 ]->two );
+			}
+			
+			text[ i ]->setText( newName );
 		}
 	}
 	file.close();
 	
 	
 	// set text
-	text[ 0 ].setText( "Move left" );
-	text[ 2 ].setText( "Move right" );
-	text[ 4 ].setText( "Jump" );
-	text[ 6 ].setText( "Slide" );
-	text[ 8 ].setText( "Climb" );
-	text[ 10 ].setText( "Attack" );
-	text[ 12 ].setText( "Jump attack" );
-	text[ 14 ].setText( "Throw" );
-	text[ 16 ].setText( "Jump throw" );
+	text[ 0 ]->setText( "Move left" );
+	text[ 2 ]->setText( "Move right" );
+	text[ 4 ]->setText( "Jump" );
+	text[ 6 ]->setText( "Slide" );
+	text[ 8 ]->setText( "Climb" );
+	text[ 10 ]->setText( "Attack" );
+	text[ 12 ]->setText( "Jump attack" );
+	text[ 14 ]->setText( "Throw" );
+	text[ 16 ]->setText( "Jump throw" );
 	
 	// set position
-	text[ 0 ].setPosition( left, bot );
-	text[ 1 ].setPosition( 250, bot );
+	text[ 0 ]->setPosition( left, bot );
+	text[ 1 ]->setPosition( 250, bot );
 	for( int i = 2; i < 8; i += 2 )
 	{
-		text[ i ].setPosition( left, text[ i -2 ].getBot() + 10 );
-		text[ i +1 ].setPosition( 250, text[ i -2 ].getBot() + 10 );
+		text[ i ]->setPosition( left, text[ i -2 ]->getBot() + 10 );
+		text[ i +1 ]->setPosition( 250, text[ i -2 ]->getBot() + 10 );
 	}
 	
-	text[ 8 ].setPosition( right +120, bot -5 );
-	text[ 9 ].setPosition( right +350, bot -5 );
-	for( int i = 10; i < nr -1; i += 2 )
+	text[ 8 ]->setPosition( right +120, bot -5 );
+	text[ 9 ]->setPosition( right +350, bot -5 );
+	for( unsigned i = 10; i < text.size() -1; i += 2 )
 	{
-		text[ i ].setPosition( right +120, text[ i -2 ].getBot() + 10 );
-		text[ i +1 ].setPosition( right +350, text[ i -2 ].getBot() + 10 );
+		text[ i ]->setPosition( right +120, text[ i -2 ]->getBot() + 10 );
+		text[ i +1 ]->setPosition( right +350, text[ i -2 ]->getBot() + 10 );
 	}
 	
-	// set add nr and substract nr
-	add = 67;
-	substract = 68;
 	
 	// load and set banned keys
 	file.open( "data/txt/menu/banned_keys.txt" );
@@ -190,9 +232,9 @@ void Keyboard::load( int left, int right, int bot, int screen_w, int screen_h )
 
 void Keyboard::draw( sf::RenderWindow &window )
 {
-	for( int i = 0; i < nr; i++ )
+	for( vector <MyText*>::iterator it = text.begin(); it != text.end(); it++ )
 	{
-		window.draw( text[ i ].get() );
+		window.draw( (*it)->get() );
 	}
 	
 	window.draw( save_button.get() );
@@ -200,129 +242,58 @@ void Keyboard::draw( sf::RenderWindow &window )
 
 void Keyboard::handle( sf::Event &event )
 {
-	save_button.setOffset( 0 );
-	
+	// keyboard stuff
 	if( which != -1 )
 	{
-		static bool rel = false;
-		if( event.type == sf::Event::KeyPressed && !rel )
+		if( event.type == sf::Event::KeyPressed && !release )
 		{
-			if( addMode == 1 )
+			// add key if we clicked key "+" before.
+			if( isPossibleKey( event ) && addMode )
 			{
-				sf::Uint8 code = event.key.code;
-				bool success = false;
-				
-				if( code > -1 && code <= 25 )
+				if( actual_keys[ which/2 ]->one < 0 )
 				{
-					success = true;
+					actual_keys[ which/2 ]->one = event.key.code;
+					text[ which +1 ]->setText( getName( event.key.code ) );
 				}
-				else if( code >= 71 && code <= 74 )
+				else if( actual_keys[ which/2 ]->two < 0 )
 				{
-					success = true;
-				}
-				else if( code >= 37 && code <= 43 )
-				{
-					success = true;
-				}
-				else if( code == 57 || code == 58 )
-				{
-					success = true;
-				}
-					
-					
-				for( unsigned i = 0; i < banned_keys.size(); i++ )
-				{
-					if( code == banned_keys[ i ] )
-					{
-						success = false;
-						break;
-					}
+					actual_keys[ which/2 ]->two = event.key.code;
+					text[ which +1 ]->setText( getName( actual_keys[ which/2 ]->one ) + " + " + getName( event.key.code ) );
 				}
 				
-				if( success )
-				{
-					if( actual_keys[ which/2 ].a < 0 )
-					{
-						actual_keys[ which/2 ].a = event.key.code;
-						text[ which +1 ].setText( getName( event.key.code ) );
-						text[ which +1 ].reloadPosition();
-					}
-					else if( actual_keys[ which/2 ].b < 0 )
-					{
-						actual_keys[ which/2 ].b = event.key.code;
-						text[ which +1 ].setText( getName( actual_keys[ which/2 ].a ) + " + " + getName( event.key.code ) );
-						text[ which +1 ].reloadPosition();
-					}
-				}
+				text[ which +1 ]->reloadPosition();
 			}
 			
-			if( event.key.code == substract ) // '-'
+			// check if we clicked key "-"
+			if( !substract( event ) )
 			{
-				if( actual_keys[ which/2 ].b != -1 )
-				{
-					actual_keys[ which/2 ].b = -1;
-				}
-				else if( actual_keys[ which/2 ].a != -1 )
-				{
-					actual_keys[ which/2 ].a = -1;
-				}
-				
-				
-				if( actual_keys[ which/2 ].isEmpty() )
-				{
-					text[ which +1 ].setText( "-" );
-					text[ which +1 ].reloadPosition();
-				}
-				else if( actual_keys[ which/2 ].a != -1 )
-				{
-					text[ which +1 ].setText( getName( actual_keys[ which/2 ].a ) );
-					text[ which +1 ].reloadPosition();
-				}
-				
-				
-				// printf( "key nr %d, text nr %d\n", which/2, which +1 );
-				
-				text[ which ].setColor( 0x8C, 0x00, 0x1A );
-				addMode = 0;
-			}
-			else if( event.key.code == add ) // '+'
-			{
-				if( actual_keys[ which/2 ].oneIsFree() )
-				{
-					addMode = 1;
-					text[ which ].setColor( 0x8C, 0xA9, 0x3E );
-				}
-				else
-				{
-					addMode = 0;
-				}
+				// check if we clicked key "+" if yes then allow ( set addMode = true )
+				add( event );
 			}
 			
-			rel = true;
+			release = true;
 		}
 		else if( event.type == sf::Event::KeyReleased )
 		{
-			rel = false;
+			release = false;
 		}
 	}
 	
+	
+	// mouse stuff
 	if( event.type == sf::Event::MouseButtonPressed )
 	{
+		// check whether key is empty
 		for( unsigned i = 0; i < actual_keys.size(); i++ )
 		{
-			if( actual_keys[ i ].isEmpty() )
+			if( actual_keys[ i ]->isEmpty() )
 			{
-				actual_keys[ i ] = keys[ i ];
+				int one = keys[ i ]->one;
+				actual_keys[ i ]->one = one;
 				
-				string newName = getName( actual_keys[ i ].a );
-					
-				if( actual_keys[ i ].b > -1 )
-				{
-					newName +=  " + " + getName( actual_keys[ i ].b );
-				}
-					
-				text[ i*2 +1 ].setText( newName );
-				text[ i*2 +1 ].reloadPosition();
+				text[ i*2 +1 ]->setText( getName( actual_keys[ i ]->one ) );
+				text[ i*2 +1 ]->reloadPosition();
+				// printf( "%s\n", getName( actual_keys[ i ]->one ).c_str() );
 			}
 		}
 		
@@ -330,26 +301,32 @@ void Keyboard::handle( sf::Event &event )
 		int x = event.mouseButton.x;
 		int y = event.mouseButton.y;
 		
+		// check if we have the same keys
 		if( lastChosen != -1 )
 		{
 			bool error = false;
 
-			for( unsigned m = 0; m < actual_keys.size(); m++ )
+			for( unsigned i = 0; i < actual_keys.size(); i++ )
 			{
-				if( static_cast <unsigned> (lastChosen) == m )
+				if( static_cast <unsigned> (lastChosen) == i )
 				{
 					continue;
 				}
-				else if( actual_keys[ lastChosen ].a == actual_keys[ m ].a && actual_keys[ lastChosen ].b == actual_keys[ m ].b )
+				else if( actual_keys[ lastChosen ]->one == actual_keys[ i ]->one && actual_keys[ lastChosen ]->two == actual_keys[ i ]->two )
 				{
 					error = true;
 					break;
 				}
-				else if( actual_keys[ lastChosen ].a == actual_keys[ m ].b && actual_keys[ lastChosen ].b == actual_keys[ m ].a )
+				else if( actual_keys[ lastChosen ]->one == actual_keys[ i ]->two && actual_keys[ lastChosen ]->two == actual_keys[ i ]->one )
 				{
 					error = true;
 					break;
 				}
+			}
+			
+			if( actual_keys[ lastChosen ]->one == actual_keys[ lastChosen ]->two )
+			{
+				error = true;
 			}
 			
 			if( error )
@@ -357,21 +334,26 @@ void Keyboard::handle( sf::Event &event )
 				actual_keys[ lastChosen ] = keys[ lastChosen ];
 				
 				string newName = "";
-				newName += getName( actual_keys[ lastChosen ].a );
-				if( actual_keys[ lastChosen ].b > -1 )
-					newName +=  " + " + getName( actual_keys[ lastChosen ].b );
 				
-				text[ lastChosen *2 +1 ].setText( newName );
-				text[ lastChosen *2 +1 ].reloadPosition();
+				newName += getName( actual_keys[ lastChosen ]->one );
+				
+				if( actual_keys[ lastChosen ]->two > -1 )
+				{
+					newName +=  " + " + getName( actual_keys[ lastChosen ]->two );
+				}
+					
+				
+				text[ lastChosen *2 +1 ]->setText( newName );
+				text[ lastChosen *2 +1 ]->reloadPosition();
 			}
 			
 			lastChosen = -1;
 		}
 		
 		which = -1;
-		for( int i = 0; i < nr; i += 2 )
+		for( unsigned i = 0; i < text.size(); i += 2 )
 		{
-			if( text[ i ].checkCollision( x, y, 0, 5 ) )
+			if( text[ i ]->checkCollision( x, y, 0, 5 ) )
 			{
 				which = i;
 				lastChosen = which/2;
@@ -380,15 +362,202 @@ void Keyboard::handle( sf::Event &event )
 		}
 		
 		
-		for( int i = 0; i < nr; i += 2 )
+		for( unsigned i = 0; i < text.size(); i += 2 )
 		{
-			text[ i ].setColor( 0xFF, 0xFF, 0xFF );
+			text[ i ]->setColor( 0xFF, 0xFF, 0xFF );
 		}
 		
 		if( which != -1 )
 		{
-			text[ which ].setColor( 0xFF, 0xDE, 0x00 );
+			text[ which ]->setColor( 0xFF, 0xDE, 0x00 );
 		}
+	}
+	
+	handleButton( event );
+}
+
+
+
+void Keyboard::fadein( int j, int max )
+{
+	for( auto &it :text )
+	{
+		it->fadein( j, max );
+	}
+	
+	save_button.fadein( j, max );
+}
+
+void Keyboard::fadeout( int j, int min )
+{
+	for( auto &it :text )
+	{
+		it->fadeout( j, min );
+	}
+	
+	save_button.fadeout( j, min );
+}
+
+
+
+
+
+int Keyboard::strToInt( string s )
+{
+    bool m = false;
+    int tmp = 0;
+    unsigned i = 0;
+	
+    if( s[ 0 ] == '-' )
+    {
+          i++;
+          m = true;
+    }
+	
+    while( i < s.size() )
+    {
+      tmp = 10*tmp +s[ i ] -48;
+      i++;
+    }
+	
+    return m ? -tmp : tmp;   
+}
+
+string Keyboard::getName( int n )
+{
+	string name = "";
+	
+	if( n >= 0 && n <= 25 )	// from A to Z
+	{
+		// in ASCII 97 means letter 'a'
+		name = static_cast <char> ( n + 97 );
+	}
+	else
+	{
+		switch( n )
+		{
+			case 71: name = "left";	 	break;
+			case 72: name = "right";	break;
+			case 73: name = "up";		break;
+			case 74: name = "down";		break;
+			
+			case 57: name = "space";	break;
+			case 58: name = "return";	break;
+			
+			case 37: name = "l ctrl";	break;
+			case 41: name = "r ctrl";	break;
+			
+			case 38: name = "l shift";	break;
+			case 42: name = "r shift";	break;
+			
+			case 39: name = "l alt";	break;
+			case 43: name = "r alt";	break;
+		}
+	}
+	
+	return name;
+}
+
+
+
+void Keyboard::add( sf::Event &event )
+{
+	if( event.key.code == 67 ) // '+'
+	{
+		if( actual_keys[ which/2 ]->oneIsFree() )
+		{
+			addMode = true;
+			text[ which ]->setColor( 0x8C, 0xA9, 0x3E );	// green
+		}
+		else
+		{
+			addMode = false;
+		}
+	}
+}
+
+bool Keyboard::substract( sf::Event &event )
+{
+	if( event.key.code == 68 ) // '-'
+	{
+		if( actual_keys[ which/2 ]->two != -1 )
+		{
+			actual_keys[ which/2 ]->two = -1;
+		}
+		else if( actual_keys[ which/2 ]->one != -1 )
+		{
+			actual_keys[ which/2 ]->one = -1;
+		}
+		
+		
+		if( actual_keys[ which/2 ]->isEmpty() )
+		{
+			text[ which +1 ]->setText( "-" );
+			text[ which +1 ]->reloadPosition();
+		}
+		else
+		{
+			text[ which +1 ]->setText( getName( actual_keys[ which/2 ]->one ) );
+			text[ which +1 ]->reloadPosition();
+		}
+		
+		
+		// printf( "key nr %d, text nr %d\n", which/2, which +1 );
+		
+		text[ which ]->setColor( 0x8C, 0x00, 0x1A );	// red
+		addMode = false;
+		
+		return true;
+	}
+	
+	return false;
+}
+
+bool Keyboard::isPossibleKey( sf::Event &event )
+{
+	sf::Uint8 code = event.key.code;
+	
+	bool success = true;
+	for( unsigned i = 0; i < banned_keys.size(); i++ )
+	{
+		if( code == banned_keys[ i ] )
+		{
+			success = false;
+			break;
+		}
+	}
+	
+	if( success )
+	{
+		if( code > -1 && code <= 25 )
+		{
+			return true;
+		}
+		else if( code >= 71 && code <= 74 )
+		{
+			return true;
+		}
+		else if( code >= 37 && code <= 43 )
+		{
+			return true;
+		}
+		else if( code == 57 || code == 58 )
+		{
+			return true;
+		}
+	}
+
+	return false;
+}
+
+void Keyboard::handleButton( sf::Event &event )
+{
+	save_button.setOffset( 0 );
+	
+	if( event.type == sf::Event::MouseButtonPressed )
+	{
+		int x = event.mouseButton.x;
+		int y = event.mouseButton.y;
 		
 		if( save_button.checkCollision( x, y ) )
 		{
@@ -397,8 +566,8 @@ void Keyboard::handle( sf::Event &event )
 			
 			for( unsigned i = 0; i < actual_keys.size(); i++ )
 			{
-				file << to_string( actual_keys[ i ].a ) + "\n";
-				file << to_string( actual_keys[ i ].b ) + "\n";
+				file << to_string( actual_keys[ i ]->one ) + "\n";
+				file << to_string( actual_keys[ i ]->two ) + "\n";
 			}
 			
 			save_button.setOffset( 2 );
@@ -433,114 +602,4 @@ void Keyboard::handle( sf::Event &event )
 	{
 		save_button.setOffset( 2 );
 	}
-}
-
-
-
-void Keyboard::fadein( int j, int max )
-{
-	for( int i = 0; i < nr; i++ )
-	{
-		text[ i ].fadein( j, max );
-	}
-	
-	save_button.fadein( j, max );
-}
-
-void Keyboard::fadeout( int j, int min )
-{
-	for( int i = 0; i < nr; i++ )
-	{
-		text[ i ].fadeout( j, min );
-	}
-	
-	save_button.fadeout( j, min );
-}
-
-
-
-int Keyboard::strToInt(string s)
-{
-    bool m=false;
-    int tmp=0;
-    unsigned i=0;
-    if(s[0]=='-')
-    {
-          i++;
-          m = true;
-    }
-	
-    while(i<s.size())
-    {
-      tmp = 10*tmp+s[i]-48;
-      i++;
-    }
-	
-    return m ? -tmp : tmp;   
-}
-
-DoubleKey Keyboard::addKey( int a, int b )
-{
-	DoubleKey doubleKey;
-	doubleKey.a = a;
-	doubleKey.b = b;
-	
-	return doubleKey;
-}
-
-string Keyboard::getName( int n )
-{
-	string name = "";
-	
-	if( n <= 25 )	// from A to Z
-	{
-		// in ASCII 97 means letter 'a'
-		name = static_cast <char> ( n + 97 );
-	}
-	else
-	{
-		switch( n )
-		{
-			case 71: name = "left";	 	break;
-			case 72: name = "right";	break;
-			case 73: name = "up";		break;
-			case 74: name = "down";		break;
-			
-			case 57: name = "space";	break;
-			case 58: name = "return";	break;
-			
-			case 37: name = "l ctrl";	break;
-			case 41: name = "r ctrl";	break;
-			
-			case 38: name = "l shift";	break;
-			case 42: name = "r shift";	break;
-			
-			case 39: name = "l alt";	break;
-			case 43: name = "r alt";	break;
-		}
-	}
-	
-	return name;
-}
-
-
-
-bool DoubleKey::isEmpty()
-{
-	if( a == -1 && b == -1 )
-	{
-		return true;
-	}
-	
-	return false;
-}
-
-bool DoubleKey::oneIsFree()
-{
-	if( a == -1 || b == -1 )
-	{
-		return true;
-	}
-	
-	return false;
 }
