aa1d68a862e1d5a62595b4d06e64b71e0317e079
devmichalek <devmichalek@gmail.com>
2017-02-04 18:59:05 +0100

    new template, new object

--- to edit/engine.h
+++ to edit/engine.h
@@ -9,6 +9,8 @@
 #include "04_platform/halloween/play_halloween.h"
 #include "04_platform/desert/play_desert.h"
 #include "04_platform/winter/play_winter.h"
+#include "05_loading_world/loading_world.h"
+#include "06_panel/panel_menu.h"
 
 class Engine
 {
@@ -17,11 +19,14 @@ class Engine
 	Intro* intro;
 	Menu* menu;
 	Level_menu* level_menu;
+	Loading_world* loading_world;
 	
 	Play_wood* play_wood;
 	Play_halloween* play_halloween;
 	Play_desert* play_desert;
 	Play_winter* play_winter;
+	
+	Panel_menu* panel_menu;
 
 public:
 
@@ -35,4 +40,7 @@ public:
     void states();
 
     void loop();
+	
+	template <typename world>
+	void load_world( world w );
 };
