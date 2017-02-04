7b7110b375cb6c42a82212bbee6bf1d44915b613
devmichalek <devmichalek@gmail.com>
2017-02-04 18:59:16 +0100

    deleted comments

--- to edit/main.cpp
+++ to edit/main.cpp
@@ -1,17 +1,19 @@
+/**
+    main.cpp
+    Purpose: Create engine, call loop then delete.
+
+    @author Adrian Michalek
+    @version 2016.08.14
+	@email adrmic98@gmail.com
+*/
+
 #include "engine.h"
-#include <stdio.h>
 
 int main( int argc, char** argv )
 {
     Engine* engine = new Engine;
 
-    //if( !engine->load() )
-    //{
-    //    printf( "Not created engine!\n" );
-    //}
-
 	engine->loop();
-
 
     delete engine;
     engine = NULL;
