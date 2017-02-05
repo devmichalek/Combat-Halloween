8776b2e0418f5f2de4ab60fe9e772f92a0a3af7e
devmichalek <devmichalek@gmail.com>
2017-02-05 18:00:43 +0100

    greenery class to shows plants etc.

---
+++ to edit/04_platform/greenery/greenery.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include "drawable/sprite.h"
+#include "greenblock.h"
+#include "plant.h"
+
+class Greenery
+{
+	sf::Uint8 min;
+	vector <Plant*> plants;
+	vector <MySprite*> sprites;
+	vector <GreenBlock*> blocks;
+	
+public:
+	
+	// Basics.
+	Greenery();
+	~Greenery();
+	void free();
+	void reset( int distance );
+	
+	void load( int type );
+	void draw( sf::RenderWindow* &window, int screen_w );
+	void drawBG( sf::RenderWindow* &window, int screen_w );
+	
+	void fadein( int v = 1, int max = 0xFF );
+	void fadeout( int v = 1, int min = 0 );
+	
+	
+	// Creating
+	int to_int( string s );
+	unsigned getDistance( int v1, int v2 );
+	void positioning( vector <Block*> blocks );
+	
+	// In action
+	void moveX( sf::Uint8 direction, float vel );
+	void backToGrass( int add );
+};
