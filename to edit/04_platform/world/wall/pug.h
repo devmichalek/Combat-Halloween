b08ac414d42630721d9547dc51ce433cdce6ea14
devmichalek <devmichalek@gmail.com>
2017-02-07 23:07:17 +0100

    pug as boxer as moving blocks which can harm you, wall as container (huge)

---
+++ to edit/04_platform/world/wall/pug.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include "04_platform/world/brick/block.h"
+#include <vector>
+
+using namespace std;
+
+class Pug
+{
+	int vel;
+	bool hit;
+	int state;
+	int startY;
+	int surplus;
+	
+	vector <Block*> blocks;
+	
+public:
+
+	Pug();
+	~Pug();
+	void free();
+	
+	// Support.
+	void addBlock( int n, int x, int y );
+	
+	void positioning( int width );
+	void setPosition( int x, int y, int vel, int surplus = 0 );
+	void moveX( float vel );
+	void moving( int width );
+	
+	unsigned getSize();
+	int getX( unsigned which );
+	int getY( unsigned which );
+	int getNr( unsigned which );
+	
+	bool harm( int width );
+};
---
+++ to edit/04_platform/world/wall/wall.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include "drawable/sprite.h"
+#include "pug.h"
+
+class Wall
+{
+	sf::Uint8 min;
+	int width;
+	int screen_w;
+	
+	vector <Pug*> blocks;
+	vector <MySprite*> sprites;
+	
+public:
+	
+	// Basics.
+	Wall();
+	~Wall();
+	void free();
+	void reset( int distance );
+	void load( int type, int width, int screen_w );
+	void draw( sf::RenderWindow* &window );
+	void fadein( int v = 1, int max = 0xFF );
+	void fadeout( int v = 1, int min = 0 );
+	
+	
+	// Creating
+	void positioning( vector <Block*> blocks, int chance );
+	
+	// In action
+	void moveX( sf::Uint8 direction, float vel );
+	void backToGrass( int add );
+	
+	// Check collision.
+	bool harm( Rect* rect );
+	bool checkCollision( Rect* rect );
+};
