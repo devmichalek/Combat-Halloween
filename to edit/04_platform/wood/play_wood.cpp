fee2ec546d5ac7fd6af51538022c216412b00881
devmichalek <devmichalek@gmail.com>
2017-02-07 23:03:30 +0100

    wood is readygit commit -am deleted!

--- to edit/04_platform/wood/play_wood.cpp
+++ to edit/04_platform/wood/play_wood.cpp
@@ -5,17 +5,22 @@ Play_wood::Play_wood()
 	state = 0;
 	info = "";
 	
-	// Create play_wood objects
+	screen_w = 0;
+	screen_h = 0;
+	
 	hero = new Hero;
-	moving_bg = new Moving_bg;
-	brick = new Brick;
 	kunai = new Kunai;
 	heart = new Heart;
 	scope = new Scope;
-	greenery = new Greenery;
-	ladder = new Ladder;
+	
+	brick = new Brick;
+	effect = new Effect;
+	background = new Background;
+	islands = new Islands;
+	water = new Water;
 	wall = new Wall;
-	bg = new Bg;
+	ladder = new Ladder;
+	greenery = new Greenery;
 }
 
 Play_wood::~Play_wood()
@@ -28,50 +33,69 @@ void Play_wood::free()
 	state = 0;
 	info = "";
 	
+	screen_w = 0;
+	screen_h = 0;
+	
 	sound.free();
+	
 	delete hero;
-	delete moving_bg;
-	delete brick;
 	delete kunai;
 	delete heart;
 	delete scope;
-	delete greenery;
-	delete ladder;
+	
+	
+	delete brick;
+	delete effect;
+	delete background;
+	delete islands;
+	delete water;
 	delete wall;
-	delete bg;
+	delete ladder;
+	delete greenery;
 }
 
 void Play_wood::reset()
 {
 	state = 0;
+	
 	hero->reset( screen_h );
 	hero->setKeys();
-	moving_bg->setXY( hero->getX(), hero->getY() );
-	int distance = brick->reset();
-	greenery->reset( distance );
-	ladder->reset( distance );
-	wall->reset( distance );
 	heart->reset();
 	scope->reset();
-	bg->reset();
+	
+	int distance = brick->reset();
+	effect->reset();
+	background->reset( hero->getX(), hero->getY() );
+	islands->reset( distance );
+	water->reset( distance );
+	wall->reset( distance );
+	ladder->reset( distance );
+	greenery->reset( distance );
 }
 
 
 
 void Play_wood::load( int screen_w, int screen_h )
 {
+	state = 0;
+	info = "setting keys";
+	
+	int type = 1;
+	int width = 128;
 	this->screen_w = screen_w;
 	this->screen_h = screen_h;
 	
-	moving_bg->load( "data/sprites/play/1.png", screen_w, screen_h );
-	brick->load( screen_w, screen_h, 1 );
 	kunai->load();
 	heart->load();
-	greenery->load( 1 );
-	ladder->load( 1 );
-	wall->load( 1 );
-	bg->load( screen_w, screen_h );
-	info = "loading hero";
+	
+	brick->load( type, width, screen_w, screen_h );
+	effect->load( screen_w, screen_h );
+	background->load( type, screen_w, screen_h );
+	islands->load( type, width, screen_w, screen_h );
+	water->load( type, width, screen_w, screen_h );
+	wall->load( type, width, screen_w );
+	ladder->load( type, width, screen_w );
+	greenery->load( type, width, screen_w );
 }
 
 void Play_wood::handle( sf::Event &event )
@@ -85,143 +109,137 @@ void Play_wood::draw( sf::RenderWindow* &window )
 	
 	if( hero->isDead() )
 	{
-		sf::Uint8 v = 1;
-		moving_bg->fadeout( v );
-		brick->fadeout( v );
-		hero->fadeout( v );
-		kunai->fadeout( v );
-		heart->fadeout( v );
-		greenery->fadeout( v );
-		ladder->fadeout( v );
-		wall->fadeout( v );
-		bg->fadeout( v );
+		sf::Uint8 value = 1;
+		hero->fadeout( value );
+		kunai->fadeout( value );
+		heart->fadeout( value );
+		
+		brick->fadeout( value );
+		effect->fadeout( value );
+		background->fadeout( value );
+		islands->fadeout( value );
+		water->fadeout( value );
+		wall->fadeout( value );
+		ladder->fadeout( value );
+		greenery->fadeout( value );
 	}
 	else
 	{
-		sf::Uint8 v = 2;
-		moving_bg->fadein( v );
-		brick->fadein( v );
-		hero->fadein( v );
-		kunai->fadein( v );
-		heart->fadein( v );
-		greenery->fadein( v );
-		ladder->fadein( v );
-		wall->fadein( v );
+		sf::Uint8 value = 2;
+		hero->fadein( value );
+		kunai->fadein( value );
+		heart->fadein( value );
+		
+		brick->fadein( value );
+		effect->fadein( value );
+		background->fadein( value );
+		islands->fadein( value );
+		water->fadein( value );
+		wall->fadein( value );
+		ladder->fadein( value );
+		greenery->fadein( value );
 	}
 	
 
+	// bg
+	background->draw( window );
+	greenery->draw_bg( window );
 	
-	moving_bg->draw( window );
-	greenery->drawBG( window, screen_w );
-	ladder->draw( window, screen_w );
+	// blocks
+	ladder->draw( window );
+	
 	hero->draw( window );
-	wall->draw( window, screen_w );
 	kunai->draw( window );
+	
+	// rest
+	water->draw( window );
 	brick->draw( window );
-	greenery->draw( window, screen_w );
+	islands->draw( window );
+	wall->draw( window );
+	greenery->draw( window );
 	heart->draw( window );
-	bg->draw( window );
+	effect->draw( window );
 }
 
 
 
-bool Play_wood::positioning( int size, int flatness, int type )
+bool Play_wood::positioning( int type, int size, int flatness, int flying_is, int pug  )
 {
 	switch( state )
 	{
-		case 0: 
-		info = "setting keys";
-		hero->load( screen_w, screen_h, "data/sprites/hero/" + to_string( type ) + "/" );
-		break;
+		case 0:	hero->load( type, screen_w, screen_h ); hero->setKeys();
+		info = "setting position x, y of background";	break;
 		
-		case 1:
-		info = "setting position x, y of background";
-		hero->setKeys();
-		break;
+		case 1:	background->setPosition( hero->getX(), hero->getY() );
+		info = "reserving memory (it can take a while)";	break;
 		
-		case 2:
-		info = "reserving memory (it can take a while)";
-		moving_bg->setXY( hero->getX(), hero->getY() );
-		break;
+		case 2:	brick->reserve( size );
+		info = "creating top borders of hill";	break;
 		
-		case 3:
-		info = "creating top border of hills";
-		brick->reserve( size );
-		break;
+		case 3:	brick->createTopBorders( size, flatness, ladder->getW( 0 ), ladder->getH( 0 ) );
+		info = "creating flying islands";	break;
 		
-		case 4:
-		info = "creating left border of hills";
-		brick->createTopBorders( size, flatness, ladder->getW( 0 ), ladder->getH( 0 ) );
-		break;
+		case 4:	islands->createFlyingIslands( brick->getBlocks(), brick->getPlanks(), flying_is );
+		info = "creating left borders of hill";	break;
 		
-		case 5:
-		info = "creating right border of hills";
-		brick->createLeftBorders();
-		break;
 		
-		case 6:
-		info = "setting left x of world";
-		brick->createRightBorders();
-		break;
 		
-		case 7:
-		info = "setting right x of world";
-		brick->setLeft();
-		break;
+		case 5:	brick->createLeftBorders();
+		info = "creating right borders of hill";	break;
+
+		case 6:	brick->createRightBorders();
+		info = "setting the smallest x of world";	break;
 		
-		case 8:
-		info = "filling hills step 1";
-		brick->setRight();
-		break;
 		
-		case 9:
-		info = "filling hills step 2";
-		brick->createStuffing( 10, 11 );
-		break;
 		
-		case 10:
-		info = "filling hills step 3";
-		brick->createStuffing( 14, 11 );
-		break;
+		case 7:	brick->setLeft();
+		info = "setting the biggest x of world";	break;
 		
-		case 11:
-		info = "creating top islands";
-		brick->createStuffing( 8, 15 );
-		break;
-	
-		case 12:
-		info = "creating bottom islands";
-		brick->createTopIslands( ladder->getW( 1 ), ladder->getH( 1 ) );
-		break;
+		case 8:	brick->setRight();
+		info = "filling hills step 1";	break;
 		
-		case 13:
-		info = "creating water";
-		brick->createBotIslands( ladder->getW( 1 ), ladder->getH( 1 ) );
-		break;
 		
-		case 14:
-		info = "shrink to fit vector";
-		brick->createWater();
-		break;
 		
-		case 15:
-		info = "setting ladders";
-		brick->shrink();
-		break;
-	
-		case 16:
-		info = "setting greenery";
-		ladder->positioning( brick->getPlanks() );
-		break;
+		case 9:		brick->createStuffing( 10, 11 );
+		info = "filling hills step 2";	break;
 		
-		case 17:
-		info = "setting wall";
-		wall->positioning( brick->getBlocks(), screen_w, brick->getRight() -screen_w );
+		case 10:	brick->createStuffing( 14, 11 );
+		info = "filling hills step 3";	break;
 		
-		case 18:
-		info = "done";
-		greenery->positioning( brick->getBlocks() );
-		break;
+		case 11:	brick->createStuffing( 8, 15 );
+		info = "creating top islands";	break;
+		
+		
+		
+		case 12:	islands->createTopIslands( brick->getBlocks(), ladder->getW( 1 ), ladder->getH( 1 ) );
+		info = "creating bot islands";	break;
+		
+		case 13:	islands->createBotIslands( brick->getBlocks(), ladder->getW( 1 ), ladder->getH( 1 ) );
+		info = "creating water";	break;
+		
+		
+		
+		case 14:	water->createWater( brick->getBlocks(), islands->getBlocks(), brick->getRight() );
+		info = "shrink to fit vector of blocks";	break;
+		
+		
+		
+		case 15:	brick->shrink();
+		info = "setting ladders";	break;
+		
+		
+		
+		case 16:	ladder->positioning( brick->getPlanks() );	ladder->positioning( islands->getPlanks() );
+		info = "setting greenery";	break;
+		
+		case 17:	greenery->positioning( brick->getBlocks() );	greenery->positioning( islands->getBlocks() );
+		info = "setting wall";	break;
+		
+		
+		
+		case 18:	wall->positioning( brick->getBlocks(), pug );
+		info = "done";	break;
+		
 		
 		default:
 		return true;
@@ -242,7 +260,7 @@ string Play_wood::getInfo()
 
 bool Play_wood::nextState()
 {
-	if( hero->isDead() && moving_bg->getAlpha() == 0 )
+	if( hero->isDead() && background->getAlpha() == 0 )
 	{
 		return true;
 	}
