487c2463af2aa4bab31574eeb593fc176e1a7701
devmichalek <devmichalek@gmail.com>
2017-02-09 21:30:05 +0100

    mine as new enemy

---
+++ to edit/04_platform/enemy/mine/mine.h
@@ -0,0 +1,50 @@
+#pragma once
+
+#include "04_platform/world/brick/block.h"
+#include <SFML/Graphics/Color.hpp>
+
+class Mine
+{
+	sf::Uint8 nr;
+	int x1, x2, y;
+	int state;
+	int offset;
+	sf::Uint8 delay;
+	sf::Uint8 counter;
+	
+	sf::Uint8 line_one;
+	sf::Uint8 line_two;
+	
+	bool harmed;
+	
+public:
+
+	Mine();
+	~Mine();
+	void free();
+	void reset();
+	
+	// Getters.
+	int getX();
+	int getY();
+	
+	// Set as active.
+	void setActive();
+	
+	// In action.
+	void moveX( int vel );
+	
+	
+	
+	
+	bool harm();
+	void mechanics();
+	void positioning( int x1, int x2, int y );
+	
+	bool ableToDraw();
+	bool ableToDestroy();
+	
+	// getters
+	sf::Uint8 getNr();
+	sf::Uint8 getOffset();
+};
---
+++ to edit/04_platform/enemy/mine/mine_factory.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include "drawable/sprite.h"
+#include <vector>
+#include "04_platform/enemy/mine/mine.h"
+
+class Mine_factory
+{
+	int width;
+	int screen_w;
+	int screen_h;
+	
+	vector <Mine*> mines;
+	vector <MySprite*> sprites;
+	
+public:
+	
+	// Basics.
+	Mine_factory();
+	~Mine_factory();
+	void free();
+	void reset( int distance );
+	
+	void load( int width, int screen_w, int screen_h );
+	void draw( sf::RenderWindow* &window );
+	
+	void fadein( int v = 1, int max = 0xFF );
+	void fadeout( int v = 1, int min = 0 );
+	
+	// Creating.
+	void addMine( int x1, int x2, int y );
+	void positioning( vector <Block*> blocks, int chance );
+	
+	// Collision.
+	void checkCollision( Rect* rect );
+	bool harm( Rect* rect );
+	
+	// In action
+	void moveX( sf::Uint8 direction, float vel );
+	void backToGrass( int add );
+};
