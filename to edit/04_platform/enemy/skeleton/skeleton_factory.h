973b064cdde6b43d4200cb8f35888852ae2942d4
devmichalek <devmichalek@gmail.com>
2017-02-17 14:33:56 +0100

    one of the biggest classes in game, contain all skeleton types monsters, controling collision, controling damage, harmness, drawing, fadein fadeout, uses sword to harm hero etc.....

--- to edit/04_platform/enemy/skeleton/skeleton_factory.h
+++ to edit/04_platform/enemy/skeleton/skeleton_factory.h
@@ -9,8 +9,12 @@ class Skeleton_factory
 	int screen_w;
 	int screen_h;
 	
-	vector <int> line;
-	vector <int> widths;
+	// life stuff
+	MySprite life;
+	MySprite frame;
+	
+	vector <Skeleton*> ::iterator sword;
+	vector <sf::Uint8> line;
 	vector <MySprite*> sprites;
 	vector <Skeleton*> skeletons;
 
@@ -21,25 +25,31 @@ public:
 	~Skeleton_factory();
 	void free();
 	void reset( int distance );
-	
 	void load( int width, int screen_w, int screen_h );
 	void draw( sf::RenderWindow* &window );
-	
 	void fadein( int v = 1, int max = 0xFF );
 	void fadeout( int v = 1, int min = 0 );
 	
 	// Creating.
-	void addSkeleton( int x, int y );
+	void addSkeleton( int x, int y, int damage );
 	void positioning( vector <Block*> blocks, int chance );
 	
 	// Collision.
-	void appear( int heroX );
-	void walk( int heroX, int heroY );
-	bool harm( Rect* rect );
+	void appear( Rect* rect );
+	void walk( Rect* rect );
+	bool harm( Rect* rect, int damage );
 	void ableAttack( Rect* rect );
+	bool isSword( Rect* rect );
+	int getDamage();
 	
 	// In action
 	void moveX( sf::Uint8 direction, float vel );
 	void undoFall( sf::Uint8 add );
+	void mechanics();
 	void setColor( sf::Color color );
+	
+	// Connecting with other life
+	unsigned getSize();
+	Rect* getRect( int which );
+	void harmDefinite( int which, int damage );
 };
