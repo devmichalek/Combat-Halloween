38757c37b9e02c763167392f0609bd732c6e1ac2
devmichalek <devmichalek@gmail.com>
2017-02-17 14:38:30 +0100

    set damage basing on chance of vege

--- to edit/04_platform/world/wall/wall.cpp
+++ to edit/04_platform/world/wall/wall.cpp
@@ -8,6 +8,7 @@ Wall::Wall()
 	min = 0;
 	width = 0;
 	screen_w = 0;
+	damage = 0;
 }
 
 Wall::~Wall()
@@ -20,6 +21,7 @@ void Wall::free()
 	min = 0;
 	width = 0;
 	screen_w = 0;
+	damage = 0;
 	
 	if( !blocks.empty() )
 	{
@@ -87,8 +89,6 @@ void Wall::draw( sf::RenderWindow* &window )
 {
 	for( auto &it :blocks )
 	{
-		it->moving( width );
-		
 		for( unsigned j = 0; j < it->getSize(); j++ )
 		{
 			if( it->getX(j) > -width*2 && it->getX(j) < screen_w +width*2 )
@@ -122,41 +122,53 @@ void Wall::fadeout( int v, int min )
 
 void Wall::positioning( vector <Block*> blocks, int chance )
 {
+	damage = chance;
+	
 	for( unsigned i = 0; i < blocks.size() -2; i++ )
 	{
 		if( blocks[ i ]->y <= 3*width && blocks[ i ]->x > screen_w )
 		{
-			if( (blocks[ i ]->nr == 1 || blocks[ i ]->nr == 6 || blocks[ i ]->nr == 5 || blocks[ i ]->nr == 0) &&
-				blocks[ i ]->y == blocks[ i +2 ]->y )
+			if( (blocks[ i ]->nr == 5 || blocks[ i ]->nr == 0 || blocks[ i ]->nr == 1 || blocks[ i ]->nr == 6) &&
+				(blocks[ i +2 ]->nr == 2 || blocks[ i +2 ]->nr == 7))
 			{
+				int nr = 0;
+				
 				if( rand()%100 < chance )
 				{
-					int nr = 0;
-					if( rand()%2 == 0 )
-					{
-						nr = 1;
-					}
-					
 					this->blocks.push_back( new Pug() );
 					this->blocks[ this->blocks.size()-1 ]->positioning( width, nr );
 					
-					int vel = 1;
-					
-					if( nr == 1 )
-					{
-						if( rand()%2 == 1 )	vel = 4;
-						else				vel = 3;
-					}
-					else if( rand()%2 == 1 ) vel = 2;
+					int vel = 0;
+					if( rand()%2 == 1 )	vel = 1;
+					else				vel = 2;
 					
 					if( vel > 1 )
 					{
-						this->blocks[ this->blocks.size()-1 ]->setPosition( blocks[ i ]->x, blocks[ i ]->y -width, vel, width );
+						this->blocks[ this->blocks.size()-1 ]->setPosition( blocks[ i ]->x +width/2, blocks[ i ]->y -width, vel, width );
 					}
 					else
 					{
-						this->blocks[ this->blocks.size()-1 ]->setPosition( blocks[ i ]->x, blocks[ i ]->y -width, vel );
+						this->blocks[ this->blocks.size()-1 ]->setPosition( blocks[ i ]->x +width/2, blocks[ i ]->y -width, vel );
 					}
+					
+					i += 3;
+				}
+			}
+				
+			else if( blocks[ i ]->nr == 1 || blocks[ i ]->nr == 6 )
+			{
+				int nr = 1;
+				
+				if( rand()%100 < chance )
+				{
+					this->blocks.push_back( new Pug() );
+					this->blocks[ this->blocks.size()-1 ]->positioning( width, nr );
+					
+					int vel = 0;
+					if( rand()%2 == 1 )	vel = 4;
+					else				vel = 3;
+					
+					this->blocks[ this->blocks.size()-1 ]->setPosition( blocks[ i ]->x, blocks[ i ]->y -width, vel, width );
 					
 					i += 3;
 				}
@@ -190,6 +202,14 @@ void Wall::undoFall( sf::Uint8 add )
 	for( auto &it :blocks )
 	{
 		it->moveX( add );
+	}
+}
+
+void Wall::mechanics()
+{
+	for( auto &it :blocks )
+	{
+		it->moving( width );
 	}
 }
 
@@ -254,4 +274,14 @@ bool Wall::checkCollision( Rect* rect )
 	}
 	
 	return false;
+}
+
+int Wall::getDamage()
+{
+	return damage;
+}
+
+int Wall::getFallDamage()
+{
+	return damage *2;
 }
