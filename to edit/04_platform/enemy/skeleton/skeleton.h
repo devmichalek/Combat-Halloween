5ce6fe55ff3d3146bbeba766e14012ce13badb23
devmichalek <devmichalek@gmail.com>
2017-02-17 14:32:58 +0100

    getting heart points with security of alpha level

--- to edit/04_platform/enemy/skeleton/skeleton.h
+++ to edit/04_platform/enemy/skeleton/skeleton.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "04_platform/world/brick/block.h"
+#include "drawable/rect.h"
 #include <SFML/Graphics/Color.hpp>
 #include <vector>
 
@@ -8,27 +9,51 @@ using namespace std;
 
 class Skeleton
 {
-	int state;
-	int offset;
-	int delay;
-	int counter;
+	enum
+	{
+		APPEAR = 0,
+		IDLE,
+		WALK,
+		ATTACK,
+		DEAD
+	};
+	
+	int8_t state;
+	sf::Uint8 offset;
+	sf::Uint8 delay;
+	sf::Uint8 counter;
+	vector <sf::Uint8> line;
+	
+	// pos x
+	int left;
+	int right;
+	
+	// heart points
+	int hp;
+	int hp_state;
+	
+	// damage
+	sf::Uint8 damage;
+	
+	// velocity
+	float vel;
+	
+	// direction
+	sf::Uint8 direction;
+	
+	// scale
+	float xScale, yScale;
+	
+	// attack stuff
+	sf::Uint8 attack;
+	sf::Uint8 attack_line;
+	sf::Uint8 attack_count;
 	
 	vector <float> y;
 	vector <float> x;
-	vector <int> line;
 	vector <int> width;
-	
-	int startX;
-	int endX;
-	
-	int hp;
-	int hp_state;
-	sf::Uint8 damage;
-	float vel;
-	sf::Uint8 direction;
-	
-	sf::Uint8 attack;
-	sf::Uint8 attack_line;
+	vector <int> height;
+	bool flag;
 	
 public:
 	
@@ -38,30 +63,50 @@ public:
 	void reset( int distance );
 	
 	// Setters.
-	void setPosition( vector <float> x, vector <float> y );
-	void setLine( vector <int> line, vector <int> width );
-	void setDamage( int damage );
-	void setHP( int hp );
-	void setGlobalX( int startX, int endX );
+	void setX( vector <float> x );
+	void setY( vector <float> y );
+	void setLine( vector <sf::Uint8> line );
+	void setWidth( vector <int> width );
+	void setHeight( vector <int> height );
+	void setVelocity( float vel );
+	void setDelay( sf::Uint8 delay );
+	void setDamage( sf::Uint8 damage );
+	void setHeartPoints( int hp );
+	void setBorders( int left, int right );
+	void setAttackLine( sf::Uint8 attack_line );
+	void setScale( float xScale, float yScale );
+	void setDead();
 	
 	
 	// Getters.
 	float getX();
 	float getY();
+	
+	float getRealX();
+	float getRealY();
+	float getRealWidth();
+	float getRealHeight();
+	
+	float getAttackX();
+	float getAttackY();
+	int getAttackWidth();
+	int getAttackHeight();
+	
+	int8_t getState();
+	sf::Uint8 getOffset();
 	sf::Uint8 getDamage();
-	int getOffset();
-	int getNr();
-	int get_x_scale();
-	bool harmAvailable();
+	float getHorizontalScale();
+	float getVerticalScale();
+	int getHeartPoints();
+	bool isAlive();
+	bool isSword();
 	
 	
-	// Main
-	void appear( int heroX );
-	bool alive();
-	void walk( int heroX, int heroY, int w );
-	void mechanics( int w );
+	// Activity.
+	void appear( Rect* rect );
+	void walk( Rect* rect );
+	void mechanics();
 	void ableAttack();
-	
-	// In action.
 	void moveX( int vel );
+	void harm( int damage );
 };
