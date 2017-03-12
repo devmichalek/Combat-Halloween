1133d9ecb4a79ca900836a8d8ac2ce7fa0288f3f
devmichalek <devmichalek@gmail.com>
2017-03-12 16:47:25 +0100

    remove not necessery stuff, inheritance

--- to edit/04_platform/enemy/vampire/vampire.h
+++ to edit/04_platform/enemy/vampire/vampire.h
@@ -13,28 +13,9 @@ class Vampire :public Skeleton
 	};
 	
 public:
-	
-	Vampire();
-	~Vampire();
-	void free();
-	void reset( int distance );
-	
+
 	// Setters.
-	void setX( vector <float> x );
-	void setX2( vector <float> x2 );
-	void setY( vector <float> y );
-	void setLine( vector <sf::Uint8> line );
-	void setWidth( vector <int> width );
-	void setHeight( vector <int> height );
-	void setVelocity( float vel );
-	void setDelay( sf::Uint8 delay );
-	void setDamage( sf::Uint8 damage );
-	void setHeartPoints( int hp );
-	void setBorders( int left, int right );
-	void setAttackLine( sf::Uint8 attack_line );
-	void setScale( float xScale, float yScale );
 	void setDead();
-	
 	
 	// Getters.
 	float getX();
@@ -43,22 +24,14 @@ public:
 	float getRealX();
 	float getRealY();
 	float getRealWidth();
-	float getRealHeight();
 	
 	float getAttackX();
 	float getAttackY();
 	int getAttackWidth();
 	int getAttackHeight();
-	int getLeft();
-	int getRight();
-	int getPlane();
 	
 	int8_t getState();
 	sf::Uint8 getOffset();
-	sf::Uint8 getDamage();
-	float getHorizontalScale();
-	float getVerticalScale();
-	int getHeartPoints();
 	bool isAlive();
 	bool harmSomebody();
 	
@@ -68,6 +41,4 @@ public:
 	void walk( Rect* rect );
 	void mechanics();
 	void ableAttack();
-	void moveX( int vel );
-	void harm( int damage );
 };
