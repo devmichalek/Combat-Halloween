4ab5f190c703625051f91c4c294dc23444e11284
devmichalek <devmichalek@gmail.com>
2017-03-19 22:58:53 +0100

    snow balls with new radius

--- to edit/04_platform/world/weather/snow/snow_cloud.cpp
+++ to edit/04_platform/world/weather/snow/snow_cloud.cpp
@@ -1,27 +1,93 @@
 #include "04_platform/world/weather/snow/snow_cloud.h"
 #include <cstdlib>
 
+void Snow_cloud::free()
+{
+	x = 0;
+	vel = 0;
+	
+	cloud.free();
+	
+	if( !alphas.empty() )
+	{
+		alphas.clear();
+	}
+	
+	if( !drops.empty() )
+	{
+		for( auto &it :drops )
+		{
+			delete it;
+		}
+		
+		drops.clear();
+	}
+}
+
+void Snow_cloud::reset( int screen_w, int screen_h )
+{
+	setVel();
+	x = static_cast <float> ( rand()%screen_w ) ;
+	
+	cloud.setPosition( x, -rand()%40 );
+	for( auto &it :drops )
+	{
+		it->setPosition( x +rand()%cloud.getWidth(), cloud.getY() +rand()%screen_h );
+	}
+}
+
+void Snow_cloud::draw( sf::RenderWindow* &window )
+{
+	for( auto &it :drops )
+	{
+		window->draw( *it );
+	}
+	
+	window->draw( cloud.get() );
+}
+
+void Snow_cloud::fadeout( int v, int min )
+{
+	cloud.fadeout( v, min );
+	
+	for( auto &it :alphas )
+	{
+		it -= v;
+		if( it < min )
+		{
+			it = min;
+			if( it < 0 )
+			{
+				it = 0;
+			}
+		}
+	}
+	
+	for( unsigned i = 0; i < drops.size(); i++ )
+	{
+		sf::Color c = drops[ i ]->getFillColor();
+		c.a = alphas[ i ];
+		drops[ i ]->setFillColor( c );
+	}
+}
 
 void Snow_cloud::create( string path, int screen_w, int screen_h )
 {
 	cloud.setName( "snow_cloud-cloud" );
 	cloud.load( path );
 	
-	int amount = rand()%40 +50;
+	int amount = rand()%40 +20;
 	for( int i = 0; i < amount; i++ )
 	{
-		drops.push_back( new sf::ConvexShape() );
-		drops[ drops.size()-1 ]->setPointCount( 4 );
-		drops[ drops.size()-1 ]->setPoint( 0, sf::Vector2f( 0, 0 ) );
-		drops[ drops.size()-1 ]->setPoint( 1, sf::Vector2f( 3, 0 ) );
-		drops[ drops.size()-1 ]->setPoint( 2, sf::Vector2f( 3, 3 ) );
-		drops[ drops.size()-1 ]->setPoint( 3, sf::Vector2f( 0, 3 ) );
+		drops.push_back( new sf::CircleShape( static_cast <float> (rand()%3 +1) ) );
 		alphas.push_back( rand()%100 +100 );
 		drops[ drops.size()-1 ]->setFillColor( sf::Color( 0xFF, 0xFF, 0xFF, alphas[ alphas.size() -1 ] ) );
 	}
 	
 	reset( screen_w, screen_h );
-	// printf( "%f\n", drops[ 0 ]->getPosition().x );
+	
+	// printf( "%d\n", drops.size() );
+	// printf( "%f  %f  %f\n", drops[ 0 ]->getPosition().x, drops[ 0 ]->getPosition().y, drops[ 0 ]->getRadius() );
 }
 
 
@@ -57,4 +123,17 @@ void Snow_cloud::mechanics( int screen_w, int screen_h )
 			it->setPosition( cloud.getX() +rand()%cloud.getWidth(), cloud.getY() +rand()%cloud.getHeight() );
 		}
 	}
+}
+
+void Snow_cloud::moveX( int add )
+{
+	x += add;
+	cloud.setPosition( x, cloud.getY() );
+	for( auto &it :drops )
+	{
+		it->setPosition( it->getPosition().x +add, it->getPosition().y );
+	}
+	
+	// printf( "--%d\n", drops.size() );
+	// printf( "%f %f\n", drops[ 0 ]->getPosition().x, drops[ 0 ]->getPosition().y );
 }
--- to edit/04_platform/world/weather/snow/snow_cloud.h
+++ to edit/04_platform/world/weather/snow/snow_cloud.h
@@ -4,10 +4,20 @@
 
 class Snow_cloud :public Rain_cloud
 {
+	vector <sf::CircleShape*> drops;
+	
 public:
 	
 	using Rain_cloud::Rain_cloud;
 	
+	void free();
+	void reset( int screen_w, int screen_h );
+	
+	void draw( sf::RenderWindow* &window );
+	void fadeout( int v, int min );
+	
 	void create( string path, int screen_w, int screen_h );
 	void mechanics( int screen_w, int screen_h );
+	
+	void moveX( int add );
 };
