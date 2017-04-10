8f429a06c8ad1ad66a7bf3b3f7f28be3f16fecea
devmichalek <devmichalek@gmail.com>
2017-04-10 13:41:46 +0200

    Show_scores class

---
+++ to edit/06_panel/show_scores.cpp
@@ -0,0 +1,86 @@
+#include "show_scores.h"
+#include "file/file.h"
+
+Show_scores::Show_scores()
+{
+	free();
+}
+
+Show_scores::~Show_scores()
+{
+	free();
+}
+
+void Show_scores::free()
+{
+	screen_w = 0;
+	screen_h = 0;
+	
+	max = min = 0;
+	n = 0;
+	vel = 0;
+	text.free();
+}
+
+void Show_scores::reset()
+{
+	n = min;
+	vel = 1;
+	
+	text.setText( con::itos( n ) );
+	text.center( screen_w, screen_h/3 );
+}
+
+
+void Show_scores::load( int screen_w, int screen_h )
+{
+	free();
+	
+	this->screen_w = screen_w;
+	this->screen_h = screen_h;
+	
+	text.setName( "show_scores-text" );
+	text.setFont( "data/00_loading/Jaapokki-Regular.otf", 70, 0xFF, 0xFF, 0xFF );
+	
+	reset();
+}
+
+void Show_scores::draw( sf::RenderWindow* &window )
+{
+	window->draw( text.get() );
+	
+	if( text.getAlpha() == 0xFF )
+	{
+		mechanics();
+	}
+}
+
+void Show_scores::mechanics()
+{
+	if( n < max )
+	{
+		n += vel;
+		if( n > max )	n = max;
+		text.setText( con::itos( n ) );
+		text.center( screen_w, screen_h/3 );
+	}
+}
+
+void Show_scores::set( int scores )
+{
+	min = 0;
+	max = scores;
+	vel += scores /500;
+	// printf( "%d\n", max );
+}
+
+
+void Show_scores::fadein( int v, int max )
+{
+	text.fadein( v, max );
+}
+
+void Show_scores::fadeout( int v, int min )
+{
+	text.fadeout( v, min );
+}
---
+++ to edit/06_panel/show_scores.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "drawable/sprite.h"
+#include "drawable/text.h"
+
+class Show_scores
+{
+	int screen_w;
+	int screen_h;
+	int max, min;
+	int n;
+	int vel;
+	MyText text;
+	
+public:
+	
+	Show_scores();
+	~Show_scores();
+	void free();
+	void reset();
+	
+	void load( int screen_w, int screen_h );
+	void draw( sf::RenderWindow* &window );
+	void mechanics();
+	void set( int scores );
+	
+	void fadein( int v = 1, int max = 0xFF );
+	void fadeout( int v = 1, int min = 0 );
+};
