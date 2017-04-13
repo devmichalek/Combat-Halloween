66e728f38ac9f98c50ebccd2367fe0afb1f3bf29
devmichalek <devmichalek@gmail.com>
2017-04-13 16:19:12 +0200

    Play state accelerated with menu

--- to edit/04_platform/02_winter/winter.cpp
+++ to edit/04_platform/02_winter/winter.cpp
@@ -27,6 +27,7 @@ Winter::Winter()
 	scores = new Scores;
 	money = new Money;
 	pause = new Pause;
+	sound_pad = new Sound_pad;
 	
 	// Actions.
 	hp_dots = new Hp_dots;
@@ -88,6 +89,7 @@ void Winter::free()
 	delete scores;
 	delete money;
 	delete pause;
+	delete sound_pad;
 	
 	// Actions.
 	delete hp_dots;
@@ -189,6 +191,7 @@ void Winter::load( int screen_w, int screen_h, unsigned FPS )
 	scores->load( type, screen_w );
 	money->load( screen_w );
 	pause->load( screen_w, screen_h );
+	sound_pad->load( screen_w, screen_h );
 	
 	// Actions.
 	hp_dots->load( type, screen_w );
@@ -222,6 +225,7 @@ void Winter::load( int screen_w, int screen_h, unsigned FPS )
 void Winter::handle( sf::Event &event )
 {
 	pause->handle( event );
+	sound_pad->handle( event );
 }
 
 void Winter::draw( sf::RenderWindow* &window )
@@ -262,6 +266,7 @@ void Winter::draw( sf::RenderWindow* &window )
 		heart->fadeout( value );
 		scores->fadeout( value );
 		money->fadeout( value );
+		sound_pad->fadeout( value );
 		
 		// Actions.
 		hp_dots->fadeout( value );
@@ -310,6 +315,7 @@ void Winter::draw( sf::RenderWindow* &window )
 		heart->fadein( value );
 		scores->fadein( value );
 		money->fadein( value );
+		sound_pad->fadein( value );
 		
 		// Actions.
 		hp_dots->fadein( value );
@@ -385,10 +391,34 @@ void Winter::draw( sf::RenderWindow* &window )
 	heart->draw( window );
 	scores->draw( window );
 	money->draw( window );
+	sound_pad->draw( window );
 	
 	// Effect and pause.
 	effect->draw( window );
 	pause->draw( window );
+	
+	// Sound changing
+	if( sound_pad->musicChanged() )
+	{
+		music->pause();
+		sound.setMusicPlay( !sound.getMusicPlay() );
+	}
+	
+	if( sound_pad->chunkChanged() )
+	{
+		hero->turn();
+		kunai->turn();
+		coins->turn();
+		wall->turn();
+		boulder->turn();
+		spikes->turn();
+		score_dots->turn();
+		exit->turn();
+		islands->turn();
+		mine_factory->turn();
+		golem_factory.turn();
+		sound.setChunkPlay( !sound.getChunkPlay() );
+	}
 }
 
 
@@ -569,9 +599,14 @@ bool Winter::backToLevel()
 void Winter::setSound()
 {
 	// Set chunks.
+	sound_pad->setChunk( sound.getChunkPlay() );
+	sound_pad->setMusic( sound.getMusicPlay() );
+	
+	// Set chunks.
 	if( !sound.getChunkPlay() )
 	{
 		hero->turnOff();
+		kunai->turnOff();
 		coins->turnOff();
 		wall->turnOff();
 		boulder->turnOff();
@@ -585,6 +620,7 @@ void Winter::setSound()
 	else
 	{
 		hero->turnOn();
+		kunai->turnOn();
 		coins->turnOn();
 		wall->turnOn();
 		boulder->turnOn();
@@ -597,6 +633,7 @@ void Winter::setSound()
 		
 		// Set chunks volume.
 		hero->setVolume( sound.getChunkVolume() );
+		kunai->setVolume( sound.getChunkVolume() );
 		coins->setVolume( sound.getChunkVolume() );
 		wall->setVolume( sound.getChunkVolume() );
 		boulder->setVolume( sound.getChunkVolume() );
--- to edit/04_platform/02_winter/winter_mechanics.cpp
+++ to edit/04_platform/02_winter/winter_mechanics.cpp
@@ -200,30 +200,41 @@ void Winter::mechanics()
 	// KUNAI DESTROY
 	for( unsigned i = 0; i < kunai->getSize(); i++ )
 	{
-		if( brick->checkPixelCollision( kunai->getRect( i ) ) ||
-		kunai->getX( i ) + kunai->getW( i ) > screen_w +kunai->getW( i ) ||
-		kunai->getX( i ) < -kunai->getW( i ) ||
-		wall->checkCollision( kunai->getRect( i ) )	||
-		islands->checkPixelCollision( kunai->getRect( i ) ) )
+		if( kunai->isActive( i ) )
 		{
-			kunai->destroy( i );
-		}
-		else if( kunai->isExplosiveKunai( i ) )
-		{
-			if( golem_factory.harm( kunai->getRect( i ), kunai->getDamage( i ), true ) )
+			if( brick->checkPixelCollision( kunai->getRect( i ) ) ||
+			kunai->getX( i ) + kunai->getW( i ) > screen_w +kunai->getW( i ) ||
+			kunai->getX( i ) < -kunai->getW( i ) ||
+			wall->checkCollision( kunai->getRect( i ) )	||
+			islands->checkPixelCollision( kunai->getRect( i ) ) )
 			{
 				kunai->destroy( i );
 			}
-		}
-		else if( golem_factory.harm( kunai->getRect( i ), kunai->getDamage( i ) ) )
-		{
-			if( kunai->isHealKunai( i ) )
-			{
-				showheal->run( kunai->getDamage( i ) );
-				heart->harm( kunai->getDamage( i ) );
-			}
 			
-			kunai->destroy( i );
+			else if( kunai->isStuntKunai( i ) )
+			{
+				if( golem_factory.stunt( kunai->getRect( i ), kunai->getDamage( i ) ) )
+				{
+					kunai->destroy( i );
+				}
+			}
+			else if( kunai->isExplosiveKunai( i ) )
+			{
+				if( golem_factory.harm( kunai->getRect( i ), kunai->getDamage( i ), true ) )
+				{
+					kunai->destroy( i );
+				}
+			}
+			else if( golem_factory.harm( kunai->getRect( i ), kunai->getDamage( i ) ) )
+			{
+				if( kunai->isHealKunai( i ) )
+				{
+					showheal->run( kunai->getDamage( i ) );
+					heart->harm( kunai->getDamage( i ) );
+				}
+				
+				kunai->destroy( i );
+			}
 		}
 	}
 	
