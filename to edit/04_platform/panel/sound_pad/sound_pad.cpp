f1345358d27b49531c61c8118839ac72c9c8b254
devmichalek <devmichalek@gmail.com>
2017-04-13 16:20:42 +0200

    Sound panel at the bottom

---
+++ to edit/04_platform/panel/sound_pad/sound_pad.cpp
@@ -0,0 +1,158 @@
+#include "sound_pad.h"
+
+Sound_pad::Sound_pad()
+{
+	free();
+}
+
+Sound_pad::~Sound_pad()
+{
+	free();
+}
+
+void Sound_pad::free()
+{
+	music_state = false;
+	chunk_state = false;
+	
+	mus_scratch = false;
+	chu_scratch = false;
+	
+	scratch.free();
+	music.free();
+	chunk.free();
+}
+
+
+
+void Sound_pad::setChunk( bool s )
+{
+	chu_scratch = !s;
+}
+
+void Sound_pad::setMusic( bool s )
+{
+	mus_scratch = !s;
+}
+
+void Sound_pad::load( int screen_w, int screen_h )
+{
+	free();
+	
+	float scale = 0.6;
+	
+	scratch.setName( "sound_pad-scratch" );
+	scratch.load( "data/04_platform/panel/sound_pad/scratch.png" );
+	scratch.setScale( scale, scale );
+	
+	music.setName( "sound_pad-music" );
+	music.load( "data/04_platform/panel/sound_pad/music.png" );
+	music.setScale( scale, scale );
+	music.setPosition( 10, screen_h -music.getHeight() -10 );
+	
+	chunk.setName( "sound_pad-chunk" );
+	chunk.load( "data/04_platform/panel/sound_pad/chunk.png" );
+	chunk.setScale( scale, scale );
+	chunk.setPosition( music.getRight() +20, screen_h -chunk.getHeight() -10 );
+}
+
+void Sound_pad::draw( sf::RenderWindow* &window )
+{
+	window->draw( music.get() );
+	window->draw( chunk.get() );
+	
+	if( mus_scratch )
+	{
+		scratch.setPosition( music.getX(), music.getY() );
+		window->draw( scratch.get() );
+	}
+	
+	if( chu_scratch )
+	{
+		scratch.setPosition( chunk.getX(), chunk.getY() );
+		window->draw( scratch.get() );
+	}
+}
+
+void Sound_pad::handle( sf::Event &event )
+{
+	if( music.getAlpha() == 0xFF )
+	{
+		int x, y;
+		
+		if( event.type == sf::Event::MouseButtonPressed && !focus )
+		{
+			x = event.mouseButton.x;
+			y = event.mouseButton.y;
+					
+			if( music.checkCollision( x, y ) )
+			{
+				focus = true;
+				music_state = true;
+				mus_scratch = !mus_scratch;
+				
+				if( play )
+				{
+					click.play();
+				}
+			}
+			
+			if( chunk.checkCollision( x, y ) )
+			{
+				focus = true;
+				chunk_state = true;
+				chu_scratch = !chu_scratch;
+				
+				if( play )
+				{
+					click.play();
+				}
+			}
+		}
+		
+		if( event.type == sf::Event::MouseButtonReleased )
+		{
+			focus = false;
+		}
+	}
+}
+
+
+
+void Sound_pad::fadein( int i, int max )
+{
+	music.fadein( i, max );
+	chunk.fadein( i, max );
+	scratch.fadein( i, max );
+}
+
+void Sound_pad::fadeout( int i, int min )
+{
+	music.fadeout( i, min );
+	chunk.fadeout( i, min );
+	scratch.fadeout( i, min );
+}
+
+
+
+bool Sound_pad::musicChanged()
+{
+	if( music_state )
+	{
+		music_state = false;
+		return true;
+	}
+	
+	return false;
+}
+
+bool Sound_pad::chunkChanged()
+{
+	if( chunk_state )
+	{
+		chunk_state = false;
+		return true;
+	}
+	
+	return false;
+}
---
+++ to edit/04_platform/panel/sound_pad/sound_pad.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "drawable/sprite.h"
+#include "02_menu/click.h"
+
+class Sound_pad :public Click
+{
+	bool music_state;
+	bool chunk_state;
+	
+	bool mus_scratch;
+	bool chu_scratch;
+	
+	MySprite scratch;
+	MySprite music;
+	MySprite chunk;
+	
+public:
+	
+	Sound_pad();
+	~Sound_pad();
+	void free();
+	
+	void setChunk( bool s );
+	void setMusic( bool s );
+	void load( int screen_w, int screen_h );
+	void draw( sf::RenderWindow* &window );
+	void handle( sf::Event &event );
+	
+	void fadein( int i = 1, int max = 255 );
+	void fadeout( int i = 1, int min = 0 );
+	
+	bool musicChanged();
+	bool chunkChanged();
+};
