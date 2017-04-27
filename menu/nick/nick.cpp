aa3db586922f28d7c866a576653074b68159bb70
devmichalek <devmichalek@gmail.com>
2017-04-27 16:07:29 +0200

    Able to resize!

--- menu/nick/nick.cpp
+++ menu/nick/nick.cpp
@@ -1,3 +1,12 @@
+/**
+    nick.h
+    Purpose: class Nick to show nick in menu state.
+
+    @author Adrian Michalek
+    @version 2017.03.27
+	@email adrmic98@gmail.com
+*/
+
 #include "nick.h"
 #include "file/file.h"
 
@@ -19,13 +28,12 @@ void Nick::free()
 
 
 
-void Nick::load( int screen_w, int screen_h )
+void Nick::load( unsigned w, unsigned h )
 {
 	free();
 	
 	label.setName( "nick-label" );
-	label.load( "data/menu/fancy_label.png" );
-	label.setPosition( screen_w -label.getWidth() -5, screen_h -label.getHeight() );
+	label.load( "data/initialization/fancy_label.png" );
 	
 	text.setName( "nick-text" );
 	text.setFont( "data/initialization/Jaapokki-Regular.otf", 15, 0xFF, 0xFF, 0xFF );
@@ -37,6 +45,22 @@ void Nick::draw( sf::RenderWindow &window )
 	window.draw( label.get() );
 	window.draw( text.get() );
 }
+
+
+
+void Nick::fadein( int i, int max )
+{
+	label.fadein( i, max );
+	text.fadein( i, max );
+}
+
+void Nick::fadeout( int i, int min )
+{
+	label.fadeout( i, min );
+	text.fadeout( i, min );
+}
+
+
 
 void Nick::setNick()
 {
@@ -55,14 +79,17 @@ void Nick::setNick()
 
 
 
-void Nick::fadein( int i, int max )
+void Nick::setScale( float s_x, float s_y )
 {
-	label.fadein( i, max );
-	text.fadein( i, max );
+	label.setBasicScale( s_x, s_y );
+	label.setScale( 0.4, 0.4 );
+	
+	text.setBasicScale( s_x, s_y );
+	text.setScale();
 }
 
-void Nick::fadeout( int i, int min )
+void Nick::setView( unsigned w, unsigned h, int r_x, int r_y )
 {
-	label.fadeout( i, min );
-	text.fadeout( i, min );
+	label.setPosition( w -label.getWidth() -(5 *label.getXScale() /0.4) +r_x, h -label.getHeight() +r_y );
+	text.center( label.getWidth(), label.getHeight(), label.getX(), label.getY() );
 }
--- menu/nick/nick.h
+++ menu/nick/nick.h
@@ -1,3 +1,12 @@
+/**
+    nick.h
+    Purpose: class Nick to show nick in menu state.
+
+    @author Adrian Michalek
+    @version 2017.03.27
+	@email adrmic98@gmail.com
+*/
+
 #pragma once
 
 #include "drawable/sprite.h"
@@ -9,15 +18,22 @@ class Nick
 	MySprite label;
 	
 public:
-
+	
+	// Basics.
 	Nick();
 	~Nick();
 	void free();
 	
-	void load( int screen_w, int screen_h );
+	void load( unsigned w, unsigned h );
 	void draw( sf::RenderWindow &window );
-	void setNick();
 	
 	void fadein( int i = 1, int max = 255 );
 	void fadeout( int i = 1, int min = 0 );
+	
+	// In addition.
+	void setNick();
+	
+	// Window.
+	void setScale( float s_x, float s_y );
+	void setView( unsigned w, unsigned h, int r_x, int r_y );
 };
