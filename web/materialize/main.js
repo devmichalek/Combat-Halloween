19367ea33e35593235daaab95c4ff428644dabfc
devmichalek <devmichalek@gmail.com>
2017-05-02 17:19:41 +0200

    website based on materialize

---
+++ web/materialize/main.js
@@ -0,0 +1,24 @@
+
+$(".dropdown-button").dropdown();
+
+$(".button-collapse").sideNav();
+
+$(document).ready(function(){ $(".button-collapse").sideNav(); });
+
+$(document).ready(function(){
+      $('.parallax').parallax();
+    });
+
+
+$(document).ready(function(){
+    $('.scrollspy').scrollSpy();
+  });
+
+$('#modal1').modal('open');
+
+$('#modal1').modal('close');
+
+$(document).ready(function(){
+// the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+$('.modal').modal();
+});
