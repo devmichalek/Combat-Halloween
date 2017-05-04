2893cbabbc7ace29cc8ea0c2fedf79a7662191a4
devmichalek <devmichalek@gmail.com>
2017-05-04 23:41:38 +0200

    main.js

--- web/materialize/main.js
+++ web/materialize/main.js
@@ -14,11 +14,11 @@ $(document).ready(function(){
     $('.scrollspy').scrollSpy();
   });
 
-$('#modal1').modal('open');
-
-$('#modal1').modal('close');
-
 $(document).ready(function(){
 // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
 $('.modal').modal();
 });
+
+$('#modal1').modal('open');
+
+$('#modal1').modal('close');
