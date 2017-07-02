4cfe65f467aef3085a9d1882f3259b219dc0050d
devmichalek <devmichalek@gmail.com>
2017-07-02 22:37:31 +0200

    New class sprite, sf::Sprite, sf::Texture, sf::Image

---
+++ own/sprite.cpp
@@ -0,0 +1,382 @@
+#include "sprite.h"
+
+MySprite::MySprite()
+{
+	identity = "";
+	image = NULL;
+    sprite = NULL;
+	texture = NULL;
+	alpha = 0;
+}
+
+MySprite::~MySprite()
+{
+    free();
+}
+
+void MySprite::free()
+{
+	identity = "";
+	
+	if( image != NULL )
+    {
+        delete image;
+        image = NULL;
+    }
+	
+	if( sprite != NULL )
+	{
+		delete sprite;
+		sprite = NULL;
+	}
+	
+	if( texture != NULL )
+	{
+		delete texture;
+		texture = NULL;
+	}
+	
+	if( !rects.empty() )
+	{
+		for( unsigned i = 0; i < rects.size(); i++ )
+		{
+			delete rects[ i ];
+			rects[ i ] = NULL;
+		}
+		
+		rects.clear();
+	}
+	
+	alpha = 0;
+}
+
+const sf::Sprite& MySprite::get() const
+{
+    return *sprite;
+}
+
+
+
+void MySprite::setIdentity( string identity )
+{
+	this->identity = identity;
+}
+
+const string& MySprite::getIdentity() const
+{
+	return identity;
+}
+
+
+
+void MySprite::setColor( sf::Color color )
+{
+	sf::Color newColor( sprite->getColor() );
+	newColor.r = color.r;
+	newColor.g = color.g;
+	newColor.b = color.b;
+
+	sprite->setColor( newColor );
+}
+
+void MySprite::setAlpha( float alpha )
+{
+	if( this->alpha != alpha )
+	{
+		this->alpha = alpha;
+		sf::Color newColor( sprite->getColor() );
+		newColor.a = alpha;
+		sprite->setColor( newColor );
+	}
+}
+
+float MySprite::getAlpha()
+{
+	return alpha;
+}
+
+
+
+void MySprite::fadein( float v, int max )
+{
+	if( alpha < max )
+	{
+		alpha += v;
+		
+		if( alpha > max )
+		{
+			alpha = max;
+		}
+		
+		sf::Color newColor( sprite->getColor() );
+		newColor.a = alpha;
+		sprite->setColor( newColor );
+	}
+}
+
+void MySprite::fadeout( float v, int min )
+{
+	if( alpha > min )
+	{
+		alpha -= v;
+		
+		if( alpha < min )
+		{
+			alpha = min;
+		}
+		
+		sf::Color newColor( sprite->getColor() );
+		newColor.a = alpha;
+		sprite->setColor( newColor );
+	}
+}
+
+void MySprite::flipHorizontally()
+{
+	sprite->setScale( sprite->getScale().x *-1, sprite->getScale().y );
+}
+
+void MySprite::flipVertically()
+{
+	sprite->setScale( sprite->getScale().x, sprite->getScale().y *-1 );
+}
+
+
+
+void MySprite::load( string path, int amount )
+{
+    free();
+	
+	// Load image.
+	try
+	{
+		image = new sf::Image;
+		if( image == NULL )
+		{
+			throw identity + " not created image";
+		}
+		else if( !image->loadFromFile( path ) )
+		{
+			throw identity + " not loaded " + path;
+		}
+		else if( amount < 1 )
+		{
+			throw identity + " amount of offsets is less than 1";
+		}
+		else
+		{
+			int width = image->getSize().x;
+			int height = image->getSize().y;
+			
+			// Set rects and 0 offset
+			for( int i = 0; i < amount; i++ )
+			{
+				rects.push_back( new sf::IntRect() );
+				rects[ rects.size() -1 ]->width = width /amount;
+				rects[ rects.size() -1 ]->height = height;
+				rects[ rects.size() -1 ]->left = width *i /amount;
+				rects[ rects.size() -1 ]->top = 0;
+			}
+		}
+	}
+	catch( string msg )
+	{
+		cerr << msg << endl;
+	}
+	
+	// Load texture and set sprite.
+	try
+	{
+		texture = new sf::Texture;
+		if( !texture->loadFromFile( path ) )
+		{
+			throw identity + " not loaded " + path;
+		}
+		else
+		{
+			texture->setSmooth( true );
+			sprite = new sf::Sprite;
+			if( sprite == NULL )
+			{
+				throw identity + " not created sprite";
+			}
+			else
+			{
+				sprite->setTexture( *texture );
+				setOffset( 0 );
+			}
+		}
+	}
+	catch( string msg )
+	{
+		cerr << msg << endl;
+	}
+}
+
+void MySprite::create( int w, int h )
+{
+	free();
+	
+	// Create texture.
+	try
+	{
+		texture = new sf::Texture;
+		if( !texture->create( w, h ) )
+		{
+			throw identity + " not loaded ";
+		}
+		else
+		{
+			texture->setSmooth( true );
+			sprite = new sf::Sprite;
+			if( sprite == NULL )
+			{
+				throw identity + " not created sprite";
+			}
+			else
+			{
+				sf::Uint8* pixels;
+				pixels = new sf::Uint8 [ w * h * 4 ];
+				
+				for( int i = 0; i < w*h*4; i++ )
+				{
+					pixels[ i ] = 0xFF;
+				}
+				
+				texture->update( pixels );
+				
+				delete [] pixels;
+				pixels = NULL;
+				
+				sprite->setTexture( *texture );
+				setOffset( 0 );
+				alpha = 0xFF;
+				setAlpha( 0 );
+			}
+		}
+	}
+	catch( string msg )
+	{
+		cerr << msg << endl;
+	}
+}
+
+
+
+
+void MySprite::setPosition( float x, float y )
+{
+    sprite->setPosition( x, y );
+}
+
+void MySprite::move( float x, float y )
+{
+	sprite->move( x, y );
+}
+
+void MySprite::center( float x, float y, int w, int h )
+{
+	float left = w/2 - (image->getSize().x*sprite->getScale().x /rects.size())/2 + x;
+    float top = h/2 - (image->getSize().y*sprite->getScale().y)/2 + y;
+
+	sprite->setPosition( left, top );
+}
+
+
+
+void MySprite::setScale( float x, float y )
+{
+	sprite->setScale( x, y );
+}
+
+void MySprite::setRotation( double angle )
+{
+	sprite->setRotation( angle );
+}
+
+void MySprite::setOffset( int n )
+{
+	if( n > -1 && n < static_cast <int> (rects.size()) )
+	{
+		sprite->setTextureRect( *rects[ n ] );
+	}
+}
+
+
+
+const float& MySprite::getX() const
+{
+	return sprite->getPosition().x;
+}
+
+const float& MySprite::getY() const
+{
+	return sprite->getPosition().y;
+}
+
+const float MySprite::getWidth() const
+{
+	float scale = sprite->getScale().x;
+	if( scale < 0 )	scale = -scale;
+	return image->getSize().x *scale /rects.size();
+}
+
+const float MySprite::getHeight() const
+{
+	float scale = sprite->getScale().y;
+	if( scale < 0 )	scale = -scale;
+	return image->getSize().y *scale;
+}
+
+const float& MySprite::getLeft() const
+{
+	return sprite->getPosition().x;
+}
+
+const float MySprite::getRight() const
+{
+	return sprite->getPosition().x +getWidth();
+}
+
+const float& MySprite::getTop() const
+{
+	return sprite->getPosition().y;
+}
+
+const float MySprite::getBot() const
+{
+	return sprite->getPosition().y +getHeight();
+}
+
+bool MySprite::checkCollision( float x, float y, float w, float h )
+{
+	if( y + h <= getTop() )
+        return false;
+
+    if( y >= getBot() )
+        return false;
+
+    if( x + w <= getLeft() )
+        return false;
+
+    if( x >= getRight() )
+        return false;
+
+    return true;
+}
+
+/*
+bool MySprite::checkPixelCollision( int x, int y )
+{
+	if( x -left >= 0 && y -top >= 0 && x < left + width && y < top + height )
+	{
+		sf::Color c = image->getPixel( x -left, y -top );
+		if( c.a > 0 )
+		{
+			return true;
+		}
+	}
+	
+	return false;
+}
+*/
---
+++ own/sprite.h
@@ -0,0 +1,65 @@
+#pragma once
+#define _GLIBCXX_USE_CXX11_ABI 0
+#include "SFML/Graphics/Rect.hpp"
+#include "SFML/Graphics/Image.hpp"
+#include "SFML/Graphics/Sprite.hpp"
+#include "SFML/Graphics/Texture.hpp"
+#include <vector>
+#include <iostream>
+using namespace std;
+
+class MySprite
+{
+	string identity;
+	sf::Image* image;
+    sf::Sprite* sprite;
+	sf::Texture* texture;
+	vector <sf::IntRect*> rects;
+	float alpha;
+	
+public:
+
+    MySprite();
+    ~MySprite();
+    void free();
+	const sf::Sprite& get() const;	// Return sprite
+	
+	void setIdentity( string identity );
+	const string& getIdentity() const;
+	
+	void setColor( sf::Color color );
+	void setAlpha( float alpha = 0 );
+	float getAlpha();
+	
+	void fadein( float v = 1, int max = 0xFF );
+	void fadeout( float v = 1, int min = 0 );
+	
+	void flipHorizontally();
+	void flipVertically();
+	
+	
+	void load( string path, int amount = 1 );
+	void create( int w, int h );
+	
+	
+    void setPosition( float x, float y );
+	void move( float x, float y );
+	void center( float x, float y, int w, int h );
+	
+	void setRotation( double angle );
+	void setScale( float x = 1, float y = 1 );
+	void setOffset( int n );
+	
+	// rest accessors
+	const float& getX() const;
+	const float& getY() const;
+    const float getWidth() const;
+    const float getHeight() const;
+	
+    const float& getLeft() const;
+    const float getRight() const;
+    const float& getTop() const;
+    const float getBot() const;
+	
+	bool checkCollision( float x, float y, float w = 0, float h = 0 );
+};
