bba6cd18bf029888f681b560071aa963ab635a8b
devmichalek <devmichalek@gmail.com>
2017-07-02 22:35:38 +0200

    New class, objects opens txt files

---
+++ own/file.cpp
@@ -0,0 +1,139 @@
+#include "own/file.h"
+#include <stdio.h>
+
+MyFile::MyFile()
+{
+	file = NULL;
+}
+
+MyFile::~MyFile()
+{
+	free();
+}
+
+void MyFile::free()
+{
+	if( file != NULL )
+	{
+		if( file->is_open() )
+		{
+			file->close();
+		}
+		
+		delete file;
+		file = NULL;
+	}
+}
+
+void MyFile::load( string path, ios_base::openmode mode )
+{
+	free();
+	
+	file = new fstream;
+	file->open( path, mode );
+	
+	if( !file->good() )
+	{
+		printf( "Something went wrong with file from %s\n", path.c_str() );
+		free();
+	}
+}
+
+bool MyFile::is_good()
+{
+	if( file == NULL )
+	{
+		return false;
+	}
+	else if( !file->is_open() )
+	{
+		return false;
+	}
+	
+	return true;
+}
+
+fstream& MyFile::get()
+{
+	return *file;
+}
+
+
+
+// other
+int con::stoi( string str )
+{
+	bool m = false;
+	int tmp = 0;
+	unsigned i = 0;
+	
+	
+	if( str[ 0 ] == '-' )
+	{
+		i++;
+		m = true;
+	}
+	
+	while( i < str.size() )
+	{
+		tmp = 10 *tmp +str[ i ] -48;
+		i++;
+    }
+	
+    return m ? -tmp : tmp;   
+}
+
+float con::stof( string str )
+{
+	const char* s = str.c_str();
+	
+	float rez = 0, fact = 1;
+	
+	if( *s == '-' )
+	{
+		s++;
+		fact = -1;
+	}
+	
+	for( int point_seen = 0; *s; s++ )
+	{
+		if( *s == '.' )
+		{
+			point_seen = 1; 
+			continue;
+		}
+		
+		int d = *s - '0';
+		
+		if( d >= 0 && d <= 9 )
+		{
+			if( point_seen )
+			{
+				fact /= 10.0f;
+			}
+				
+			rez = rez * 10.0f + static_cast <float> (d);
+		}
+	}
+
+	return rez *fact;
+};
+
+string con::itos( int n )
+{
+	string tmp;
+	 
+	if( n < 0 )
+	{
+		tmp = "-";
+		n = -n;
+	}
+	
+	if( n > 9 )
+	{
+		tmp += itos( n / 10 );
+	}
+	
+	tmp += n % 10 + 48;
+	return tmp;
+}
---
+++ own/file.h
@@ -0,0 +1,31 @@
+#pragma once
+#define _GLIBCXX_USE_CXX11_ABI 0
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+class MyFile
+{
+	fstream* file;
+	
+public:
+	
+	MyFile();
+	~MyFile();
+	void free();
+	
+	void load( string path, ios_base::openmode mode = ios::in | ios::out );
+	
+	bool is_good();
+	fstream& get();
+};
+
+// conversion
+namespace con
+{
+	int stoi( string str );
+	float stof( string str );
+	
+	string itos( int n );
+}
