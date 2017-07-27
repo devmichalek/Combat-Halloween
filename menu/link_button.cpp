f2eab29453067ca51d424a26bda509cb8a1ca06a
devmichalek <devmichalek@gmail.com>
2017-07-27 17:03:46 +0200

    Button which opens website after you have clicked

---
+++ menu/link_button.cpp
@@ -0,0 +1,102 @@
+#include "link_button.h"
+#include <windows.h>
+
+Link_button::Link_button()
+{
+	free();
+}
+
+Link_button::~Link_button()
+{
+	free();
+}
+
+void Link_button::free()
+{
+	button.free();
+	click.free();
+	playable = true;
+}
+
+
+
+void Link_button::load( float x, float y, string path )
+{
+	free();
+	
+	button.setIdentity( "link_button-button" );
+	button.load( path, 3 );
+	button.setPosition( x, y );
+	
+	click.setIdentity( "link_button-chunk" );
+	click.load( "sounds/click.wav" );
+}
+
+void Link_button::setScale( float x, float y )
+{
+	button.setScale( x, y );
+}
+
+void Link_button::setUrl( string url )
+{
+	this->url = url;
+}
+
+void Link_button::handle( sf::Event& event )
+{
+	if( event.type == sf::Event::MouseButtonPressed )
+	{
+		if( event.mouseButton.button == sf::Mouse::Left )
+		{
+			// printf( "%d %d\n", event.mouseButton.x, event.mouseButton.y );
+			if( button.checkCollisionCircle( event.mouseButton.x, event.mouseButton.y ) )
+			{
+				if( playable )
+				{
+					click.play();
+				}
+				
+				button.setOffset( 1 );
+				string command = "start " +url;
+				system( command.c_str() );
+			}
+		}
+	}
+	
+	if( event.type == sf::Event::MouseMoved )
+	{
+		button.setOffset( 0 );
+	}
+}
+
+void Link_button::draw( sf::RenderWindow* &window, double elapsedTime )
+{
+	window->draw( button.get() );
+}
+
+
+
+void Link_button::fadein( float v, int max )
+{
+	button.fadein( v, max );
+}
+
+void Link_button::fadeout( float v, int min )
+{
+	button.fadeout( v, min );
+}
+
+float Link_button::getRight()
+{
+	return button.getRight();
+}
+
+void Link_button::setPlayable( bool playable )
+{
+	this->playable = playable;
+}
+
+void Link_button::setVolume( float volume )
+{
+	click.setVolume( volume );
+}
---
+++ menu/link_button.h
@@ -0,0 +1,37 @@
+#include "own/sprite.h"
+#include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/Window/Event.hpp>
+#include "own/chunk.h"
+
+class Link_button
+{
+	MySprite button;
+	string url;
+	
+	// Sound.
+	Chunk click;
+	bool playable;
+	
+public:
+
+	// Basics.
+	Link_button();
+	~Link_button();
+	void free();
+	
+	void load( float x, float y, string path );
+	void setScale( float x, float y );
+	void setUrl( string url );
+	void handle( sf::Event& event );
+	void draw( sf::RenderWindow* &window, double elapsedTime );
+	
+	void fadein( float v = 1, int max = 0xFF );
+	void fadeout( float v = 1, int min = 0 );
+	
+	// Getters.
+	float getRight();
+	
+	// Sound.
+	void setPlayable( bool playable = true );
+	void setVolume( float volume = 50 );
+};
