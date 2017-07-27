85c4030bf24c0807c0fd64f4beb0262048b4aeef
devmichalek <devmichalek@gmail.com>
2017-07-27 17:01:42 +0200

    button class, represents simple button with states

---
+++ menu/button.cpp
@@ -0,0 +1,109 @@
+#include "button.h"
+
+Button::Button()
+{
+	free();
+}
+
+Button::~Button()
+{
+	free();
+}
+
+void Button::free()
+{
+	button.free();
+	locked = false;
+	pressed = false;
+	
+	click.free();
+	playable = true;
+}
+
+
+
+void Button::load( float x, float y, string path, bool locked )
+{
+	free();
+	
+	this->locked = locked;
+	
+	button.setIdentity( "button-button" );
+	button.load( path, 3 );
+	button.setPosition( x, y );
+	
+	if( locked )
+	{
+		button.setOffset( 2 );
+	}
+	
+	click.setIdentity( "link_button-chunk" );
+	click.load( "sounds/click.wav" );
+}
+
+void Button::setScale( float x, float y )
+{
+	button.setScale( x, y );
+}
+
+void Button::handle( sf::Event& event )
+{
+	if( !locked )
+	{
+		if( event.type == sf::Event::MouseButtonPressed )
+		{
+			if( event.mouseButton.button == sf::Mouse::Left )
+			{
+				if( button.checkCollision( event.mouseButton.x, event.mouseButton.y ) )
+				{
+					if( playable )
+					{
+						click.play();
+					}
+					
+					button.setOffset( 1 );
+					pressed = true;
+				}
+			}
+		}
+	}
+}
+
+void Button::draw( sf::RenderWindow* &window, double elapsedTime )
+{
+	window->draw( button.get() );
+}
+
+
+
+void Button::fadein( float v, int max )
+{
+	button.fadein( v, max );
+}
+
+void Button::fadeout( float v, int min )
+{
+	button.fadeout( v, min );
+}
+
+bool Button::isPressed()
+{
+	return pressed;
+}
+
+float Button::getBot()
+{
+	return button.getBot();
+}
+
+
+
+void Button::setPlayable( bool playable )
+{
+	this->playable = playable;
+}
+
+void Button::setVolume( float volume )
+{
+	click.setVolume( volume );
+}
---
+++ menu/button.h
@@ -0,0 +1,38 @@
+#include "own/sprite.h"
+#include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/Window/Event.hpp>
+#include "own/chunk.h"
+
+class Button
+{
+	MySprite button;
+	bool locked;
+	bool pressed;
+	
+	// Sound.
+	Chunk click;
+	bool playable;
+	
+public:
+	
+	// Basics.
+	Button();
+	~Button();
+	void free();
+	
+	void load( float x, float y, string path, bool locked = false );
+	void setScale( float x, float y );
+	void handle( sf::Event& event );
+	void draw( sf::RenderWindow* &window, double elapsedTime );
+	
+	void fadein( float v = 1, int max = 0xFF );
+	void fadeout( float v = 1, int min = 0 );
+	
+	// Getters.
+	bool isPressed();
+	float getBot();
+	
+	// Sound.
+	void setPlayable( bool playable = true );
+	void setVolume( float volume = 50 );
+};
