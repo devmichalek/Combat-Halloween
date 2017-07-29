d2f64d89e0acbe33293e66e8b2af68b76c83732c
devmichalek <devmichalek@gmail.com>
2017-07-29 16:21:21 +0200

    settings ready to show

--- menu/settings.cpp
+++ menu/settings.cpp
@@ -1,4 +1,5 @@
 #include "settings.h"
+#include "own/file.h"
 
 Settings::Settings()
 {
@@ -12,7 +13,54 @@ Settings::~Settings()
 
 void Settings::free()
 {
+	screen_w = 0;
+	screen_h = 0;
 	
+	if( !keys_current.empty() )
+	{
+		keys_current.clear();
+	}
+	
+	if( !state_texts.empty() )
+	{
+		for( auto &it :state_texts )
+		{
+			it->free();
+			delete it;
+			it = NULL;
+		}
+		
+		state_texts.clear();
+	}
+	
+	if( !active_texts.empty() )
+	{
+		for( auto &it :active_texts )
+		{
+			it->free();
+			delete it;
+			it = NULL;
+		}
+		
+		active_texts.clear();
+	}
+	
+	contactme.free();
+	
+	table.free();
+	gear_top.free();
+	gear_bot.free();
+	moving = 0;
+	x1 = x2 = 0;
+	
+	table_second.free();
+	gear_left.free();
+	gear_right.free();
+	moving_second = 0;
+	y1 = y2 = 0;
+	
+	click.free();
+	playable = true;
 }
 
 
@@ -20,6 +68,120 @@ void Settings::free()
 void Settings::load( float screen_w, float screen_h )
 {
 	free();
+	this->screen_w = screen_w;
+	this->screen_h = screen_h;
+	
+	// Set font state_texts and active_texts.
+	for( unsigned i = 0; i < AMOUNT; i++ )
+	{
+		state_texts.push_back( new MyText );
+		state_texts[ i ]->setIdentity( "settings-state_texts" );
+		state_texts[ i ]->setFont( "fonts/jcandlestickextracond.ttf" );
+		active_texts.push_back( new MyText );
+		active_texts[ i ]->setIdentity( "settings-active_texts" );
+		active_texts[ i ]->setFont( "fonts/jcandlestickextracond.ttf" );
+	}
+	
+	// Set state_texts text.
+	state_texts[ MOVE_LEFT ]->setText( "move left" );
+	state_texts[ MOVE_RIGHT ]->setText( "move right" );
+	state_texts[ JUMP ]->setText( "jump" );
+	state_texts[ ATTACK ]->setText( "attack" );
+	state_texts[ JUMP_ATTACK ]->setText( "jump attack" );
+	state_texts[ PAUSE ]->setText( "pause" );
+	state_texts[ CHAT ]->setText( "chat" );
+	
+	// Set active_texts text.
+	MyFile file;
+	file.load( "txt/keys_current.txt" );
+	if( file.is_good() )
+	{
+		for( unsigned i = 0; i < JUMP_ATTACK; i++ )
+		{
+			string line = "";
+			file.get() >> line;
+			keys_current.push_back( stoi( line ) );
+			active_texts[ i ]->setText( getName( keys_current[ i ] ) );
+		}
+	}
+	file.free();
+	active_texts[ JUMP_ATTACK ]->setText( getName( keys_current[ JUMP ] ) +" + " +getName( keys_current[ ATTACK ] ) );
+	active_texts[ PAUSE ]->setText( "p" );
+	active_texts[ CHAT ]->setText( "o" );
+	
+	// Set size state_texts and active_texts.
+	int size = screen_h /28;
+	for( unsigned i = 0; i < AMOUNT; i++ )
+	{
+		state_texts[ i ]->setSize( size );
+		active_texts[ i ]->setSize( size );
+		state_texts[ i ]->setColor( sf::Color( 0xDD, 0xDD, 0xDD ) );
+		active_texts[ i ]->setColor( sf::Color( 0xFF, 0xFF, 0xFF ) );
+	}
+	
+	
+	
+	contactme.setIdentity( "settings-contactme" );
+	contactme.setFont( "fonts/jcandlestickextracond.ttf" );
+	contactme.setText( "Contact me: adrmic98@gmail.com" );
+	contactme.setSize( size );
+	contactme.setColor( sf::Color( 0xFF, 0xFF, 0xFF ) );
+	
+	
+	
+	// Set table.
+	table.setIdentity( "settings-table" );
+	table.load( "images/menu/table.png" );
+	table.setScale( screen_w /2560, screen_h /1440 );
+	x1 = screen_w -screen_w /128;
+	x2 = screen_w +screen_w /128 -table.getWidth();	
+	table.setPosition( x1, screen_h /2.3 );
+	
+	// Set gear_top.
+	gear_top.setIdentity( "settings-gear_top" );
+	gear_top.load( "images/menu/gear.png" );
+	gear_top.setOrigin( gear_top.getWidth() *0.5, gear_top.getHeight() *0.5 );
+	gear_top.setScale( screen_w /2560, screen_h /1440 );
+	gear_top.setPosition( screen_w, table.getTop() );
+	
+	// Set gear_bot.
+	gear_bot.setIdentity( "settings-gear_bot" );
+	gear_bot.load( "images/menu/gear.png" );
+	gear_bot.setOrigin( gear_bot.getWidth() *0.5, gear_bot.getHeight() *0.5 );
+	gear_bot.setScale( screen_w /2560, screen_h /1440 );
+	gear_bot.setPosition( screen_w, table.getBot() );
+	
+	position();
+	
+	
+	
+	// Set second table.
+	table_second.setIdentity( "settings-table_second" );
+	table_second.load( "images/menu/table_second.png" );
+	table_second.setScale( screen_w /2560, screen_h /1440 );
+	y1 = screen_h -screen_h /72;
+	y2 = screen_h +screen_h /72 -table_second.getHeight();	
+	table_second.setPosition( screen_w /2.13, y1 );
+	
+	// Set gear_left.
+	gear_left.setIdentity( "settings-gear_left" );
+	gear_left.load( "images/menu/gear.png" );
+	gear_left.setOrigin( gear_left.getWidth() *0.5, gear_left.getHeight() *0.5 );
+	gear_left.setScale( screen_w /2560, screen_h /1440 );
+	gear_left.setPosition( table_second.getLeft(), screen_h );
+	
+	// Set gear_right.
+	gear_right.setIdentity( "settings-gear_right" );
+	gear_right.load( "images/menu/gear.png" );
+	gear_right.setOrigin( gear_right.getWidth() *0.5, gear_right.getHeight() *0.5 );
+	gear_right.setScale( screen_w /2560, screen_h /1440 );
+	gear_right.setPosition( table_second.getRight(), screen_h );
+	
+	
+	
+	// Load sound.
+	click.setIdentity( "link_button-chunk" );
+	click.load( "sounds/click.wav" );
 }
 
 void Settings::handle( sf::Event& event )
@@ -29,34 +191,243 @@ void Settings::handle( sf::Event& event )
 
 void Settings::draw( sf::RenderWindow* &window )
 {
+	// First table.
+	window->draw( table.get() );
+	for( auto &it :state_texts )
+	{
+		window->draw( it->get() );
+	}
 	
+	for( auto &it :active_texts )
+	{
+		window->draw( it->get() );
+	}
+	window->draw( contactme.get() );
+	window->draw( gear_top.get() );
+	window->draw( gear_bot.get() );
+	
+	// Second table.
+	window->draw( table_second.get() );
+	window->draw( gear_left.get() );
+	window->draw( gear_right.get() );
 }
 
 void Settings::mechanics( double elapsedTime )
 {
-	
+	if( moving != 0 )
+	{
+		table.move( moving, 0 );
+		position();
+		
+		float rotation = elapsedTime *0xFF;
+		if( moving < 0 )
+		{
+			gear_top.setRotation( gear_top.getRotation() +rotation );
+			gear_bot.setRotation( gear_bot.getRotation() +rotation );
+		}
+		else
+		{
+			gear_top.setRotation( gear_top.getRotation() -rotation );
+			gear_bot.setRotation( gear_bot.getRotation() -rotation );
+		}
+	}
 }
 
 
 
 void Settings::fadein( float v, int max )
 {
+	for( auto &it :state_texts )
+	{
+		it->fadein( v, max );
+	}
 	
+	for( auto &it :active_texts )
+	{
+		it->fadein( v, max );
+	}
+	
+	contactme.fadein( v, max );
+	table.fadein( v, max );
+	gear_top.fadein( v, max );
+	gear_bot.fadein( v, max );
 }
 
 void Settings::fadeout( float v, int min )
 {
+	for( auto &it :state_texts )
+	{
+		it->fadeout( v, min );
+	}
 	
+	for( auto &it :active_texts )
+	{
+		it->fadeout( v, min );
+	}
+	
+	contactme.fadeout( v, min );
+	table.fadeout( v, min );
+	gear_top.fadeout( v, min );
+	gear_bot.fadeout( v, min );
+}
+
+
+
+void Settings::exsertTable( double elapsedTime )
+{
+	if( table.getX() > x2 )
+	{
+		moving = -elapsedTime *0xFF;
+	}
+	else
+	{
+		moving = 0;
+	}
+	
+	if( table.getX() < x2 )
+	{
+		table.setPosition( x2, table.getY() );
+		position();
+	}
+}
+
+void Settings::shovelTable( double elapsedTime )
+{
+	if( table.getX() < x1 )
+	{
+		moving = elapsedTime *0xFF;
+	}
+	else
+	{
+		moving = 0;
+	}
+	
+	if( table.getX() > x1 )
+	{
+		table.setPosition( x1, table.getY() );
+		position();
+	}
+}
+
+void Settings::reset()
+{
+	MyFile file;
+	file.load( "txt/keys.txt" );
+	if( file.is_good() )
+	{
+		for( unsigned i = 0; i < JUMP_ATTACK; i++ )
+		{
+			string line = "";
+			file.get() >> line;
+			active_texts[ i ]->setText( getName( stoi( line ) ) );
+		}
+	}
+	file.free();
+}
+
+void Settings::position()
+{
+	// Position state_texts.
+	for( unsigned i = 0; i < state_texts.size(); i++ )
+	{
+		state_texts[ i ]->setPosition( table.getX() +screen_w /80, table.getY() +(screen_h /28 *i) +screen_h /54 );
+	}
+	
+	// Position active_texts.
+	for( unsigned i = 0; i < active_texts.size(); i++ )
+	{
+		active_texts[ i ]->setPosition( table.getX() +table.getWidth() /2, state_texts[ i ]->getY() );
+	}
+	
+	// Contactme.
+	contactme.setPosition( table.getX() +screen_w /80, table.getBot() -contactme.getHeight() -screen_h /54 );
+}
+
+bool Settings::isPossibleKey( sf::Event &event )
+{
+	sf::Uint8 code = event.key.code;
+	
+	// if pause key or chat key
+	if( code == 14 || code == 15 )
+	{
+		return false;
+	}
+	
+	if( code > -1 && code <= 35 )
+	{
+		return true;
+	}
+	else if( code >= 75 && code <= 84 )
+	{
+		return true;
+	}
+	else if( code >= 71 && code <= 74 )
+	{
+		return true;
+	}
+	else if( code == 57 )
+	{
+		return true;
+	}
+
+	return false;
+}
+
+string Settings::getName( int n )
+{
+	string name = "";
+	
+	if( n > -1 && n <= 25 )	// from a to z
+	{
+		// in ASCII 97 means letter 'a'
+		name = static_cast <char> ( n + 97 );
+	}
+	else if( n >= 26 && n <= 35 )
+	{
+		// 26 + 22 = 48
+		// in ASCII 48 means character 0
+		name = static_cast <char> ( n + 22 );
+	}
+	else if( n >= 75 && n <= 84 )
+	{
+		// 75 - 27 = 48
+		// in ASCII 48 means character 0
+		name = static_cast <char> ( n - 27 );
+	}
+	
+	// Rest.
+	else if( n == 71 )
+	{
+		name = "Left";
+	}
+	else if( n == 72 )
+	{
+		name = "Right";
+	}
+	else if( n == 73 )
+	{
+		name = "Up";
+	}
+	else if( n == 74 )
+	{
+		name = "Down";
+	}
+	else if( n == 57 )
+	{
+		name = "Space";
+	}
+	
+	return name;
 }
 
 
 
 void Settings::setPlayable( bool playable )
 {
-	
+	this->playable = playable;
 }
 
 void Settings::setVolume( float volume )
 {
-	
+	click.setVolume( volume );
 }
--- menu/settings.h
+++ menu/settings.h
@@ -7,15 +7,22 @@
 
 class Settings
 {
-	enum activity
+	float screen_w;
+	float screen_h;
+	
+	enum
 	{
 		MOVE_LEFT = 0,
 		MOVE_RIGHT,
 		JUMP,
 		ATTACK,
-		JUMP_ATTACK
+		JUMP_ATTACK,
+		PAUSE,
+		CHAT,
+		AMOUNT
 	};
 	
+	vector <int> keys_current;
 	vector <MyText*> state_texts;
 	vector <MyText*> active_texts;
 	MyText contactme;
@@ -26,6 +33,17 @@ class Settings
 	MySprite gear_bot;
 	float moving;
 	float x1, x2;
+	
+	// Second table.
+	MySprite table_second;
+	MySprite gear_left;
+	MySprite gear_right;
+	float moving_second;
+	float y1, y2;
+	
+	// Sound.
+	Chunk click;
+	bool playable;
 	
 public:
 
@@ -42,7 +60,123 @@ public:
 	void fadein( float v = 1, int max = 0xFF );
 	void fadeout( float v = 1, int min = 0 );
 	
+	// Support.
+	void exsertTable( double elapsedTime );
+	void shovelTable( double elapsedTime );
+	void reset();
+	void position();
+	bool isPossibleKey( sf::Event &event );
+	string getName( int n );
+	
 	// Sound.
 	void setPlayable( bool playable = true );
 	void setVolume( float volume = 50 );
 };
+
+/*
+enum  	Key { 
+  Unknown = -1, 
+  A = 0, 
+  B, 
+  C, 
+  D, // 3
+  E, 
+  F, 
+  G, 
+  H, 
+  I, 
+  J, 
+  K, // 10
+  L, 
+  M, 
+  N, 
+  O, 
+  P, // 15
+  Q, 
+  R, 
+  S, 
+  T, 
+  U, // 20
+  V, 
+  W, 
+  X, 
+  Y, 
+  Z, // 25
+  Num0, 
+  Num1, 
+  Num2, 
+  Num3, 
+  Num4, // 30
+  Num5, 
+  Num6, 
+  Num7, 
+  Num8, 
+  Num9, // 35
+  Escape, 
+  LControl, 
+  LShift, 
+  LAlt, 
+  LSystem, // 40
+  RControl, 
+  RShift, 
+  RAlt, 
+  RSystem, 
+  Menu, // 45
+  LBracket, 
+  RBracket, 
+  SemiColon, 
+  Comma, 
+  Period,  // 50
+  Quote, 
+  Slash, 
+  BackSlash, 
+  Tilde, 
+  Equal, // 55
+  Dash, 
+  Space, // 57
+  Return, 
+  BackSpace, 
+  Tab, // 60
+  PageUp, 
+  PageDown, 
+  End, 
+  Home, 
+  Insert, // 65
+  Delete, 
+  Add, 
+  Subtract, 
+  Multiply, 
+  Divide, // 70
+  Left, // 71
+  Right, // 72
+  Up, // 73
+  Down, // 74 
+  Numpad0, // 75
+  Numpad1, 
+  Numpad2, 
+  Numpad3, 
+  Numpad4, 
+  Numpad5, 
+  Numpad6, 
+  Numpad7, 
+  Numpad8, 
+  Numpad9, // 84
+  F1, 
+  F2, 
+  F3, 
+  F4, 
+  F5, 
+  F6, 
+  F7, 
+  F8, 
+  F9, 
+  F10, 
+  F11, 
+  F12, 
+  F13, 
+  F14, 
+  F15, 
+  Pause, 
+  KeyCount 
+}
+*/
