2457b1ea4c1c615af6fd4e63d0aacded4685fcd4
devmichalek <devmichalek@gmail.com>
2017-07-31 19:34:10 +0200

    volumebutton as a class for music and sound

---
+++ menu/volume_button.cpp
@@ -0,0 +1,194 @@
+#include "volume_button.h"
+#include "own/file.h"
+
+Volume_button::Volume_button()
+{
+	free();
+}
+
+Volume_button::~Volume_button()
+{
+	free();
+}
+
+void Volume_button::free()
+{
+	plus.free();
+	minus.free();
+	volume = 0;
+	min = max = 0;
+	active = true;
+	plus_pressed = false;
+	minus_pressed = false;
+	
+	click.free();
+	playable = true;
+}
+
+
+
+void Volume_button::load( float left, float right, float bot, float screen_w, float screen_h )
+{
+	free();
+	
+	// Load plus button.
+	plus.setIdentity( "volume_button-plus" );
+	plus.load( "images/menu/plus.png", 3 );
+	plus.setScale( screen_w /2560, screen_h /1440 );
+	plus.setPosition( right -plus.getWidth(), bot -plus.getHeight() /1.2 );
+	
+	// Load minus button.
+	minus.setIdentity( "volume_button-minus" );
+	minus.load( "images/menu/minus.png", 3 );
+	minus.setScale( screen_w /2560, screen_h /1440 );
+	minus.setPosition( left, bot -minus.getHeight() /1.2 );
+	
+	// Load chunk.
+	click.setIdentity( "volume_button-click" );
+	click.load( "sounds/click.wav" );
+}
+
+bool Volume_button::handle( sf::Event& event )
+{
+	if( event.type == sf::Event::MouseButtonReleased && active )
+	{
+		plus.setOffset( 0 );
+		minus.setOffset( 0 );
+		plus_pressed = false;
+		minus_pressed = false;
+	}
+	
+	if( event.type == sf::Event::MouseButtonPressed )
+	{
+		if( event.mouseButton.button == sf::Mouse::Left )
+		{
+			float x = event.mouseButton.x;
+			float y = event.mouseButton.y;
+			
+			if( plus.checkCollision( x, y ) )
+			{
+				if( active )
+				{
+					if( playable && volume < max )
+					{
+						click.play();
+					}
+					
+					plus_pressed = true;
+				}
+				
+				return true;
+			}
+			else if( minus.checkCollision( x, y ) )
+			{
+				if( active )
+				{
+					if( playable && volume > min )
+					{
+						click.play();
+					}
+					
+					minus_pressed = true;
+				}
+				
+				return true;
+			}
+		}
+	}
+	
+	return false;
+}
+
+void Volume_button::draw( sf::RenderWindow* &window )
+{
+	window->draw( plus.get() );
+	window->draw( minus.get() );
+	
+	if( !active )
+	{
+		plus.setOffset( 2 );
+		minus.setOffset( 2 );
+	}
+	
+	if( plus_pressed && volume < max )
+	{
+		volume += 0.1;
+		if( volume > max )
+		{
+			volume = max;
+		}
+		
+		plus.setOffset( 1 );
+	}
+	else if( minus_pressed && volume > min )
+	{
+		volume -= 0.1;
+		if( volume < min )
+		{
+			volume = min;
+		}
+		
+		minus.setOffset( 1 );
+	}
+}
+
+
+
+void Volume_button::fadein( float v, int max )
+{
+	plus.fadein( v, max );
+	minus.fadein( v, max );
+}
+
+void Volume_button::fadeout( float v, int min )
+{
+	plus.fadeout( v, min );
+	minus.fadeout( v, min );
+}
+
+
+
+void Volume_button::setMainVolume( float volume )
+{
+	this->volume = volume;
+	min = 0;
+	max = 100;
+}
+
+float Volume_button::getMainVolume()
+{
+	return volume;
+}
+
+void Volume_button::setActive( bool active )
+{
+	this->active = active;
+	
+	if( active )
+	{
+		plus.setOffset( 0 );
+		minus.setOffset( 0 );
+	}
+}
+
+bool Volume_button::isChanged()
+{
+	if( plus_pressed || minus_pressed )
+	{
+		return true;
+	}
+	
+	return false;
+}
+
+
+
+void Volume_button::setPlayable( bool playable )
+{
+	this->playable = playable;
+}
+
+void Volume_button::setVolume( float volume )
+{
+	click.setVolume( volume );
+}
---
+++ menu/volume_button.h
@@ -0,0 +1,46 @@
+#include "own/sprite.h"
+#include "own/text.h"
+#include "own/chunk.h"
+#include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/Window/Event.hpp>
+
+class Volume_button
+{
+	MySprite plus;
+	MySprite minus;
+	float volume;
+	float min, max;
+	bool active;
+	bool plus_pressed;
+	bool minus_pressed;
+	
+	// Sound.
+	Chunk click;
+	bool playable;
+	
+public:
+	
+	// Basics.
+	Volume_button();
+	~Volume_button();
+	void free();
+	
+	void load( float left, float right, float bot, float screen_w, float screen_h );
+	bool handle( sf::Event& event );
+	void draw( sf::RenderWindow* &window );
+	
+	void fadein( float v = 1, int max = 0xFF );
+	void fadeout( float v = 1, int min = 0 );
+	
+	
+	// Support.
+	void setMainVolume( float volume );
+	float getMainVolume();
+	void setActive( bool active );
+	bool isChanged();
+	
+	
+	// Sound.
+	void setPlayable( bool playable = true );
+	void setVolume( float volume = 50 );
+};
