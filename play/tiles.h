373cf45b458cf252ea51d5ca14154933ba9900a3
devmichalek <devmichalek@gmail.com>
2017-08-21 21:11:31 +0200

    draw tiles +mechanics like collisions

--- play/tiles.h
+++ play/tiles.h
@@ -1,8 +1,45 @@
 #pragma once
 #include "own/sprite.h"
 #include <SFML/Graphics/RenderWindow.hpp>
+#include <vector>
+#include <thread>
 
 class Tiles
 {
+	float screen_w;
+	float screen_h;
+	float left;
+	float right;
 	
+	vector <MySprite*> sprites;
+	vector <sf::Vector2f> fs;
+	vector <int> types;
+	
+	// Thread stuff.
+	std::thread* myThread;
+	bool thread_ready;
+	bool ready;
+	
+public:
+	
+	// Basics.
+	Tiles();
+	~Tiles();
+	void free();
+	
+	void load( float screen_w, float screen_h );
+	void draw( sf::RenderWindow* &window );
+	
+	void fadein( float v = 1, int max = 0xFF );
+	void fadeout( float v = 1, int min = 0 );
+	
+	// Thread.
+	bool isNull();
+	bool isReady();
+	void setThread();
+	void setTiles();
+	
+	// The rest.
+	float getLeft();
+	float getRight();
 };
