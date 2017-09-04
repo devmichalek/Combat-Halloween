7f4ef119f36ea26a5b568ac8ed24e14476598707
devmichalek <devmichalek@gmail.com>
2017-09-04 15:14:29 +0200

    deleted 2 $_SESSION[''] variables

--- website/index.php
+++ website/index.php
@@ -6,7 +6,6 @@
 	unset($_SESSION['e_mail']);
 	unset($_SESSION['e_password']);
 	unset($_SESSION['e_passwordcon']);
-	unset($_SESSION['e_statute']);
 	unset($_SESSION['e_bot']);
 ?>
 
@@ -39,7 +38,7 @@
 		<div class="twelve columns">
 			<section class="header" style="text-align: center; margin-top: 5em;">
 				<div class="title myfont">
-					<h2>A dead simple, <span style="color: #F2583E; margin-right: 0em; margin-left: 0em; display: inline-block;">2D platform</span> game.</h2>
+					<h2>A dead simple, free, <span style="color: #F2583E; margin-right: 0em; margin-left: 0em; display: inline-block;">2D platform</span> game.</h2>
 				</div>
 			</section>
 		</div>
@@ -51,16 +50,16 @@
 		<!-- Specs -->
 		<div class="row">
 			<div class="six columns" style="text-align: center; margin-top: 5em;">
-					<img class="value-img" src="images/0.png" onload="this.width/=2;" onload="this.height/=2;">
+					<img class="value-img" src="images/0.png" width="240" height="124">
 					<h5 class="myfont">
-						Take a magical sword and tough shield then go fight with monsters lurking and waiting in bushes for you. Don't you want to battle all alone with them? Hold on for next update with multiplayer option!
+						Take a magical sword as well as tough shield and go fight with monsters lurking and waiting in bushes for you. Don't you want to battle all alone with them? Hold on for next update with multiplayer option!
 					</h5>
 			</div>
 			<div class="six columns" style="text-align: center; margin-top: 5em;">
-					<img class="value-img" src="images/1.png" onload="this.width/=2;" onload="this.height/=2;">
+					<img class="value-img" src="images/1.png" width="240" height="124">
 					<h5 class="myfont">
 					Built own worlds and see how the others struggle with your unnique traps and foes placed right.
-					World editor is a so much fun plus you don't have to spend lots of time learning. It's very simple.
+					World editor is much fun plus you don't have to spend lots of time learning. It's as easy as anything.
 					</h5>
 			</div>
 		</div>
@@ -68,11 +67,11 @@
 		<!-- About -->
 		<div class="row">
 			<div class="twelve columns" style="text-align: left; margin-top: 5em;">
-					<h3 class="myfont">About</h3>
+					<h3 class="myfont nobotmargin">About</h3>
 			</div>
 			<div class="twelve columns" style="text-align: left;">
 					<h5 class="myfont">
-					Combat Halloween it's something made by my human motives, because I like programming for long time then see effects of working, like a moving and fighting skeleton or just all this mechanism interlocks in and of itself, after all demi-alive animations with own destination in game. I would like to remind that I'm  not graphic designer and music maker. Just programming stuff with support as Gimp, tho. For any advices about game like new rapid falling spikes able to kill character in one second because you are fucking sick about spikes you have to contact me by email at the bottom of this website that you have open.
+					Combat Halloween is something made by my own fantasies, because I like programming for then seeing effects of my work, like a moving and fighting skeleton or fairly alive animations with own goal in game. I would like to remind that I'm  not graphic designer or else music maker. Just programming stuff with support as Gimp, tho. For any advice about game like a new rapid falling spikes able to kill character in one second, because you are fucking insane about spikes, you have to contact me by email at the bottom of this website that you've opened.
 					</h5>
 			</div>
 		</div>
@@ -80,12 +79,11 @@
 		<!-- Code -->
 		<div class="row">
 			<div class="twelve columns" style="text-align: left; margin-top: 5em;">
-					<h3 class="myfont">Code & Structure & Assets</h3>
+					<h3 class="myfont  nobotmargin">Code & Structure & Assets</h3>
 			</div>
 			<div class="twelve columns" style="text-align: left;">
 					<h5 class="myfont">
-					Let's start with code. 
-					Assets: More often than not I used to take sprites from <a href="http://www.gameart2d.com/" target="_blank">Game Art 2D</a> and <a href="https://opengameart.org/" target="_blank">Open Game Art</a>. Music is made by <a href="https://soundcloud.com/simon_hjort" target="_blank">Simon Hjort</a> who is an instrumental composer stationed in Sweden.
+					Let's start with code. <b>C++</b> and <b>SFML</b> nothing less nothing more. <b>Skeleton</b> framework is used in every part of this website. Structure is simple, one huge class called <b>engine</b> contains all smaller classes such as <b>intro</b>, <b>logging</b>, <b>menu</b> etc. Then in all of those smaller classes we can find something like buttons, background, music, keyboard mechanism. About assets: More often than not I used to take sprites from <a href="http://www.gameart2d.com/" target="_blank">Game Art 2D</a> and <a href="https://opengameart.org/" target="_blank">Open Game Art</a>. Music is made by <a href="https://soundcloud.com/simon_hjort" target="_blank">Simon Hjort</a> who is an instrumental composer stationed in Sweden. Chunks used to be taken from random sites with appropriate permits.
 					</h5>
 			</div>
 		</div>
