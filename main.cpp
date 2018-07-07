d8df6b8a1e9e806c790900099e78a863b27ba2d1
devmichalek <devmichalek@gmail.com>
2018-07-07 15:48:47 +0200

    deleted dirent info

--- main.cpp
+++ main.cpp
@@ -1,55 +1,8 @@
-<<<<<<< HEAD
-/**
-    main.cpp
-    Purpose: Create engine, call loop then delete.
-
-    @author Adrian Michalek
-    @version 2016.08.14
-	@email adrmic98@gmail.com
-*/
-
 #include "engine.h"
 
-int main( int argc, char** argv )
+int main()
 {
-    Engine* engine = new Engine;
-=======
-#include "engine.h"
-// #include "dirent.h"
-
-int main( int argc, char **argv )
-{
-	Engine* engine = new Engine;
->>>>>>> Combat-Halloween/master
-
+	std::unique_ptr<Engine> engine = std::make_unique<Engine>();
 	engine->loop();
-
-    delete engine;
-    engine = NULL;
-<<<<<<< HEAD
-
-    return EXIT_SUCCESS;
-}
-=======
-	
-	/*
-	DIR *dir;
-	struct dirent *ent;
-	if ((dir = opendir ("C:\\Users\\adriqun\\Documents\\cppworkspace")) != NULL) {
-	  // print all the files and directories within directory
-	  while ((ent = readdir (dir)) != NULL) {
-		printf ("%s\n", ent->d_name);
-	  }
-	  closedir (dir);
-	} else {
-	  // could not open directory
-	  perror ("");
-	  return EXIT_FAILURE;
-	}
-	
-	string s;
-	cin >> s;
-	*/
 	return EXIT_SUCCESS;
 }
->>>>>>> Combat-Halloween/master
