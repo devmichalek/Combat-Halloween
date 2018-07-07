688edac957e1b12ce802c0bb164867b4d7ff0b74
devmichalek <devmichalek@gmail.com>
2018-07-07 15:50:34 +0200

    rename reload->reset

--- menu/button.cpp
+++ menu/button.cpp
@@ -2,61 +2,140 @@
 
 Button::Button()
 {
-	free();
+	locked = false;
 }
 
 Button::~Button()
 {
+	locked = false;
+}
+
+void Button::load(const char* path, bool locked, int numOfOffsets)
+{
+	free();
+
+	click.load("sounds/click.wav");
+	button.load(path, numOfOffsets);
+
+	this->locked = locked;
+	locked ? button.setOffset(numOfOffsets - 1) : button.setOffset(0);
+}
+
+void Button::setPosition(float x, float y)
+{
+	button.setPosition(x, y);
+}
+
+void Button::setScale(float x, float y)
+{
+	button.setScale(x, y);
+}
+
+void Button::setVolume(float volume)
+{
+	click.setVolume(volume);
+}
+
+void Button::draw(sf::RenderWindow* &window)
+{
+	window->draw(button.get());
+}
+
+void Button::fadein(float v, int max)
+{
+	button.fadein(v, max);
+}
+
+void Button::fadeout(float v, int min)
+{
+	button.fadeout(v, min);
+}
+
+float Button::getLeft() const
+{
+	return button.getLeft();
+}
+
+float Button::getRight() const
+{
+	return button.getRight();
+}
+
+float Button::getTop() const
+{
+	return button.getTop();
+}
+
+float Button::getBot() const
+{
+	return button.getBot();
+}
+
+float Button::getWidth() const
+{
+	return button.getWidth();
+}
+
+float Button::getHeight() const
+{
+	return button.getHeight();
+}
+
+void Button::lock()
+{
+	if (!locked)
+	{
+		locked = true;
+		button.setOffset(2);
+	}
+}
+
+void Button::unlock()
+{
+	if (locked)
+	{
+		locked = false;
+		button.setOffset(0);
+	}
+}
+
+void Button::reset()
+{
+	free();
+	locked ? button.setOffset(button.getNumOfOffsets() - 1) : button.setOffset(0);
+}
+
+
+
+
+SquareButton::SquareButton()
+{
 	free();
 }
 
-void Button::free()
+SquareButton::~SquareButton()
 {
-	button.free();
+	free();
+}
+
+void SquareButton::free()
+{
 	locked = false;
 	pressed = false;
-	
-	click.free();
 }
 
-
-
-void Button::load( float x, float y, string path, bool locked )
+void SquareButton::handle(sf::Event& event)
 {
-	free();
-	
-	this->locked = locked;
-	
-	button.setIdentity( "button-button" );
-	button.load( path, 3 );
-	button.setPosition( x, y );
-	
-	if( locked )
+	if (!locked)
 	{
-		button.setOffset( 2 );
-	}
-	
-	click.setIdentity( "button-click" );
-	click.load( "sounds/click.wav" );
-}
-
-void Button::setScale( float x, float y )
-{
-	button.setScale( x, y );
-}
-
-void Button::handle( sf::Event& event )
-{
-	if( !locked )
-	{
-		if( event.type == sf::Event::MouseButtonPressed )
+		if (event.type == sf::Event::MouseButtonPressed)
 		{
-			if( event.mouseButton.button == sf::Mouse::Left )
+			if (event.mouseButton.button == sf::Mouse::Left)
 			{
-				if( button.checkCollision( event.mouseButton.x, event.mouseButton.y ) )
+				if (button.checkCollision(event.mouseButton.x, event.mouseButton.y))
 				{
 					click.play();
-					button.setOffset( 1 );
+					button.setOffset(1);
 					pressed = true;
 				}
 			}
@@ -64,86 +143,321 @@ void Button::handle( sf::Event& event )
 	}
 }
 
-void Button::draw( sf::RenderWindow* &window )
-{
-	window->draw( button.get() );
-}
-
-
-
-void Button::fadein( float v, int max )
-{
-	button.fadein( v, max );
-}
-
-void Button::fadeout( float v, int min )
-{
-	button.fadeout( v, min );
-}
-
-bool Button::isPressed()
-{
-	return pressed;
-}
-
-void Button::setPressed()
+void SquareButton::setPressed()
 {
 	pressed = true;
 }
 
-float Button::getBot()
+const bool& SquareButton::isPressed() const
 {
-	return button.getBot();
-}
-
-float Button::getLeft()
-{
-	return button.getLeft();
-}
-
-float Button::getTop()
-{
-	return button.getTop();
+	return pressed;
 }
 
 
 
-void Button::lock()
+
+Circlebutton::Circlebutton()
 {
-	if( !locked )
+	free();
+}
+
+Circlebutton::~Circlebutton()
+{
+	free();
+}
+
+void Circlebutton::free()
+{
+	locked = false;
+	active = false;
+	changed = false;
+}
+
+bool Circlebutton::handle(sf::Event& event)
+{
+	if (!locked)
 	{
-		locked = true;
-		button.setOffset( 2 );
+		if (event.type == sf::Event::MouseButtonPressed)
+		{
+			if (event.mouseButton.button == sf::Mouse::Left)
+			{
+				if (button.checkCollisionCircle(event.mouseButton.x, event.mouseButton.y))
+				{
+					click.play();
+					changeActive();
+					changed = true;
+					return true;
+				}
+			}
+		}
+	}
+
+	return false;
+}
+
+bool Circlebutton::isActive()
+{
+	return active;
+}
+
+void Circlebutton::setActive(bool active)
+{
+	this->active = active;
+	active ? button.setOffset(1) : button.setOffset(0);
+}
+
+void Circlebutton::changeActive()
+{
+	active = !active;
+	active ? button.setOffset(1) : button.setOffset(0);
+}
+
+bool Circlebutton::hasChanged()
+{
+	if (changed)
+	{
+		changed = false;
+		return true;
+	}
+
+	return false;
+}
+
+void Circlebutton::setChanged(bool changed)
+{
+	this->changed = changed;
+}
+
+
+
+
+LinkButton::LinkButton()
+{
+	free();
+}
+
+LinkButton::~LinkButton()
+{
+	free();
+}
+
+void LinkButton::free()
+{
+	locked = false;
+	url = "";
+}
+
+void LinkButton::handle(sf::Event& event)
+{
+	if (!locked)
+	{
+		if (event.type == sf::Event::MouseButtonPressed)
+		{
+			if (event.mouseButton.button == sf::Mouse::Left)
+			{
+				if (button.checkCollisionCircle(event.mouseButton.x, event.mouseButton.y))
+				{
+					click.play();
+					openWebsite();
+					button.setOffset(1);
+				}
+			}
+		}
+
+		if (event.type == sf::Event::MouseButtonReleased)
+		{
+			button.setOffset(0);
+		}
 	}
 }
 
-void Button::unlock()
+void LinkButton::setUrl(std::string newUrl)
 {
-	if( locked )
+	url = newUrl;
+}
+
+void LinkButton::openWebsite()
+{
+	std::string command = "start " + url;
+	system(command.c_str());
+}
+
+
+
+
+VolumeButton::VolumeButton() :min(0), max(100)
+{
+	free();
+}
+
+VolumeButton::~VolumeButton()
+{
+	free();
+}
+
+void VolumeButton::free()
+{
+	volume = 0;
+	locked = false;
+	plus_pressed = false;
+	minus_pressed = false;
+}
+
+
+
+void VolumeButton::load(float screen_w, float screen_h)
+{
+	free();
+
+	click.load("sounds/click.wav");
+
+	plus.load("images/buttons/plus.png", 3);
+	plus.setScale(screen_w / 2560, screen_h / 1440);
+
+	minus.load("images/buttons/minus.png", 3);
+	minus.setScale(screen_w / 2560, screen_h / 1440);
+}
+
+void VolumeButton::setPosition(float left, float right, float bot)
+{
+	plus.setPosition(right - plus.getWidth(), bot - plus.getHeight() / 1.2);
+	minus.setPosition(left, bot - minus.getHeight() / 1.2);
+}
+
+bool VolumeButton::handle(sf::Event& event)
+{
+	if (!locked)
 	{
-		locked = false;
-		button.setOffset( 0 );
+		if (event.type == sf::Event::MouseButtonReleased)
+		{
+
+			plus.setOffset(0);
+			minus.setOffset(0);
+			plus_pressed = false;
+			minus_pressed = false;
+
+		}
+	}
+
+	if (event.type == sf::Event::MouseButtonPressed)
+	{
+		if (event.mouseButton.button == sf::Mouse::Left)
+		{
+			float x = event.mouseButton.x;
+			float y = event.mouseButton.y;
+
+			if (plus.checkCollisionCircle(x, y))
+			{
+				if (!locked)
+				{
+					if (volume < max)
+					{
+						click.play();
+					}
+
+					plus_pressed = true;
+				}
+
+				return true;
+			}
+			else if (minus.checkCollisionCircle(x, y))
+			{
+				if (!locked)
+				{
+					if (volume > min)
+					{
+						click.play();
+					}
+
+					minus_pressed = true;
+				}
+
+				return true;
+			}
+		}
+	}
+
+	return false;
+}
+
+void VolumeButton::draw(sf::RenderWindow* &window)
+{
+	window->draw(plus.get());
+	window->draw(minus.get());
+}
+
+void VolumeButton::mechanics(double elapsedTime)
+{
+	if (plus_pressed && volume < max)
+	{
+		volume += elapsedTime * 0xFF / 4;
+		if (volume > max)
+		{
+			volume = max;
+		}
+
+		plus.setOffset(1);
+	}
+	else if (minus_pressed && volume > min)
+	{
+		volume -= elapsedTime * 0xFF / 4;
+		if (volume < min)
+		{
+			volume = min;
+		}
+
+		minus.setOffset(1);
 	}
 }
 
-void Button::reload()
+
+
+void VolumeButton::fadein(float v, int max)
 {
-	pressed = false;
-	
-	if( !locked )
+	plus.fadein(v, max);
+	minus.fadein(v, max);
+}
+
+void VolumeButton::fadeout(float v, int min)
+{
+	plus.fadeout(v, min);
+	minus.fadeout(v, min);
+}
+
+
+
+void VolumeButton::setGlobalVolume(float volume)
+{
+	this->volume = volume;
+}
+
+const float& VolumeButton::getGlobalVolume() const
+{
+	return volume;
+}
+
+void VolumeButton::setLocked(bool locked)
+{
+	this->locked = locked;
+
+	if (!locked)
 	{
-		button.setOffset( 0 );
+		plus.setOffset(0);
+		minus.setOffset(0);
+	}
+	else
+	{
+		plus.setOffset(2);
+		minus.setOffset(2);
 	}
 }
 
-
-
-void Button::setPlayable( bool playable )
+bool VolumeButton::hasChanged() const
 {
-	click.setPlayable( playable );
+	return plus_pressed || minus_pressed;
 }
 
-void Button::setVolume( float volume )
+void VolumeButton::setVolume(float volume)
 {
-	click.setVolume( volume );
+	click.setVolume(volume);
 }
--- menu/button.h
+++ menu/button.h
@@ -1,46 +1,118 @@
 #pragma once
-#include "own/sprite.h"
-#include "own/chunk.h"
+#include "sprite.h"
+#include "sound.h"
 #include <SFML/Graphics/RenderWindow.hpp>
 #include <SFML/Window/Event.hpp>
 
 class Button
 {
-	MySprite button;
+protected:
+	cmm::Sound click;
+	cmm::Sprite button;
 	bool locked;
-	bool pressed;
-	
-	// Sound.
-	Chunk click;
-	
+
 public:
-	
-	// Basics.
 	Button();
-	~Button();
-	void free();
+	virtual ~Button();
+	virtual void free() = 0;
+
+	void load(const char* path, bool locked = false, int numOfOffsets = 3);
+	void setPosition(float x, float y);
+	void setScale(float x, float y);
+	void setVolume(float volume);
 	
-	void load( float x, float y, string path, bool locked = false );
-	void setScale( float x, float y );
-	void handle( sf::Event& event );
-	void draw( sf::RenderWindow* &window );
-	
-	void fadein( float v = 1, int max = 0xFF );
-	void fadeout( float v = 1, int min = 0 );
-	
-	// Getters.
-	bool isPressed();
-	void setPressed();
-	float getBot();
-	float getLeft();
-	float getTop();
-	
-	// Setters.
+	void draw(sf::RenderWindow* &window);
+	void fadein(float v = 1, int max = 0xFF);
+	void fadeout(float v = 1, int min = 0);
+
+	float getLeft() const;
+	float getRight() const;
+	float getTop() const;
+	float getBot() const;
+	float getWidth() const;
+	float getHeight() const;
+
 	void lock();
 	void unlock();
-	void reload();
-	
-	// Sound.
-	void setPlayable( bool playable );
-	void setVolume( float volume );
+	void reset();
+};
+
+class SquareButton :public Button
+{
+	bool pressed;
+
+public:
+	SquareButton();
+	~SquareButton();
+	void free();
+
+	void handle(sf::Event& event);
+	void setPressed();
+	const bool& isPressed() const;
+};
+
+class Circlebutton :public Button
+{
+	bool active;
+	bool changed;
+
+public:
+	Circlebutton();
+	~Circlebutton();
+	void free();
+
+	bool handle(sf::Event& event);
+	bool isActive();
+	void setActive(bool active);
+	void changeActive();
+	bool hasChanged();
+	void setChanged(bool changed);
+};
+
+class LinkButton :public Button
+{
+	std::string url;
+
+public:
+	LinkButton();
+	~LinkButton();
+	void free();
+
+	void handle(sf::Event& event);
+	void setUrl(std::string newUrl);
+	void openWebsite();
+};
+
+class VolumeButton
+{
+	cmm::Sound click;
+	cmm::Sprite plus;
+	cmm::Sprite minus;
+
+	float volume;
+	const float min, max;
+
+	bool locked;
+	bool plus_pressed;
+	bool minus_pressed;
+
+public:
+	VolumeButton();
+	~VolumeButton();
+	void free();
+
+	void load(float screen_w, float screen_h);
+	void setPosition(float left, float right, float bot);
+	bool handle(sf::Event& event);
+	void draw(sf::RenderWindow* &window);
+	void mechanics(double elapsedTime);
+
+	void fadein(float v = 1, int max = 0xFF);
+	void fadeout(float v = 1, int min = 0);
+
+	void setGlobalVolume(float volume);
+	const float& getGlobalVolume() const;
+	void setLocked(bool locked);
+	bool hasChanged() const; // Whenever minus button is pressed or plus button is pressed.
+	void setVolume(float volume);
 };
