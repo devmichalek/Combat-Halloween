b0af4a314b65725af4b98adfaf8109cf64a6bf3c
devmichalek <devmichalek@gmail.com>
2018-07-14 22:47:19 +0200

    global js file

---
+++ website/bugs/main.js
@@ -0,0 +1,42 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.modal');
+    var instances = M.Modal.init(elems, options);
+  });
+
+  // Or with jQuery
+
+  $(document).ready(function(){
+    $('.modal').modal();
+  });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('select');
+    var instances = M.FormSelect.init(elems, options);
+  });
+
+  // Or with jQuery
+
+  $(document).ready(function(){
+    $('select').formSelect();
+  });
+
+  $('#description').val('New Text');
+  M.textareaAutoResize($('#description'));
+
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.datepicker');
+    var instances = M.Datepicker.init(elems, options);
+  });
+
+  // Or with jQuery
+
+  $(document).ready(function(){
+    $('.datepicker').datepicker();
+  });
+  
+  $(".button-collapse").sideNav();
+  
+  $('.datepicker').datepicker({
+    minDate : new Date()
+});
