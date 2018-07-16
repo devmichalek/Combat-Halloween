9e42890dc8f560b77c7195eae94a3f7d01411430
devmichalek <devmichalek@gmail.com>
2018-07-16 23:50:29 +0200

    addbug module

--- website/bugs/addbug.php
+++ website/bugs/addbug.php
@@ -28,7 +28,7 @@
 	{
 		$result->free_result();
 		$connection->close();
-		header('Location: http://combathalloween.netne.net/home.php');
+		header('Location: http://combathalloween.netne.net/bugs/index.php');
 	}
 ?>
 
@@ -43,7 +43,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=1.0">
 
 	<!-- Site Properties -->
-	<title>Combat Halloween Add Report</title>
+	<title>Combat Halloween Add Bug</title>
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-rc.2/css/materialize.min.css">
   	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 	<link rel="stylesheet" type="text/css" href="../main.css">
@@ -62,37 +62,35 @@
 	<nav>
 	    <div class="nav-wrapper">
 	      	<a class="nav-main brand-logo">&nbsp;&nbsp;&nbsp;Combat&nbsp;Halloween</a>
-	      	<a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+	      	<a href="" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
 	      	<ul id="nav-mobile" class="right hide-on-med-and-down">
 	      		<li><a class="nav" href="../home.php">Home</a></li>
-	        	<li><a class="nav" href="index.php">Reports</a></li>
-	        	<li><a class="nav" href="modifyreport.php">Modify Report</a></li>
+	        	<li><a class="nav btn red lighten-1 nav-button" href="index.php">Bugs</a></li>
 	      	</ul>
 	    </div>
   	</nav>
   	</div>
   	<ul class="nav-main sidenav" id="mobile-demo">
 	    <li><a class="nav" href="../home.php">Home</a></li>
-      	<li><a class="nav" href="index.php">Reports</a></li>
-      	<li><a class="nav" href="modifyreport.php">Modify Report</a></li>
+      	<li><a class="nav btn red lighten-1 nav-button" href="index.php">Bugs</a></li>
 	</ul>
 
 	<div class="container center">
 		
 		<!-- Header -->
 		<div class="s12">
-					<h2>Combat Halloween Add Report</h2>
+					<h2>Combat Halloween Add Bug</h2>
 		</div>
 		
 		
 
 
-		<form class="col s6 push-s3" method="post" action="addrep.php">
+		<form class="col s6 push-s3" method="post" action="add.php" style="margin-bottom: 20px;">
 
 			<!-- NAME -->
 			<div class="row">
 	        <div class="input-field col s6 push-s3">
-	          <input id="name" type="text" class="validate" name="name">
+	          <input id="name" type="text" class="validate" name="name" value="None">
 	          <label for="name"><h5 class="modcon2">Name</h5></label>
 	        </div>
 	      	</div>
@@ -102,9 +100,10 @@
 			<div class="input-field col s3 push-s3">
 		    <select name="type">
 	        	<option value="Bug">Bug</option>
+	        	<option value="Change">Change</option>
 	        	<option value="New Feature">New Feature</option>
 	        	<option value="Syntax Error">Syntax Error</option>
-	        	<option value="Other Report">Other Report</option>
+	        	<option value="Other">Other</option>
 		    </select>
 		    	<label>Type</label>
 		  	</div>
@@ -114,11 +113,10 @@
 		    <select name="actiontodo">
 	        	<option value="Repair">Repair</option>
 	        	<option value="Add">Add</option>
-	        	<option value="Correct">Correct</option>
-	        	<option value="Modify">Modify</option>
-	        	<option value="Suggestion not to correct">Suggestion not to correct</option>
-	        	<option value="Remove">Remove</option>
 	        	<option value="Create">Create</option>
+	        	<option value="Modify">Modify</option>
+	        	<option value="Correct">Correct</option>
+	        	<option value="Remove">Remove</option>
 		    </select>
 		    	<label>Action to do</label>
 		  	</div>
@@ -127,19 +125,42 @@
 		  	<!-- DESCRIPTION -->
 		     <div class="row">
 		       	<div class="input-field col s6 push-s3">
-		          	<textarea id="description" class="materialize-textarea" name="description" value=""></textarea>
-		          	<label for="description">Description</label>
+		          	<textarea class="materialize-textarea" name="description">None</textarea>
+		          	<label>Description</label>
 		       	</div>
 		    </div>
 
-		    <!-- LOCATION OF THE REPORT -->
+		    <!-- LOCATION OF THE BUG -->
 			<div class="row">
 	        <div class="input-field col s6 push-s3">
-	          <input id="location" type="text" class="validate" name="location">
+	          <input id="location" type="text" class="validate" name="location" value="None">
 	          <label for="location"><h5 class="modcon2">Location</h5></label>
 	        </div>
 	      	</div>
-
+	      	
+	      	<!-- SEVERITY  -->
+	      	<div class="row col s6">
+			<div class="input-field col s3 push-s3">
+		    <select name="severity">
+	        	<option value="Fatal">Fatal</option>
+	        	<option value="Serious">Serious</option>
+	        	<option value="Minor">Minor</option>
+		    </select>
+		    	<label>Severity</label>
+		  	</div>
+		  	
+		  	<!-- PRIORITY  -->
+			<div class="input-field col s3 push-s3">
+		    <select name="priority">
+	        	<option value="Must Do">Must Do</option>
+	        	<option value="Low">Low</option>
+	        	<option value="Normal">Normal</option>
+	        	<option value="High">High</option>
+		    </select>
+		    	<label>Priority</label>
+		  	</div>
+            </div>
+            
 	      	<!-- DEADLINE -->
 			<div class="row">
 	        <div class="input-field col s6 push-s3">
@@ -156,16 +177,29 @@
 	        </div>
 	      	</div>
 
-	      	<!-- STATUS -->
+	      	<!-- RESOLUTION -->
 			<div class="row s6">
 		  	<div class="input-field col s3 push-s3">
-		    <select name="status">
+		    <select name="resolution">
+		        <option value="Deferred">Deferred</option>
+		        <option value="Disagree With Suggestion">Disagree With Suggestion</option>
+		        <option value="Duplicated">Duplicated</option>
+		        <option value="Fixed">Fixed</option>
+		        <option value="Hold">Hold</option>
+		        <option value="Implemented">Implemented</option>
+		        <option value="In Development">In Development</option>
+		        <option value="Irreproducible">Irreproducible</option>
+		        <option value="Known Problem">Known Problem</option>
+	        	<option value="Need More Info">Need More Info</option>
 	        	<option value="New">New</option>
-	        	<option value="In development">In development</option>
+	        	<option value="Obsolete">Obsolete</option>
+	        	<option value="Reeopen">Reeopen</option>
 	        	<option value="Revised">Revised</option>
-	        	<option value="Fixed">Fixed</option>
+	        	<option value="Suggestion Not To Correct">Suggestion Not To Correct</option>
+	        	<option value="Support Action Required">Support Action Required</option>
+	        	<option value="Withdrawn">Withdrawn</option>
 		    </select>
-		    	<label>Status</label>
+		    	<label>Resolution</label>
 		  	</div>
 			
 			<!-- DEVELOPER -->
@@ -183,6 +217,7 @@
 			<button class="btn waves-effect waves-light red lighten-1 nav-button" type="submit" name="action">Add
 		    <i class="material-icons right">send</i>
 		  </button>
+		  
 		</form>
 	</div>
 
@@ -193,10 +228,19 @@
 	<footer class="page-footer">
       <div class="container">
         <div class="row">
-          <div class="col l6 s12">
-            <h5 class="white-text">Adrian Michałek </h5>
-            <p class="grey-text text-lighten-4">Project of SFML & QT.</p>
-          </div>
+            <div class="col s4">
+                <h5 class="white-text">Admins:</h5>
+                <p class="white-text">Adrian Michałek (devmichalek@gmail.com)</p>
+            </div>
+            <div class="col s4">
+                <h5 class="white-text">Moderators:</h5>
+                <p class="white-text">Sebastian Bialucha (sebastek321@gmail.com)</p>
+                <p class="white-text">Kamil Gawlik (kamilgawlik@icloud.com)</p>
+            </div>
+            <div class="col s4">
+                <h5 class="white-text">About:</h5>
+                <p class="grey-text text-lighten-4">Project of SFML & QT.</p>
+            </div>
         </div>
       </div>
       <div class="footer-copyright">
