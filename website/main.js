d610674dc4de6ea82a5d3bc308d654707dec5eff
devmichalek <devmichalek@gmail.com>
2018-07-17 23:36:57 +0200

    update

--- website/main.js
+++ website/main.js
@@ -1,35 +1,49 @@
-  $(document).ready(function(){
-    $('.sidenav').sidenav();
-  });
-
-
-$('.dropdown-trigger').dropdown();
-
-$(".button-collapse").sideNav();
-
-
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.parallax');
-    var instances = M.Parallax.init(elems, options);
-  });
-
 $(document).ready(function(){
-    $('.parallax').parallax();
-  });
-
-$(document).ready(function(){
-    $('.scrollspy').scrollSpy();
-  });
-
-$(document).ready(function(){
-// the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
-$('.modal').modal();
+  $('.modal').modal();
 });
 
 $('#modal1').modal('open');
 
 $('#modal1').modal('close');
 
+
+
+$(document).ready(function(){
+  $('select').formSelect();
+});
+
+$(document).ready(function(){
+    $('.parallax').parallax();
+  });
+
+
+$(document).ready(function(){
+  $('.datepicker').datepicker();
+});
+
+
+
+$(document).ready(function(){
+  $('.sidenav').sidenav();
+});
+
+$('.dropdown-trigger').dropdown();
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.dropdown-trigger');
+    var instances = M.Dropdown.init(elems, options);
+  });
+
+$(".button-collapse").sideNav();
+
+
+$('select').material_select();
+
+
 $(document).ready(function() {
     M.updateTextFields();
+  });
+
+$(document).ready(function(){
+    $('.scrollspy').scrollSpy();
   });
