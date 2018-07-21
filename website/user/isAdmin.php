73ee6b6c81d7158a549919ba0e9dd1a082142b03
devmichalek <devmichalek@gmail.com>
2018-07-21 13:07:08 +0200

    checks if user has the admin permissions

---
+++ website/user/isAdmin.php
@@ -0,0 +1,25 @@
+<?php
+	require_once "../connect.php";
+	mysqli_report(MYSQLI_REPORT_STRICT);
+	$connection = @new mysqli($host, $db_user, $db_password, $db_name);
+
+	if($result = @$connection->query( sprintf("SELECT * FROM users WHERE username='%s'", mysqli_real_escape_string($connection, $_SESSION['username']))))
+	{
+		$row = $result->fetch_assoc();
+		
+		if($row['permissions'] != "admin")
+		{
+			$result->free_result();
+			$connection->close();
+			header('Location: http://combathalloween.netne.net/home.php');
+		}
+
+		$result->free_result();
+	}
+	else
+	{
+		$result->free_result();
+		$connection->close();
+		header('Location: http://combathalloween.netne.net/home.php');
+	}
+?>
