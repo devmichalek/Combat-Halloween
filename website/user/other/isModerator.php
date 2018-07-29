251baf535457d5bfe9e69d8984939d2aa2a62cbc
devmichalek <devmichalek@gmail.com>
2018-07-29 23:38:17 +0200

    file checks if user is a moderator or admin

---
+++ website/user/other/isModerator.php
@@ -0,0 +1,27 @@
+<?php
+	require_once("/storage/ssd1/697/6426697/public_html/connect.php");
+	mysqli_report(MYSQLI_REPORT_STRICT);
+	$connection = @new mysqli($host, $db_user, $db_password, $db_name);
+
+	if($result = @$connection->query( sprintf("SELECT * FROM users WHERE username='%s'", mysqli_real_escape_string($connection, $_SESSION['username']))))
+	{
+		$row = $result->fetch_assoc();
+		
+		if($row['permissions'] == "user")
+		{
+			$result->free_result();
+			$connection->close();
+			header('Location: http://combathalloween.netne.net/home.php');
+			exit();
+		}
+
+		$result->free_result();
+	}
+	else
+	{
+		$result->free_result();
+		$connection->close();
+		header('Location: http://combathalloween.netne.net/home.php');
+		exit();
+	}
+?>
