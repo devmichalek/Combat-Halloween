03598bb64713b824d01e77c63fd8b5dd9ba493bb
devmichalek <devmichalek@gmail.com>
2018-07-30 20:04:13 +0200

    new colors, new classes, loading navbar from file

--- website/common/sidenav-b-logged.php
+++ website/common/sidenav-b-logged.php
@@ -1,4 +1,4 @@
-<li><div class="user-view">
+    <li><div class="user-view">
 	<div class="background" ></div>
   	<?php
   	    echo '<img class="circle" src=';
@@ -8,15 +8,15 @@
         else
             echo '"http://combathalloween.netne.net/images/users/default.png">';
   	?>
-  	<div class="name" style="color: black;"><?php echo $_SESSION['username']; ?></div>
-  	<div class="email" style="color: black;"><?php echo $_SESSION['email']; ?><br>Permissions: <span style="color: red;"><?php echo $_SESSION['permissions']; ?></span></div>
-</div></li>
+  	<div class="name japokki-black"><?php echo $_SESSION['username']; ?></div>
+  	<div class="email japokki-black"><?php echo $_SESSION['email']; ?><br>Permissions: <span style="color: red;"><?php echo $_SESSION['permissions']; ?></span></div>
+    </div></li>
 
-<li><a class="subheader light-blue lighten-3 center" style="margin-top: -10px;"><i class="material-icons right">person</i>Account </a></li>
-<li><a href="http://combathalloween.netne.net/user/other/profile.php#changeUsername"><i class="material-icons">person</i>Change Username</a></li>
-<li><a href="http://combathalloween.netne.net/user/other/profile.php#changePassword"><i class="material-icons">person_outline</i>Change Password</a></li>
-<li><a href="http://combathalloween.netne.net/user/other/profile.php#changeEmail"><i class="material-icons">email</i>Change Email</a></li>
-
-<li><div class="divider"></div></li>
-<li><a class="subheader blue lighten-3 center"><i class="material-icons right">format_paint</i>Other</a></li>
-<li><a href="http://combathalloween.netne.net/user/other/profile.php#changeAvatar"><i class="material-icons">contacts</i>Change Avatar</a></li>
+    <li><a class="subheader center bg-black japokki-white" style="margin-top: -10px;"><i class="material-icons right char-white">person</i>Account </a></li>
+    <li><a class="japokki-black" href="http://combathalloween.netne.net/user/other/profile.php#changeUsername"><i class="material-icons">person</i>Change Username</a></li>
+    <li><a class="japokki-black" href="http://combathalloween.netne.net/user/other/profile.php#changePassword"><i class="material-icons">drag_handle</i>Change Password</a></li>
+    <li><a class="japokki-black" href="http://combathalloween.netne.net/user/other/profile.php#changeEmail"><i class="material-icons">email</i>Change Email</a></li>
+    
+    <li><div class="divider"></div></li>
+    <li><a class="subheader center bg-black japokki-white"><i class="material-icons right char-white">format_paint</i>Other </a></li>
+    <li><a class="japokki-black" href="http://combathalloween.netne.net/user/other/profile.php#changeAvatar"><i class="material-icons">contacts</i>Change Avatar</a></li>
--- website/common/sidenav-e.php
+++ website/common/sidenav-e.php
@@ -1,2 +1,11 @@
-<!-- SIDENAV END -->
+    <!-- If user is logged then show log out button -->
+    <?php
+        if(isset($_SESSION['logged']))
+        {
+            echo '<li><a class="japokki-black" href="http://combathalloween.netne.net/user/login/logout.php"><i class="material-icons">exit_to_app</i>Log Out</a></li>';
+        }
+    ?>
+
+<!-- End of the list -->
 </ul>
+<!-- SIDENAV END -->
