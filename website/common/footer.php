73a02e75c6a37cd705d6ed696a38bb4e05a93d61
devmichalek <devmichalek@gmail.com>
2018-07-30 20:03:50 +0200

    new colors, new classes, loading navbar from file

--- website/common/footer.php
+++ website/common/footer.php
@@ -3,10 +3,14 @@
     if(isset($_SESSION['logged']))
     {
         echo '<div class="fixed-action-btn horizontal click-to-toggle">';
-        echo '<a class="btn-floating btn-large red lighten-1"><i class="large material-icons">format_quote</i></a>';
+        echo '<a class="btn-floating pulse btn-large red lighten-1"><i class="large material-icons">attach_file</i></a>';
         echo '<ul>';
-        echo '<li><a class="btn-floating red lighten-1" href="http://combathalloween.netne.net/bugs/other/documentation.php"><i class="material-icons">insert_chart</i></a></li>';
-        echo '<li><a class="btn-floating blue lighten-1 sidenav-trigger" href="#" data-target="mobile-demo"><i class="material-icons">person</i></a></li>';
+        echo '<li><a class="btn-floating pulse amber lighten-1" href="http://combathalloween.netne.net/other/commands.php" data-target="mobile-demo"><i class="material-icons">receipt</i></a></li>';
+        echo '<li><a class="btn-floating red lighten-1';
+        if($_SESSION['permissions'] == "user")  echo " disabled ";
+        else                                    echo " pulse ";
+        echo '" href="http://combathalloween.netne.net/bugs/other/documentation.php"><i class="material-icons">insert_chart</i></a></li>';
+        echo '<li><a class="btn-floating pulse blue-grey darken-1 sidenav-trigger" href="#" data-target="mobile-demo"><i class="material-icons">person</i></a></li>';
         echo '</ul>';
         echo '</div>';
     }
@@ -14,37 +18,40 @@
 </main>
 
 <!-- Footer -->
-<footer class="page-footer">
+<footer class="page-footer bg-black japokki-white">
     <div class="container">
     <div class="row center">
-        <div class="col s12 m4">
-            <h5 class="white-text">Admins:</h5>
-            <p class="white-text">Adrian Michałek<br>devmichalek@gmail.com</p>
+        <div class="col s12 m3">
+            <h5>Code & Structure:</h5>
+            <div class="chip"><img src="http://combathalloween.netne.net/images/users/devmichalek@gmail.com.jpg" alt="Contact Person">Adrian Michałek</div><br>
+            <div class="chip">devmichalek@gmail.com<i class="close material-icons">close</i></div>
         </div>
-        <div class="col s12 m4">
-            <h5 class="white-text">Moderators:</h5>
-            <p class="white-text">Sebastian Bialucha<br>sebastek321@gmail.com</p>
-            <p class="white-text">Kamil Gawlik<br>kamilgawlik@icloud.com</p>
+        <div class="col s12 m3">
+            <h5>Support:</h5>
+            <div class="chip"><img src="http://combathalloween.netne.net/images/users/sebastek321@gmail.com.jpg" alt="Contact Person">Sebastian Bialucha</div><br>
+            <div class="chip">sebastek321@gmail.com<i class="close material-icons">close</i></div>
+            <div class="chip"><img src="http://combathalloween.netne.net/images/users/kamilgawlik@icloud.com.jpg" alt="Contact Person">Kamil Gawlik</div><br>
+            <div class="chip">kamilgawlik@icloud.com<i class="close material-icons">close</i></div>
         </div>
-        <div class="col s12 m4">
-            <h5 class="white-text">About:</h5>
-            <p class="grey-text text-lighten-4">Project of SFML & QT.</p>
+        <div class="col s12 m6">
+            <h5>Project Structure:</h5>
+            <p class="grey-text text-lighten-4">The website based on Materialize and Chart.js, some PHP & SQL code and mostly HTML & CSS & JS. The game was made with C++ language (C++14 standard) and SFML. Map editor was written with QT modules like Network, Window or Graphics. The whole project "Combat Halloween" was developed with Agile Software Development & SWOT approach.</p>
         </div>
     </div>
-  <div class="footer-copyright col s12 m4">
-    <a class="modal-trigger waves-effect waves-light btn" href="#license">©2018 Terms of Use</a>
-  </div>
+    <div class="footer-copyright col s12 m4">
+    <a class="modal-trigger waves-effect waves-light btn blue-grey darken-1" href="#license">©2018 Terms of Use</a>
+    </div>
   </div>
 </footer>
 
   <!-- LICENSE -->
-<div id="license" class="modal modal-fixed-footer">
+<div id="license" class="modal modal-fixed-footer japokki-black">
   <div class="modal-content">
-    <h4 class="modcon">MIT License</h4>
-    <div><p><?php require_once('LICENSE.txt'); ?></p></div>
+    <h4>MIT License</h4>
+    <div><p ><?php require_once('LICENSE.txt'); ?></p></div>
   </div>
   <div class="modal-footer">
-    <a href="#!" class="modal-action modal-close waves-effect waves-green btn-flat japokki">Agree</a>
+    <a href="#!" class="modal-action modal-close waves-effect waves-green btn-flat japokki-black">Agree</a>
   </div>
 </div>
 
