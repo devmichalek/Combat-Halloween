e25e5a067a95d7a748c982f84de82e5aad9571e0
devmichalek <devmichalek@gmail.com>
2018-08-05 14:29:20 +0200

    added start state and end state

--- main/main.cpp
+++ main/main.cpp
@@ -1,8 +1,17 @@
 #include "engine.h"
+#include "boost/lexical_cast.hpp"
 
-int main()
+int main(int argc, char** argv)
 {
-	std::unique_ptr<Engine> engine = std::make_unique<Engine>();
+	std::unique_ptr<Engine> engine;
+
+	if (argc > 1)
+	{
+		engine = std::make_unique<Engine>(boost::lexical_cast <int> (argv[1]), boost::lexical_cast <int> (argv[2]));
+	}
+	else
+		engine = std::make_unique<Engine>();
+	
 	engine->loop();
 	return EXIT_SUCCESS;
 }
