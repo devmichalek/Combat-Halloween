bf558aef971223773f57e27f55743376acf2e199
devmichalek <devmichalek@gmail.com>
2018-08-07 08:18:14 +0200

    foefactory

---
+++ foe/foefactory.cpp
@@ -0,0 +1,36 @@
+#include "foefactory.h"
+
+FoeFactory::FoeFactory()
+{
+
+}
+
+FoeFactory::~FoeFactory()
+{
+
+}
+
+void FoeFactory::free()
+{
+
+}
+
+void FoeFactory::reset()
+{
+
+}
+
+void FoeFactory::load(float screen_w, float screen_h)
+{
+
+}
+
+void FoeFactory::draw(sf::RenderWindow* &window)
+{
+
+}
+
+void FoeFactory::mechanics(double elapsedTime)
+{
+
+}
---
+++ foe/foefactory.h
@@ -0,0 +1,22 @@
+#pragma once
+#include "xyquadtree.h"
+#include <SFML/Graphics/RenderWindow.hpp>
+
+class FoeFactory
+{
+	int screen_w;
+	int screen_h;
+	XYQuadTree tree;
+
+public:
+	FoeFactory();
+	~FoeFactory();
+private:
+	void free();
+public:
+	void reset();
+
+	void load(float screen_w, float screen_h);
+	void draw(sf::RenderWindow* &window);
+	void mechanics(double elapsedTime);
+};
