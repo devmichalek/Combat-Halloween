e627782a9c56d3f0279d055d4a2c5e23cec21d78
devmichalek <devmichalek@gmail.com>
2018-08-09 23:35:57 +0200

    starting with skeleton, test

--- foe/foefactory.h
+++ foe/foefactory.h
@@ -4,8 +4,8 @@
 
 class FoeFactory
 {
-	sf::Rect<float> res;		// resolution of the window
-	sf::Rect<float> resBulk;	// resolution + window width and window height are doubled
+	sf::Rect<float>* res;		// resolution of the window
+	sf::Rect<float>* resBulk;	// resolution, window width and window height are doubled
 	FoeTree* tree;
 
 public:
@@ -17,8 +17,17 @@ public:
 	void reset();
 
 	void load(const float &screen_w, const float &screen_h);
-	void draw(sf::RenderWindow* &window);
-	void mechanics(const double &elapsedTime);
+	
+	// Process ------------------------
+	void processData(	sf::RenderWindow* &,
+						double,
+						Rect* &,
+						Rect* &,
+						bool &,
+						float &,
+						float &,
+						float &
+					);
 
 	void setBorderX(const float &newX);
 	void setBorderY(const float &newY);
