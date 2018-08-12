870848d53c83177f9801ea9631972f613d1906c3
devmichalek <devmichalek@gmail.com>
2018-08-12 10:32:12 +0200

    update

--- editor/main.cpp
+++ editor/main.cpp
@@ -1,6 +1,6 @@
 #include <QtWidgets/QApplication>
 #include <qfontdatabase.h>
-#include "mainwindow.h"
+#include "loggingwindow.h"
 #include "buttonfactory.h"
 #include "logging.h"
 
@@ -8,21 +8,24 @@ int main(int argc, char *argv[])
 {
 	// App.
 	QApplication application(argc, argv);
+
+	// Add font.
 	int id = QFontDatabase::addApplicationFont("fonts/Jaapokki-Regular.otf");
 	QString family = QFontDatabase::applicationFontFamilies(id).at(0);
 	QFont font(family);
 	QApplication::setFont(font);
 
-	MainWindow* window = new MainWindow;
+	// Create window.
+	LoggingWindow* window = new LoggingWindow;
 
 	Logging logging;
 	logging.load(window->size());
 	logging.setWindowAsParent(window);
 
 	// Buttons.
-	ButtonFactory buttonFactory;
-	buttonFactory.load(window->size());
-	buttonFactory.setParent(window);
+	// ButtonFactory buttonFactory;
+	// buttonFactory.load(window->size());
+	// buttonFactory.setParent(window);
 	// buttonFactory.setEnabledAll(false);
 	// QObject::connect(&logging, SIGNAL(&Logging::isReady()), &buttonFactory, SLOT(&ButtonFactory::setEnabledAll(true)));
 
