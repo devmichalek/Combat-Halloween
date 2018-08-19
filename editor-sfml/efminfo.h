8837706d6c6a4ef8f7d71ec90155e4a0ccc4ce9a
devmichalek <devmichalek@gmail.com>
2018-08-19 20:10:31 +0200

    editor file manager info

---
+++ editor-sfml/efminfo.h
@@ -0,0 +1,40 @@
+#pragma once
+#include "sprite.h"
+#include "text.h"
+
+class EFMInfo
+{
+	cmm::Sprite plank;
+	sf::RectangleShape blackLayer;
+
+	cmm::Text openedText;
+	cmm::Text openedFormText;
+	cmm::Text chosenText;
+	cmm::Text chosenFormText;
+	cmm::Text msgText;
+
+public:
+	EFMInfo();
+	~EFMInfo();
+private:
+	void free();
+public:
+	void reset();
+
+	void load(const float &screen_w, const float &screen_h);
+	void drawPlank(sf::RenderWindow* &window);
+	void draw(sf::RenderWindow* &window);
+	void drawLayer(sf::RenderWindow* &window);
+	void mechanics(const double &elapsedTime);
+
+	bool isMsgTextActive();
+	void setOpenedText(std::string msg = "-");
+	std::string setChosenText(std::string msg = "-");
+	void setMsgText(std::string msg);
+
+	float getLeft();
+	float getRight();
+	float getTop();
+	float getBot();
+	sf::FloatRect getRect();
+};
