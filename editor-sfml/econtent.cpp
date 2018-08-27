b650aaa23a3f147e0d472931079f5fb8ea37d6fe
devmichalek <devmichalek@gmail.com>
2018-08-27 23:39:12 +0200

    structure that hold the editor's content

---
+++ editor-sfml/econtent.cpp
@@ -0,0 +1,31 @@
+#include "econtent.h"
+
+int EContent::loadVersion = 0;
+int EContent::saveVersion = 0;
+std::vector<std::string> EContent::content;
+
+EContent::~EContent()
+{
+	loadVersion = 0;
+	saveVersion = 0;
+}
+
+bool EContent::isNewSaveVersion()
+{
+	return saveVersion != 0;
+}
+
+bool EContent::isNewLoadVersion()
+{
+	return loadVersion != 0;
+}
+
+void EContent::resetSaveVersion()
+{
+	saveVersion = 0;
+}
+
+void EContent::resetLoadVersion()
+{
+	loadVersion = 0;
+}
