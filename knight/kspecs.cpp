2676e03415f45f955da05557e9de3fb67fdda665
devmichalek <devmichalek@gmail.com>
2018-09-01 07:32:39 +0200

    features

---
+++ knight/kspecs.cpp
@@ -0,0 +1,54 @@
+#include "kspecs.h"
+
+KSpecs::KSpecs()
+{
+	freeSpecs();
+}
+
+KSpecs::~KSpecs()
+{
+	freeSpecs();
+}
+
+void KSpecs::freeSpecs()
+{
+	hp = 0;
+	mp = 0;
+	armour = 0;
+	mresistant = 0;
+	velocity = 0;
+	damage = 0;
+	mdamage = 0;
+	luck = 0;
+	experience = 0;
+	level = 0;
+	scale = 0.45;
+}
+
+void KSpecs::setSpecs()
+{
+	if (features.empty())	// happens while testing
+	{
+		features.push_back(20);
+		features.push_back(10);
+		features.push_back(50);
+		features.push_back(30);
+		features.push_back(25);
+		features.push_back(4);
+		features.push_back(2);
+		features.push_back(10);
+		features.push_back(0);
+		features.push_back(1);
+	}
+
+	hp = static_cast<float>(features[HEART_POINTS]);
+	mp = static_cast<float>(features[MAGIC_POINTS]);
+	armour = static_cast<float>(features[ARMOUR]);
+	mresistant = static_cast<float>(features[MAGIC_RESISTANT]);
+	velocity = static_cast<float>(features[MOVEMENT_SPEED]);
+	damage = static_cast<float>(features[DAMAGE]);
+	mdamage = static_cast<float>(features[MAGIC_DAMAGE]);
+	luck = static_cast<float>(features[LUCK]);
+	experience = static_cast<float>(features[EXPERIENCE]);
+	level = features[LEVEL];
+}
---
+++ knight/kspecs.h
@@ -0,0 +1,27 @@
+#pragma once
+#include "specs.h"
+
+class KSpecs : public Specs// Knight Specs
+{
+public:
+	float hp;
+	float mp;
+	float armour;
+	float mresistant;
+	float velocity;
+	float damage;
+	float mdamage;
+	float luck;
+	float experience;
+	int level;
+
+	// other
+	float scale;
+
+	KSpecs();
+	~KSpecs();
+private:
+	void freeSpecs();
+public:
+	void setSpecs();	// also reset
+};
