b67111d65466fc2bcaf8a19f31170c63f61d5071
devmichalek <devmichalek@gmail.com>
2018-09-01 07:32:05 +0200

    coxing

---
+++ knight/coxing.cpp
@@ -0,0 +1,13 @@
+#include "coxing.h"
+
+std::vector<int> Coxing::keys;
+
+Coxing::Coxing()
+{
+	// Delete it later 71@72@73@23@25@
+	keys.push_back(71);
+	keys.push_back(72);
+	keys.push_back(73);
+	keys.push_back(23);
+	keys.push_back(25);
+}
