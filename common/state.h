0e8f24d1c6da1f4396173a8157f297e39a7e994f
devmichalek <devmichalek@gmail.com>
2018-09-06 23:48:11 +0200

    Created state  folder with vs files + main.cpp

---
+++ common/state.h
@@ -0,0 +1,44 @@
+#pragma once
+#include <SFML/Graphics/RenderWindow.hpp>
+
+namespace cmm
+{
+	enum STATES
+	{
+		LOADING = 0,
+		INIT,
+		LOGIN,
+		MENU,
+		LEVELMENU,
+		PLATFORM,
+		TABLE,
+		EDITOR,
+		DEFAULT
+	};
+
+	class State
+	{
+	protected:
+		bool prev;
+		bool next;
+		bool exit;
+		virtual bool isReady() const = 0;
+
+	public:
+		State();
+		virtual ~State();
+
+	//protected:
+	//	bool isPrev() const;
+	//	bool isNext() const;
+	//	bool isExit() const;
+	//	bool isState() const;
+
+	public:
+		virtual void setState(int &state) = 0;
+		virtual void load(const float& screen_w, const float &screen_h) = 0;
+		virtual void handle(const sf::Event &event) = 0;
+		virtual void draw(sf::RenderWindow* &window) = 0;
+		virtual void mechanics(const double &elapsedTime) = 0;
+	};
+}
