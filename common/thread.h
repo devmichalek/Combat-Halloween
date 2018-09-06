bb88ad03de877cac89282d626de40499ab5effb1
devmichalek <devmichalek@gmail.com>
2018-09-06 23:50:45 +0200

    delete thread

---
+++ common/thread.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <thread>
+
+namespace cmm
+{
+	struct Thread
+	{
+		bool ready;		// ready to start next action
+		bool success;	// thread finished action with success
+		std::thread* thread;
+
+		Thread();
+		~Thread();
+		void free();
+		void reset();
+	};
+}
