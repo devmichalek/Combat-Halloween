fc0b0760bfe02dbea8f9d5cfb29c4f8429a9b48e
devmichalek <devmichalek@gmail.com>
2018-09-08 19:34:48 +0200

    fps GUI

---
+++ tools/fps.cpp
@@ -0,0 +1,131 @@
+#include "fps.h"
+#include "loading.h"
+
+FPS::FPS()
+{
+	free();
+}
+
+FPS::~FPS()
+{
+	free();
+}
+
+void FPS::free()
+{
+	position = NONE;
+	active = false;
+	screen_w = screen_h = 0;
+}
+
+void FPS::reset()
+{
+	active = false;
+	text.setText("-");
+	text.setAlpha(0xFF);
+}
+
+
+
+void FPS::load(const float &screen_w, const float &screen_h)
+{
+	free();
+
+	this->screen_w = screen_w;
+	this->screen_h = screen_h;
+
+	Loading::add(text.setFont(cmm::JAPOKKI_FONT_PATH));
+	if (Loading::isError())	return;
+	text.setSize(screen_w / 60);
+	text.setOutlineThickness(1);
+	text.setOutlineColor(sf::Color::Black);
+	text.setFillColor(sf::Color::White);
+	text.setAlpha(0xFF);
+	text.setText("-");
+}
+
+void FPS::draw(sf::RenderWindow* &window)
+{
+	if (!active)
+		return;
+
+	window->draw(text.get());
+}
+
+void FPS::mechanics(const double &elapsedTime)
+{
+	if (!active)
+		return;
+
+	fps.mechanics(elapsedTime);
+
+	if (fps.timePassed())
+	{
+		std::string result = std::to_string(static_cast<int>(fps.getFPS()));
+		text.setText(result);
+		setText();
+	}
+}
+
+void FPS::fadeout(const float &v, const int &min)
+{
+	text.fadeout(v, min);
+}
+
+
+
+const bool& FPS::isActive() const
+{
+	return active;
+}
+
+void FPS::setActive(bool active)
+{
+	this->active = active;
+}
+
+void FPS::setLeftTop()
+{
+	position = LEFT_TOP;
+}
+
+void FPS::setRightTop()
+{
+	position = RIGHT_TOP;
+}
+
+void FPS::setLeftBot()
+{
+	position = LEFT_BOT;
+}
+
+void FPS::setRightBot()
+{
+	position = RIGHT_BOT;
+}
+
+void FPS::setText()
+{
+	switch (position)
+	{
+	case NONE:
+		text.center(screen_w / 2, screen_h / 2);
+		break;
+
+	case LEFT_TOP:
+		text.setPosition(screen_w / 256, screen_h / 144);
+		break;
+
+	case RIGHT_TOP:
+		text.setPosition((screen_w  - screen_w / 256) - text.getWidth(), screen_h / 144);
+		break;
+
+	case LEFT_BOT:
+		text.setPosition(screen_w / 256, (screen_h - screen_h / 72) - text.getHeight());
+		break;
+
+	case RIGHT_BOT:
+		text.setPosition((screen_w - screen_w / 256) - text.getWidth(), (screen_h - screen_h / 72) - text.getHeight());
+		break;
+	}
+}
---
+++ tools/fps.h
@@ -0,0 +1,47 @@
+#pragma once
+#include "text.h"
+#include "fpsconsole.h"
+#include <SFML/Graphics/RenderWindow.hpp>
+
+class FPS
+{
+	enum POSITION
+	{
+		NONE = -1,
+		LEFT_TOP = 0,
+		RIGHT_TOP,
+		LEFT_BOT,
+		RIGHT_BOT
+	};
+
+	sf::Int8 position;
+	cmm::Text text;
+	cmm::FPSConsole fps;
+
+	bool active;
+	float screen_w;
+	float screen_h;
+
+public:
+	FPS();
+	~FPS();
+private:
+	void free();
+public:
+	void reset();
+
+	void load(const float &screen_w, const float &screen_h);
+	void draw(sf::RenderWindow* &window);
+	void mechanics(const double &elapsedTime);
+	void fadeout(const float &v, const int &min);
+
+	const bool& isActive() const;
+	void setActive(bool active);
+
+	void setLeftTop();
+	void setRightTop();
+	void setLeftBot();
+	void setRightBot();
+private:
+	void setText();
+};
