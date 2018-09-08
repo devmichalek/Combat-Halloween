d486325a0d9093474e641194df7fece397dc149f
devmichalek <devmichalek@gmail.com>
2018-09-08 19:33:52 +0200

    mostly pure virtual functions - button

---
+++ tools/button.cpp
@@ -0,0 +1,108 @@
+#include "button.h"
+#include "loading.h"
+
+Button::Button()
+{
+	locked = false;
+}
+
+Button::~Button()
+{
+	locked = false;
+}
+
+void Button::load(const char* path, bool locked, int numOfOffsets)
+{
+	free();
+
+	Loading::add(click.load("sounds/click.wav"));
+	Loading::add(button.load(path, numOfOffsets));
+	if (Loading::isError())	return;
+
+	this->locked = locked;
+	locked ? button.setOffset(numOfOffsets - 1) : button.setOffset(0);
+}
+
+void Button::setPosition(float x, float y)
+{
+	button.setPosition(x, y);
+}
+
+void Button::setScale(float x, float y)
+{
+	button.setScale(x, y);
+}
+
+void Button::setVolume(const float &volume)
+{
+	click.setVolume(volume);
+}
+
+void Button::draw(sf::RenderWindow* &window)
+{
+	window->draw(button.get());
+}
+
+void Button::fadein(const float &v, const int &max)
+{
+	button.fadein(v, max);
+}
+
+void Button::fadeout(const float &v, const int &min)
+{
+	button.fadeout(v, min);
+}
+
+const float& Button::getLeft() const
+{
+	return button.getLeft();
+}
+
+float Button::getRight() const
+{
+	return button.getRight();
+}
+
+const float& Button::getTop() const
+{
+	return button.getTop();
+}
+
+float Button::getBot() const
+{
+	return button.getBot();
+}
+
+float Button::getWidth() const
+{
+	return button.getWidth();
+}
+
+float Button::getHeight() const
+{
+	return button.getHeight();
+}
+
+void Button::lock()
+{
+	if (!locked)
+	{
+		locked = true;
+		button.setOffset(2);
+	}
+}
+
+void Button::unlock()
+{
+	if (locked)
+	{
+		locked = false;
+		button.setOffset(0);
+	}
+}
+
+void Button::reset()
+{
+	free();
+	locked ? button.setOffset(button.getNumOfOffsets() - 1) : button.setOffset(0);
+}
---
+++ tools/button.h
@@ -0,0 +1,38 @@
+#pragma once
+#include "sprite.h"
+#include "sound.h"
+#include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/Window/Event.hpp>
+
+class Button
+{
+protected:
+	cmm::Sound click;
+	cmm::Sprite button;
+	bool locked;
+
+public:
+	Button();
+	virtual ~Button();
+	virtual void free() = 0;
+
+	void load(const char* path, bool locked = false, int numOfOffsets = 3);
+	void setPosition(float x, float y);
+	void setScale(float x, float y);
+	void setVolume(const float &volume);
+	
+	void draw(sf::RenderWindow* &window);
+	void fadein(const float &v, const int &max);
+	void fadeout(const float &v, const int &min);
+
+	const float& getLeft() const;
+	float getRight() const;
+	const float& getTop() const;
+	float getBot() const;
+	float getWidth() const;
+	float getHeight() const;
+
+	void lock();
+	void unlock();
+	void reset();
+};
