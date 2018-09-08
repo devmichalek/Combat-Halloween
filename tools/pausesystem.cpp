27a613c3f32fec4da4e9038b1ce1d887f1cad83e
devmichalek <devmichalek@gmail.com>
2018-09-08 19:35:06 +0200

    pause system GUI

---
+++ tools/pausesystem.cpp
@@ -0,0 +1,98 @@
+#include "pausesystem.h"
+#include "loading.h"
+
+PauseSystem::PauseSystem()
+{
+	free();
+}
+
+PauseSystem::~PauseSystem()
+{
+	free();
+}
+
+void PauseSystem::free()
+{
+	active = false;
+	key_released = false;
+}
+
+
+
+void PauseSystem::load(const float &screen_w, const float &screen_h)
+{
+	free();
+
+	Loading::add(text.setFont(cmm::JCANDLE_FONT_PATH));
+	if (Loading::isError())	return;
+	text.setText("GAME PAUSED");
+	text.setSize(screen_h / 10);
+	text.setFillColor(sf::Color(0xFF, 0xFF, 0xFF));
+	text.center(screen_w / 2, screen_h / 2);
+
+	Loading::add(click.load("sounds/click.wav"));
+	if (Loading::isError())	return;
+
+	Loading::add(blackout.create(static_cast<int>(screen_w), static_cast<int>(screen_h)));
+	if (Loading::isError())	return;
+	blackout.setColor(sf::Color(0, 0, 0));
+}
+
+void PauseSystem::handle(const sf::Event &event)
+{
+	// keyboard stuff
+	if (event.type == sf::Event::KeyPressed && !key_released)
+	{
+		if (event.key.code == 15)	// 'p'
+		{
+			key_released = true;
+			active = !active;
+			click.play();
+		}
+	}
+
+	if (event.type == sf::Event::KeyReleased)
+	{
+		key_released = false;
+	}
+}
+
+void PauseSystem::draw(sf::RenderWindow* &window)
+{
+	window->draw(blackout.get());
+	window->draw(text.get());
+}
+
+void PauseSystem::fadein(const float &v, const int &max)
+{
+	blackout.fadein(v, max);
+	text.fadein(v, max);
+}
+
+void PauseSystem::fadeout(const float &v, const int &min)
+{
+	blackout.fadeout(v, min);
+	text.fadeout(v, min);
+}
+
+
+
+const bool& PauseSystem::isActive() const
+{
+	return active;
+}
+
+void PauseSystem::turnOnOff()
+{
+	active = !active;
+}
+
+float PauseSystem::getAlpha() const
+{
+	return blackout.getAlpha();
+}
+
+void PauseSystem::setVolume(const float &volume)
+{
+	click.setVolume(volume);
+}
