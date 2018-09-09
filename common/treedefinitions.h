929145df1dd56b0946054ab6a16b0f372881745a
devmichalek <devmichalek@gmail.com>
2018-09-09 16:57:13 +0200

    moved from foe directory

---
+++ common/treedefinitions.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <boost/foreach.hpp>
+#include <boost/geometry.hpp>
+#include <boost/geometry/geometries/point.hpp>
+#include <boost/geometry/geometries/box.hpp>
+#include <boost/geometry/index/rtree.hpp>
+
+// bg, bgi, bgm
+namespace bg = boost::geometry;
+namespace bgi = bg::index;
+namespace bgm = bg::model;
+
+// Point
+typedef bgm::point<float, 2, bg::cs::cartesian> Point;
+
+// Box
+typedef bgm::box<Point> Box;
+
+// Box + ID
+typedef std::pair<Box, int> BoxID;
+
+// Region Tree
+typedef bgi::rtree<BoxID, bgi::quadratic<16>> RTree;
