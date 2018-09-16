b63537e49772065be62ba15586e2d9bdc94d10ed
devmichalek <devmichalek@gmail.com>
2018-09-16 20:59:15 +0200

    coxing is now part of namespace cmm
    Added:
    setKey, getKey, getActiveKeysAmount, resetKeys, saveKeys, loadKeys, isPossibleKey

--- common/coxing.cpp
+++ common/coxing.cpp
@@ -1,13 +1,133 @@
 #include "coxing.h"
+#include "dirent.h"
+#include <direct.h>
+#include <fstream>
+#include <string>
+#include <boost/lexical_cast.hpp>
 
-std::vector<int> Coxing::keys;
+std::vector<int> cmm::Coxing::keys;
 
-Coxing::Coxing()
+cmm::Coxing::Coxing()
 {
-	// Delete it later 71@72@73@23@25@
-	keys.push_back(71);
-	keys.push_back(72);
-	keys.push_back(73);
-	keys.push_back(23);
-	keys.push_back(25);
+	//...
+}
+
+void cmm::Keys::setKey(int which, int newKey)
+{
+	keys[which] = newKey;
+}
+
+int cmm::Keys::getKey(int which)
+{
+	return keys[which];
+}
+
+int cmm::Keys::getActiveKeysAmount()
+{
+	return KEY_ACTION::JUMP_ATTACK;
+}
+
+void cmm::Keys::resetKeys()
+{
+	keys.clear();
+
+	for (int i = 0; i < KEY_ACTION::JUMP_ATTACK; ++i)
+		keys.push_back(0);
+
+	// Reset values.
+	keys[MOVE_LEFT] = 71;
+	keys[MOVE_RIGHT] = 72;
+	keys[JUMP] = 73;
+	keys[ATTACK] = 23;
+	keys[SHIELD] = 25;
+
+	// Overwrite / Save (force) to file.
+	saveKeys();
+}
+
+void cmm::Keys::saveKeys()
+{
+	// Open directory
+	DIR* dir = opendir("local");
+	if (!dir)	// Create directory if it does not exist
+		_mkdir("local");
+
+	std::ofstream file;
+	file.open("local/keys.settings");
+
+	for (auto it : keys)
+	{
+		file << std::to_string(it);
+		if (it != *(keys.end() - 1))
+			file << std::endl;
+	}
+
+	file.close();	// not needed actually
+}
+
+void cmm::Keys::loadKeys()
+{
+	keys.clear();
+
+	std::fstream file;
+	file.open("local/keys.settings");
+	if (file.bad())	// file does not exist -> reset keys
+	{
+		file.close();
+		resetKeys();
+	}
+	else
+	{
+		std::string line = "";
+		while (std::getline(file, line))
+		{
+			int newKey = boost::lexical_cast<int>(line);
+			if (!isPossibleKey(newKey))
+			{
+				file.close();
+				resetKeys();
+				break;
+			}
+
+			keys.push_back(newKey);
+		}
+
+		if (keys.size() < KEY_ACTION::JUMP_ATTACK) // file is broken?
+		{
+			file.close();
+			resetKeys();
+		}
+
+	}
+	file.close();
+}
+
+bool cmm::Keys::isPossibleKey(const int &code)
+{
+	if (code == 14 || code == 15)	// pause key or chat key
+	{
+		return false;
+	}
+	if (code > -1 && code <= 35)	// a .. z and numbers
+	{
+		return true;
+	}
+	if (code >= 75 && code <= 84)	// numbers
+	{
+		return true;
+	}
+	if (code >= 71 && code <= 74)	// arrows
+	{
+		return true;
+	}
+	if (code == 57)					// space
+	{
+		return true;
+	}
+	if (code >= 37 && code <= 43 && code != 40)	// lctrl, lshift, lalt, rctrl, rshift, ralt
+	{
+		return true;
+	}
+
+	return false;
 }
--- common/coxing.h
+++ common/coxing.h
@@ -1,10 +1,156 @@
 #pragma once
 #include <vector>
 
-class Coxing
+namespace cmm
 {
-protected:
-	static std::vector<int> keys;
-public:
-	Coxing();
-};
+	class Coxing
+	{
+	protected:
+		enum KEY_ACTION
+		{
+			MOVE_LEFT = 0,
+			MOVE_RIGHT,
+			JUMP,
+			ATTACK,
+			SHIELD
+		};
+
+		static std::vector<int> keys;
+	public:
+		Coxing();
+	};
+
+	class Keys : public Coxing
+	{
+	protected:
+		enum KEY_ACTION
+		{
+			MOVE_LEFT = 0,
+			MOVE_RIGHT,
+			JUMP,
+			ATTACK,
+			SHIELD,
+			JUMP_ATTACK,
+			JUMP_SHIELD,
+			PAUSE,
+			CHAT,
+			AMOUNT
+		};
+
+	public:
+		static void setKey(int which, int newKey);	// special for chat
+		static int getKey(int which);
+		static int getActiveKeysAmount();
+		void resetKeys();
+		void saveKeys();
+		void loadKeys();
+		bool isPossibleKey(const int &code);
+	};
+}
+/*
+enum  	Key {
+Unknown = -1,
+A = 0,
+B,
+C,
+D, // 3
+E,
+F,
+G,
+H,
+I,
+J,
+K, // 10
+L,
+M,
+N,
+O,
+P, // 15
+Q,
+R,
+S,
+T,
+U, // 20
+V,
+W,
+X,
+Y,
+Z, // 25
+Num0,
+Num1,
+Num2,
+Num3,
+Num4, // 30
+Num5,
+Num6,
+Num7,
+Num8,
+Num9, // 35
+Escape,
+LControl, // 37
+LShift, // 38
+LAlt, // 39
+LSystem, // 40
+RControl, // 41
+RShift, // 42
+RAlt, // 43
+RSystem,
+Menu, // 45
+LBracket,
+RBracket,
+SemiColon,
+Comma,
+Period,  // 50
+Quote,
+Slash,
+BackSlash,
+Tilde,
+Equal, // 55
+Dash,
+Space, // 57
+Return,
+BackSpace,
+Tab, // 60
+PageUp,
+PageDown,
+End,
+Home,
+Insert, // 65
+Delete,
+Add,
+Subtract,
+Multiply,
+Divide, // 70
+Left, // 71
+Right, // 72
+Up, // 73
+Down, // 74
+Numpad0, // 75
+Numpad1,
+Numpad2,
+Numpad3,
+Numpad4,
+Numpad5,
+Numpad6,
+Numpad7,
+Numpad8,
+Numpad9, // 84
+F1,
+F2,
+F3,
+F4,
+F5,
+F6,
+F7,
+F8,
+F9,
+F10,
+F11,
+F12,
+F13,
+F14,
+F15,
+Pause,
+KeyCount
+}
+*/
