edd83c341aa85b9c4d377a94b812eba4e8310c4e
devmichalek <devmichalek@gmail.com>
2018-09-20 20:43:32 +0200

    update due to new load system

--- states/platform/foe/main.cpp
+++ states/platform/foe/main.cpp
@@ -1,76 +1,89 @@
 #include "core.h"
+#include "loading.h"
 #include "foefactory.h"
-#include "text.h"
+#include "fps.h"
 
 cmm::Core* core = new cmm::Core(0, sf::Color(21, 21, 29, 0xFF));
 FoeFactory* foeFactory = new FoeFactory;
+std::vector<cmm::State*> states;
+sf::Rect<float>* character = new sf::Rect<float>;
+sf::Rect<float>* characterAttack = new sf::Rect<float>;
+sf::RectangleShape charRect;
+sf::RectangleShape charAttackRect;
+FPS fps;
+bool makeUpdate = true;
+bool characterHasAttacked = false;
+float characterDamage = 0;
+float characterArmour = 1;
+float characterHP = 1000;
 
-enum STATES
+// Load function.
+void loading_loop()
 {
-	LEVELMENU = 4,
-	PLATFORM,
-	ENDSCENE
-};
+	float screen_w = (float)core->getWidth();
+	float screen_h = (float)core->getHeight();
 
-int main(int argc, char** argv)
+	while (core->open)
+	{
+		while (core->isEvent())
+		{
+			if (core->getEvent().type == sf::Event::Closed)
+				core->open = false;
+		}
+
+		states[core->state]->draw(core->getWindow());
+		states[core->state]->mechanics(core->getElapsedTime());
+
+		switch (LoadingState::getState())
+		{
+		case 30:
+			foeFactory->load(screen_w, screen_h);
+
+			character->left = -1000;
+			character->top = -1000;
+			character->width = 100;
+			character->height = 100;
+
+			characterAttack->left = -1000;
+			characterAttack->top = -1000;
+			characterAttack->width = 50;
+			characterAttack->height = 50;
+
+			charRect.setFillColor(sf::Color(0xFF, 0xFF, 0xFF, 50));
+			charAttackRect.setFillColor(sf::Color(0xFF, 0, 0, 30));
+			charRect.setSize(sf::Vector2f(character->width, character->height));
+			charAttackRect.setSize(sf::Vector2f(characterAttack->width, characterAttack->height));
+
+			fps.load(core->getWidth(), core->getHeight());
+			fps.setRightTop();
+			fps.setActive(true);
+			break;
+		}
+
+		states[core->state]->setState(core->state);
+		if (core->state != cmm::LOADING)// game starts from here
+			break;
+	}
+
+	//delete states[cmm::LOADING];
+	//states[cmm::LOADING] = nullptr;
+	states.shrink_to_fit();
+}
+
+void main_loop()
 {
-	core->create("Combat Halloween");
-	core->state = PLATFORM;
-	float screen_w = core->getWidth();
-	float screen_h = core->getHeight();
-	foeFactory->load(screen_w, screen_h);
-
-	sf::Rect<float>* character = new sf::Rect<float>;
-	character->left = -1000;
-	character->top = -1000;
-	character->width = 100;
-	character->height = 100;
-
-	sf::Rect<float>* characterAttack = new sf::Rect<float>;
-	characterAttack->left = -1000;
-	characterAttack->top = -1000;
-	characterAttack->width = 50;
-	characterAttack->height = 50;
-
-	bool makeUpdate = true;
-	bool characterHasAttacked = false;
-	float characterDamage = 0;
-	float characterArmour = 1;
-	float characterHP = 1000;
-
-	sf::RectangleShape charRect;
-	sf::RectangleShape charAttackRect;
-	charRect.setFillColor(sf::Color(0xFF, 0xFF, 0xFF, 50));
-	charAttackRect.setFillColor(sf::Color(0xFF, 0, 0, 30));
-	charRect.setSize(sf::Vector2f(character->width, character->height));
-	charAttackRect.setSize(sf::Vector2f(characterAttack->width, characterAttack->height));
-
-	cmm::Text fpsText;
-	fpsText.setFont(cmm::JAPOKKI_FONT_PATH);
-	fpsText.setFillColor(sf::Color::White);
-	fpsText.setPosition(0, 0);
-	fpsText.setSize(22);
-	fpsText.setAlpha(0xFF);
-	fpsText.setText("a");
-
-	double counter = 0;
-	double max = 0.25;
 	double elapsedTime = 0;
 	while (core->open)
 	{
-		elapsedTime = core->getElapsedTime();
-		characterHasAttacked = false;
-
 		// clear
+		elapsedTime = core->getElapsedTime();
 		core->clear();
 
 		// events, mouse, keyboard
 		while (core->isEvent())
 		{
 			if (core->getEvent().type == sf::Event::Closed)
-			{
 				core->open = false;
-			}
 
 			if (core->getEvent().type == sf::Event::MouseButtonPressed)
 			{
@@ -101,26 +114,42 @@ int main(int argc, char** argv)
 		charAttackRect.setPosition(sf::Vector2f(characterAttack->left, characterAttack->top));
 		core->getWindow()->draw(charRect);
 		core->getWindow()->draw(charAttackRect);
-		core->getWindow()->draw(fpsText.get());
 
-		if (core->state == PLATFORM)
-		{
-			foeFactory->setBorders(0, 0);
-			foeFactory->update(core->getWindow(), elapsedTime,
-				character, characterAttack, characterHasAttacked, characterDamage, characterHP,
-				characterArmour);
-		}
+		fps.draw(core->getWindow());
+		fps.mechanics(elapsedTime);
 
-		counter += elapsedTime;
-		if (counter > max)
-		{
-			fpsText.setText(std::to_string(1.0f / elapsedTime));
-			counter = 0;
-		}
+		foeFactory->setBorders(0, 0);
+		foeFactory->update(core->getWindow(), elapsedTime,
+			character, characterAttack, characterHasAttacked, characterDamage, characterHP,
+			characterArmour);
 
+		// display
 		core->display();
 	}
+}
 
+int main(int argc, char** argv)
+{
+	// Init only loading state.
+	states.push_back(new Loading);
+	if (Loading::isError())
+		return -1;
+
+	// Create window.
+	core->create("Combat Halloween");
+	states[cmm::LOADING]->load(core->getWidth(), core->getHeight());
+
+	// Loops.
+	loading_loop();
+	main_loop();
+
+	// Free states.
+	for (auto &it : states)
+	{
+		delete it;
+		it = nullptr;
+	}
+	states.clear();
 	delete core;
 	delete foeFactory;
 	return 0;
