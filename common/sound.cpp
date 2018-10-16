4ed33e7d958ae288b102c53bb790bbbf6fa171c2
devmichalek <devmichalek@gmail.com>
2018-10-16 20:40:50 +0200

    avoid duplicated sound buffers

--- common/sound.cpp
+++ common/sound.cpp
@@ -1,5 +1,38 @@
 #include "sound.h"
 
+cmm::Sound::BufferMap::BufferMap()
+{
+	// ...
+}
+
+cmm::Sound::BufferMap::~BufferMap()
+{
+	if (!paths.empty())
+		paths.clear();
+
+	if (!buffers.empty())
+	{
+		for (auto &it : buffers)
+		{
+			delete it;
+			it = nullptr;
+		}
+		buffers.clear();
+	}
+}
+
+sf::SoundBuffer* cmm::Sound::BufferMap::get(std::string &path)
+{
+	auto it = std::find(paths.begin(), paths.end(), path);
+	if (it != std::end(paths))
+	{
+		size_t pos = paths.size() - (paths.end() - it);
+		return buffers[pos];
+	}
+
+	return nullptr;
+}
+
 void cmm::Sound::stop()
 {
 	sound->stop();
@@ -23,14 +56,29 @@ void cmm::Sound::pause()
 	sound->pause();
 }
 
-std::string cmm::Sound::load(const char* path)
+std::string cmm::Sound::load(std::string path)
 {
-	buffer = std::make_unique<sf::SoundBuffer>();
-	if (buffer->loadFromFile(path))
+	sf::SoundBuffer* buffer = nullptr;
+	sf::SoundBuffer* ptr = buffer_map.get(path);
+
+	if (!ptr) // new one
+		buffer = new sf::SoundBuffer();
+	else
+		buffer = ptr;
+
+	if (!ptr && buffer->loadFromFile(path))
 	{
 		sound = std::make_unique<sf::Sound>();
 		sound->setBuffer(*buffer);
+		buffer_map.paths.push_back(path);
+		buffer_map.buffers.push_back(buffer);
 		return "Success: Sound \"" + std::string(path) + "\" loaded correctly.";
+	}
+	else if (ptr)
+	{
+		sound = std::make_unique<sf::Sound>();
+		sound->setBuffer(*buffer);
+		return "Success: Sound Buffer set correctly.";
 	}
 
 	return "Error: Cannot create sound buffer from\"" + std::string(path) + "\".";
--- common/sound.h
+++ common/sound.h
@@ -8,16 +8,26 @@ namespace cmm
 {
 	class Sound : public SoundData
 	{
+		struct BufferMap
+		{
+			std::vector<std::string> paths;
+			std::vector<sf::SoundBuffer*> buffers;
+
+			BufferMap();
+			~BufferMap();
+			sf::SoundBuffer* get(std::string &path);
+		};
+
+		inline static BufferMap buffer_map;
 	protected:
 		std::unique_ptr<sf::Sound> sound;
-		std::unique_ptr<sf::SoundBuffer> buffer;
 
 	public:
 		void stop();
 		void play();
 		const bool isPlaying() const;
 		void pause();
-		std::string load(const char* path);
+		std::string load(std::string path);
 		void setVolume(const float &newVolume);
 		float getVolume() const;
 		const bool& isPlayable() const;
